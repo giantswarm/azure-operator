7059f634bdde7b5d877c928ac1f76fcc47f9e382
Ross Fairbanks <ross@giantswarm.io>
2017-08-31 16:30:16 +0200

    Add Azure Resource Group resource.

---
+++ service/resource/resourcegroup/resource.go
@@ -0,0 +1,298 @@
+package resourcegroup
+
+import (
+	azureresource "github.com/Azure/azure-sdk-for-go/arm/resources/resources"
+	"github.com/Azure/go-autorest/autorest/to"
+	"github.com/giantswarm/azuretpr"
+	"github.com/giantswarm/microerror"
+	"github.com/giantswarm/micrologger"
+	"github.com/giantswarm/operatorkit/framework"
+
+	"github.com/giantswarm/azure-operator/client"
+	"github.com/giantswarm/azure-operator/service/key"
+)
+
+const (
+	// Name is the identifier of the resource.
+	Name = "resourcegroup"
+
+	clusterIDTag  = "ClusterID"
+	customerIDTag = "CustomerID"
+	managedBy     = "azure-operator"
+)
+
+type Config struct {
+	// Dependencies.
+
+	AzureConfig *client.AzureConfig
+	Logger      micrologger.Logger
+}
+
+// DefaultConfig provides a default configuration to create a new service by
+// best effort.
+func DefaultConfig() Config {
+	return Config{
+		// Dependencies.
+		AzureConfig: nil,
+		Logger:      nil,
+	}
+}
+
+type Resource struct {
+	// Dependencies.
+
+	azureConfig *client.AzureConfig
+	logger      micrologger.Logger
+}
+
+// New creates a new configured resource group resource.
+func New(config Config) (*Resource, error) {
+	// Dependencies.
+	if config.AzureConfig == nil {
+		return nil, microerror.Maskf(invalidConfigError, "config.AzureConfig must not be empty.")
+	}
+	if config.Logger == nil {
+		return nil, microerror.Maskf(invalidConfigError, "config.Logger must not be empty.")
+	}
+
+	newService := &Resource{
+		azureConfig: config.AzureConfig,
+		logger: config.Logger.With(
+			"resource", Name,
+		),
+	}
+
+	return newService, nil
+}
+
+// GetCurrentState gets the resource group for this cluster from the Azure API.
+func (r *Resource) GetCurrentState(obj interface{}) (interface{}, error) {
+	customObject, err := toCustomObject(obj)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	var resourceGroup azureresource.Group
+	{
+		groupsClient, err := r.getGroupsClient()
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+
+		resourceGroup, err = groupsClient.Get(key.ClusterID(customObject))
+		if err != nil {
+			if client.ResponseWasNotFound(resourceGroup.Response) {
+				// Fall through.
+				return nil, nil
+			}
+
+			return nil, microerror.Mask(err)
+		}
+	}
+
+	return &resourceGroup, nil
+}
+
+// GetDesiredState returns the desired resource group for this cluster.
+func (r *Resource) GetDesiredState(obj interface{}) (interface{}, error) {
+	customObject, err := toCustomObject(obj)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	r.logger.Log("cluster", key.ClusterID(customObject), "debug", "computing the new resource group")
+
+	tags := map[string]string{
+		clusterIDTag:  key.ClusterID(customObject),
+		customerIDTag: key.ClusterCustomer(customObject),
+	}
+	resourceGroup := &azureresource.Group{
+		Name:      to.StringPtr(key.ClusterID(customObject)),
+		Location:  to.StringPtr(key.Location(customObject)),
+		ManagedBy: to.StringPtr(managedBy),
+		Tags:      to.StringMapPtr(tags),
+	}
+
+	r.logger.Log("cluster", key.ClusterID(customObject), "debug", "computed the new resource group")
+
+	return resourceGroup, nil
+}
+
+// GetCreateState returns the resource group for this cluster if it should be
+// created.
+func (r *Resource) GetCreateState(obj, currentState, desiredState interface{}) (interface{}, error) {
+	customObject, err := toCustomObject(obj)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+	currentResourceGroup, err := toResourceGroup(currentState)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+	desiredResourceGroup, err := toResourceGroup(desiredState)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	r.logger.Log("cluster", key.ClusterID(customObject), "debug", "checking if the resource group has to be created")
+
+	var resourceGroupToCreate *azureresource.Group
+	if currentResourceGroup == nil {
+		resourceGroupToCreate = desiredResourceGroup
+	}
+
+	r.logger.Log("cluster", key.ClusterID(customObject), "debug", "checked if the resource group has to be created")
+
+	return resourceGroupToCreate, nil
+}
+
+// GetDeleteState returns the resource group for this cluster if it should be
+// deleted.
+func (r *Resource) GetDeleteState(obj, currentState, desiredState interface{}) (interface{}, error) {
+	customObject, err := toCustomObject(obj)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+	currentResourceGroup, err := toResourceGroup(currentState)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+	desiredResourceGroup, err := toResourceGroup(desiredState)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	r.logger.Log("cluster", key.ClusterID(customObject), "debug", "checking if the resource group has to be deleted")
+
+	var resourceGroupToDelete *azureresource.Group
+	if currentResourceGroup != nil {
+		resourceGroupToDelete = desiredResourceGroup
+	}
+
+	r.logger.Log("cluster", key.ClusterID(customObject), "debug", "checked if the resource group has to be deleted")
+
+	return resourceGroupToDelete, nil
+}
+
+// GetUpdateState returns nil for the create, delete and update states because
+// resource groups are not updated.
+func (r *Resource) GetUpdateState(obj, currentState, desiredState interface{}) (interface{}, interface{}, interface{}, error) {
+	return nil, nil, nil, nil
+}
+
+// Name returns the resource name.
+func (r *Resource) Name() string {
+	return Name
+}
+
+// ProcessCreateState creates the resource group via the Azure API.
+func (r *Resource) ProcessCreateState(obj, createState interface{}) error {
+	customObject, err := toCustomObject(obj)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	resourceGroupToCreate, err := toResourceGroup(createState)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	if resourceGroupToCreate != nil {
+		r.logger.Log("cluster", key.ClusterID(customObject), "debug", "creating the resource group in the Azure API")
+
+		groupClient, err := r.getGroupsClient()
+		if err != nil {
+			return microerror.Mask(err)
+		}
+
+		_, err = groupClient.CreateOrUpdate(*resourceGroupToCreate.Name, *resourceGroupToCreate)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+
+		r.logger.Log("cluster", key.ClusterID(customObject), "debug", "created the resource group in the Azure API")
+
+	} else {
+		r.logger.Log("cluster", key.ClusterID(customObject), "debug", "the resource group already exists in the Azure API")
+	}
+
+	return nil
+}
+
+// ProcessDeleteState deletes the resource group via the Azure API.
+func (r *Resource) ProcessDeleteState(obj, deleteState interface{}) error {
+	customObject, err := toCustomObject(obj)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+	resourceGroupToDelete, err := toResourceGroup(deleteState)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	if resourceGroupToDelete != nil {
+		r.logger.Log("cluster", key.ClusterID(customObject), "debug", "deleting the resource group in the Azure API")
+
+		groupsClient, err := r.getGroupsClient()
+		if err != nil {
+			return microerror.Mask(err)
+		}
+
+		// Delete the resource group which also deletes all resources it
+		// contains. We wait for the error channel while the deletion happens.
+		_, errchan := groupsClient.Delete(*resourceGroupToDelete.Name, nil)
+		err = <-errchan
+		if err != nil {
+			return microerror.Mask(err)
+		}
+
+		r.logger.Log("cluster", key.ClusterID(customObject), "debug", "deleted the resource group in the Azure API")
+
+	} else {
+		r.logger.Log("cluster", key.ClusterID(customObject), "debug", "the resource group does not exist in the Azure API")
+	}
+
+	return nil
+}
+
+// ProcessUpdateState returns nil because resource groups are not updated.
+func (r *Resource) ProcessUpdateState(obj, updateState interface{}) error {
+	return nil
+}
+
+// Underlying returns the underlying resource.
+func (r *Resource) Underlying() framework.Resource {
+	return r
+}
+
+func (r *Resource) getGroupsClient() (*azureresource.GroupsClient, error) {
+	azureClients, err := client.NewAzureClientSet(r.azureConfig)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	return azureClients.GroupsClient, nil
+}
+
+func toCustomObject(v interface{}) (azuretpr.CustomObject, error) {
+	customObjectPointer, ok := v.(*azuretpr.CustomObject)
+	if !ok {
+		return azuretpr.CustomObject{}, microerror.Maskf(wrongTypeError, "expected '%T', got '%T'", &azuretpr.CustomObject{}, v)
+	}
+	customObject := *customObjectPointer
+
+	return customObject, nil
+}
+
+func toResourceGroup(v interface{}) (*azureresource.Group, error) {
+	if v == nil {
+		return nil, nil
+	}
+
+	resourceGroup, ok := v.(*azureresource.Group)
+	if !ok {
+		return nil, microerror.Maskf(wrongTypeError, "expected '%T', got '%T'", &azureresource.Group{}, v)
+	}
+
+	return resourceGroup, nil
+}
---
+++ service/resource/resourcegroup/resource_test.go
@@ -0,0 +1,352 @@
+package resourcegroup
+
+import (
+	"testing"
+
+	azureresource "github.com/Azure/azure-sdk-for-go/arm/resources/resources"
+	"github.com/Azure/go-autorest/autorest/to"
+	"github.com/giantswarm/azuretpr"
+	azuretprspec "github.com/giantswarm/azuretpr/spec"
+	"github.com/giantswarm/clustertpr"
+	"github.com/giantswarm/clustertpr/spec"
+	"github.com/giantswarm/micrologger/microloggertest"
+
+	"github.com/giantswarm/azure-operator/client"
+)
+
+func Test_Resource_ResourceGroup_GetDesiredState(t *testing.T) {
+	testCases := []struct {
+		Obj              interface{}
+		ExpectedName     string
+		ExpectedLocation string
+		ExpectedTags     map[string]string
+	}{
+		{
+			// Case 1. Standard cluster ID format.
+			Obj: &azuretpr.CustomObject{
+				Spec: azuretpr.Spec{
+					Azure: azuretprspec.Azure{
+						Location: "West Europe",
+					},
+					Cluster: clustertpr.Spec{
+						Cluster: spec.Cluster{
+							ID: "5xchu",
+						},
+						Customer: spec.Customer{
+							ID: "giantswarm",
+						},
+					},
+				},
+			},
+			ExpectedName:     "5xchu",
+			ExpectedLocation: "West Europe",
+			ExpectedTags: map[string]string{
+				"ClusterID":  "5xchu",
+				"CustomerID": "giantswarm",
+			},
+		},
+		{
+			// Case 2. Custom cluster ID format.
+			Obj: &azuretpr.CustomObject{
+				Spec: azuretpr.Spec{
+					Azure: azuretprspec.Azure{
+						Location: "East Asia",
+					},
+					Cluster: clustertpr.Spec{
+						Cluster: spec.Cluster{
+							ID: "test-cluster",
+						},
+						Customer: spec.Customer{
+							ID: "acme",
+						},
+					},
+				},
+			},
+			ExpectedName:     "test-cluster",
+			ExpectedLocation: "East Asia",
+			ExpectedTags: map[string]string{
+				"ClusterID":  "test-cluster",
+				"CustomerID": "acme",
+			},
+		},
+	}
+
+	var err error
+	var newResource *Resource
+	{
+		resourceConfig := DefaultConfig()
+		resourceConfig.AzureConfig = client.DefaultAzureConfig()
+		resourceConfig.Logger = microloggertest.New()
+		newResource, err = New(resourceConfig)
+		if err != nil {
+			t.Fatalf("expected '%#v' got '%#v'", nil, err)
+		}
+
+		for i, tc := range testCases {
+			result, err := newResource.GetDesiredState(tc.Obj)
+			if err != nil {
+				t.Fatalf("case %d expected '%v' got '%#v'", i+1, nil, err)
+			}
+
+			group := result.(*azureresource.Group)
+			if tc.ExpectedName != *group.Name {
+				t.Fatalf("case %d expected name '%s' got '%s'", i+1, tc.ExpectedName, *group.Name)
+			}
+			if tc.ExpectedLocation != *group.Location {
+				t.Fatalf("case %d expected location '%s' got '%s'", i+1, tc.ExpectedLocation, *group.Location)
+			}
+
+			if len(tc.ExpectedTags) != len(*group.Tags) {
+				t.Fatalf("case %d expected %d tags got %d", i+1, len(tc.ExpectedTags), len(*group.Tags))
+			}
+
+			for tag, val := range *group.Tags {
+				expectedVal, ok := tc.ExpectedTags[tag]
+				if !ok {
+					t.Fatalf("case %d tag '%s' was not expected", i+1, tag)
+				}
+
+				if *val != expectedVal {
+					t.Fatalf("case %d expected value '%s' for tag '%s' got '%s'", i+1, expectedVal, tag, *val)
+				}
+			}
+		}
+	}
+
+}
+func Test_Resource_ResourceGroup_GetCreateState(t *testing.T) {
+	testCases := []struct {
+		Obj                   interface{}
+		Cur                   interface{}
+		Des                   interface{}
+		ExpectedResourceGroup *azureresource.Group
+	}{
+		{
+			// Case 1. Current and desired states are the same. The resource
+			// group should not be created.
+			Obj: &azuretpr.CustomObject{
+				Spec: azuretpr.Spec{
+					Cluster: clustertpr.Spec{
+						Cluster: spec.Cluster{
+							ID: "5xchu",
+						},
+					},
+				},
+			},
+			Cur: &azureresource.Group{
+				Name: to.StringPtr("5xchu"),
+			},
+			Des: &azureresource.Group{
+				Name: to.StringPtr("5xchu"),
+			},
+			ExpectedResourceGroup: nil,
+		},
+		{
+			// Case 2. Current state is nil. The resource group should be
+			// created.
+			Obj: &azuretpr.CustomObject{
+				Spec: azuretpr.Spec{
+					Cluster: clustertpr.Spec{
+						Cluster: spec.Cluster{
+							ID: "5xchu",
+						},
+					},
+				},
+			},
+			Cur: nil,
+			Des: &azureresource.Group{
+				Name: to.StringPtr("5xchu"),
+			},
+			ExpectedResourceGroup: &azureresource.Group{
+				Name: to.StringPtr("5xchu"),
+			},
+		},
+	}
+
+	var err error
+	var newResource *Resource
+	{
+		resourceConfig := DefaultConfig()
+		resourceConfig.AzureConfig = client.DefaultAzureConfig()
+		resourceConfig.Logger = microloggertest.New()
+		newResource, err = New(resourceConfig)
+		if err != nil {
+			t.Fatalf("expected '%#v' got '%#v'", nil, err)
+		}
+	}
+
+	for i, tc := range testCases {
+		result, err := newResource.GetCreateState(tc.Obj, tc.Cur, tc.Des)
+		if err != nil {
+			t.Fatalf("case %d expected '%v' got '%#v'", i+1, nil, err)
+		}
+		if tc.ExpectedResourceGroup == nil {
+			if tc.ExpectedResourceGroup != result {
+				t.Fatalf("case %d expected '%#v' got '%#v'", i+1, tc.ExpectedResourceGroup, result)
+			}
+		} else {
+			name := *result.(*azureresource.Group).Name
+			expectedName := *tc.ExpectedResourceGroup.Name
+
+			if expectedName != name {
+				t.Fatalf("case %d expected '%s' got '%s'", i+1, expectedName, name)
+			}
+		}
+	}
+}
+
+func Test_Resource_ResourceGroup_GetDeleteState(t *testing.T) {
+	testCases := []struct {
+		Obj                   interface{}
+		Cur                   interface{}
+		Des                   interface{}
+		ExpectedResourceGroup *azureresource.Group
+	}{
+		{
+			// Case 1. Current and desired states are the same. The resource
+			// group should be deleted.
+			Obj: &azuretpr.CustomObject{
+				Spec: azuretpr.Spec{
+					Cluster: clustertpr.Spec{
+						Cluster: spec.Cluster{
+							ID: "5xchu",
+						},
+					},
+				},
+			},
+			Cur: &azureresource.Group{
+				Name: to.StringPtr("5xchu"),
+			},
+			Des: &azureresource.Group{
+				Name: to.StringPtr("5xchu"),
+			},
+			ExpectedResourceGroup: &azureresource.Group{
+				Name: to.StringPtr("5xchu"),
+			},
+		},
+		{
+			// Case 2. Current state is nil. The resource group should not be
+			// deleted.
+			Obj: &azuretpr.CustomObject{
+				Spec: azuretpr.Spec{
+					Cluster: clustertpr.Spec{
+						Cluster: spec.Cluster{
+							ID: "5xchu",
+						},
+					},
+				},
+			},
+			Cur: nil,
+			Des: &azureresource.Group{
+				Name: to.StringPtr("5xchu"),
+			},
+			ExpectedResourceGroup: nil,
+		},
+	}
+
+	var err error
+	var newResource *Resource
+	{
+		resourceConfig := DefaultConfig()
+		resourceConfig.AzureConfig = client.DefaultAzureConfig()
+		resourceConfig.Logger = microloggertest.New()
+		newResource, err = New(resourceConfig)
+		if err != nil {
+			t.Fatalf("expected '%#v' got '%#v'", nil, err)
+		}
+	}
+
+	for i, tc := range testCases {
+		result, err := newResource.GetDeleteState(tc.Obj, tc.Cur, tc.Des)
+		if err != nil {
+			t.Fatalf("case %d expected '%v' got '%#v'", i+1, nil, err)
+		}
+		if tc.ExpectedResourceGroup == nil {
+			if tc.ExpectedResourceGroup != result {
+				t.Fatalf("case %d expected '%#v' got '%#v'", i+1, tc.ExpectedResourceGroup, result)
+			}
+		} else {
+			name := *result.(*azureresource.Group).Name
+			expectedName := *tc.ExpectedResourceGroup.Name
+
+			if expectedName != name {
+				t.Fatalf("case %d expected '%s' got '%s'", i+1, expectedName, name)
+			}
+		}
+	}
+}
+
+func Test_Resource_ResourceGroup_GetUpdateState(t *testing.T) {
+	testCases := []struct {
+		Obj interface{}
+		Cur interface{}
+		Des interface{}
+	}{
+		{
+			// Case 1. Current and desired states are the same. No changes
+			// are returned as resource groups are not updated.
+			Obj: &azuretpr.CustomObject{
+				Spec: azuretpr.Spec{
+					Cluster: clustertpr.Spec{
+						Cluster: spec.Cluster{
+							ID: "5xchu",
+						},
+					},
+				},
+			},
+			Cur: &azureresource.Group{
+				Name: to.StringPtr("5xchu"),
+			},
+			Des: &azureresource.Group{
+				Name: to.StringPtr("5xchu"),
+			},
+		},
+		{
+			// Case 2. Current state is nil. No changes are returned as
+			// resource groups are not updated.
+			Obj: &azuretpr.CustomObject{
+				Spec: azuretpr.Spec{
+					Cluster: clustertpr.Spec{
+						Cluster: spec.Cluster{
+							ID: "5xchu",
+						},
+					},
+				},
+			},
+			Cur: &azureresource.Group{
+				Name: to.StringPtr("5xchu"),
+			},
+			Des: &azureresource.Group{
+				Name: to.StringPtr("5xchu"),
+			},
+		},
+	}
+
+	var err error
+	var newResource *Resource
+	{
+		resourceConfig := DefaultConfig()
+		resourceConfig.AzureConfig = client.DefaultAzureConfig()
+		resourceConfig.Logger = microloggertest.New()
+		newResource, err = New(resourceConfig)
+		if err != nil {
+			t.Fatalf("expected '%#v' got '%#v'", nil, err)
+		}
+	}
+
+	for i, tc := range testCases {
+		createState, deleteState, updateState, err := newResource.GetUpdateState(tc.Obj, tc.Cur, tc.Des)
+		if err != nil {
+			t.Fatalf("case %d expected '%v' got '%#v'", i+1, nil, err)
+		}
+		if createState != nil {
+			t.Fatalf("case %d expected nil create state got '%#v'", i+1, createState)
+		}
+		if deleteState != nil {
+			t.Fatalf("case %d expected nil delete state got '%#v'", i+1, createState)
+		}
+		if updateState != nil {
+			t.Fatalf("case %d expected nil update state got '%#v'", i+1, createState)
+		}
+	}
+}
