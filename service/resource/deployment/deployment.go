63efa7d00d869ed561b055181010bf1fbe527247
Ross Fairbanks <ross@giantswarm.io>
2017-09-08 16:13:24 +0200

    WIP: call main template

--- service/resource/deployment/deployment.go
+++ service/resource/deployment/deployment.go
@@ -11,7 +11,7 @@ import (
 const (
 	mainDeploymentName = "cluster-main-template"
 	mainTemplate       = "main.json"
-	templateBaseURI    = "https://raw.githubusercontent.com/giantswarm/azure-operator/master/service/arm_templates"
+	templateBaseURI    = "https://raw.githubusercontent.com/giantswarm/azure-operator/main-arm-template/service/arm_templates"
 	templateVersion    = "1.0.0.0"
 )
 
@@ -25,17 +25,17 @@ func newMainDeployment(cluster azuretpr.CustomObject) (Deployment, error) {
 	deployment := Deployment{
 		Name: mainDeploymentName,
 		Parameters: map[string]interface{}{
-			"clusterID":                     key.ClusterID(cluster),
-			"virtualNetworkCIDR":            cluster.Spec.Azure.VirtualNetwork.CIDR,
-			"masterSubnetCIDR":              cluster.Spec.Azure.VirtualNetwork.MasterSubnetCIDR,
-			"workerSubnetCIDR":              cluster.Spec.Azure.VirtualNetwork.WorkerSubnetCIDR,
+			"clusterID":          key.ClusterID(cluster),
+			"virtualNetworkCidr": cluster.Spec.Azure.VirtualNetwork.CIDR,
+			"masterSubnetCidr":   cluster.Spec.Azure.VirtualNetwork.MasterSubnetCIDR,
+			"workerSubnetCidr":   cluster.Spec.Azure.VirtualNetwork.WorkerSubnetCIDR,
 			"apiLoadBalancerCIDR":           "10.1.1.0/25",
 			"etcdLoadBalancerCIDR":          "10.1.1.128/25",
-			"ingressLoadBalancerCIDR":       "10.1.1.0/25",
-			"kubernetesAPISecurePort":       cluster.Spec.Cluster.Kubernetes.API.SecurePort,
-			"etcdPort":                      cluster.Spec.Cluster.Etcd.Port,
-			"kubernetesIngressSecurePort":   cluster.Spec.Cluster.Kubernetes.IngressController.SecurePort,
-			"kubernetesIngressInsecurePort": cluster.Spec.Cluster.Kubernetes.IngressController.InsecurePort,
+			"ingressLoadBalancerCIDR":       "10.1.2.0/25",
+			"kubernetesAPISecurePort":       fmt.Sprintf("%d", cluster.Spec.Cluster.Kubernetes.API.SecurePort),
+			"etcdPort":                      fmt.Sprintf("%d", cluster.Spec.Cluster.Etcd.Port),
+			"kubernetesIngressSecurePort":   fmt.Sprintf("%d", cluster.Spec.Cluster.Kubernetes.IngressController.SecurePort),
+			"kubernetesIngressInsecurePort": fmt.Sprintf("%d", cluster.Spec.Cluster.Kubernetes.IngressController.InsecurePort),
 		},
 		ResourceGroup:   key.ClusterID(cluster),
 		TemplateURI:     templateURI(mainTemplate),
