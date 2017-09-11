5a832241e9ed1ad84b0044026c10650a2980c403
Ross Fairbanks <ross@giantswarm.io>
2017-09-11 16:59:56 +0200

    Add extra params for calling the main deployment

--- service/resource/deployment/deployment.go
+++ service/resource/deployment/deployment.go
@@ -9,10 +9,13 @@ import (
 )
 
 const (
-	mainDeploymentName = "cluster-main-template"
-	mainTemplate       = "main.json"
-	templateBaseURI    = "https://raw.githubusercontent.com/giantswarm/azure-operator/main-arm-template/service/arm_templates"
-	templateVersion    = "1.0.0.0"
+	mainDeploymentName          = "cluster-main-template"
+	templateBaseURI             = "https://raw.githubusercontent.com/giantswarm/azure-operator/main-arm-template/service/arm_templates"
+	templateVersion             = "1.0.0.0"
+	mainTemplate                = "main.json"
+	clusterSetupTemplate        = "cluster_setup.json"
+	securityGroupsSetupTemplate = "security_groups_setup.json"
+	virtualNetworkSetupTemplate = "virtual_network_setup.json"
 )
 
 func getDeploymentNames() []string {
@@ -26,15 +29,20 @@ func newMainDeployment(cluster azuretpr.CustomObject) (Deployment, error) {
 		Name: mainDeploymentName,
 		Parameters: map[string]interface{}{
 			"clusterID":                     key.ClusterID(cluster),
+			"storageAccountType":            "Standard_LRS", // TODO Move to azuretpr
 			"virtualNetworkCidr":            cluster.Spec.Azure.VirtualNetwork.CIDR,
 			"masterSubnetCidr":              cluster.Spec.Azure.VirtualNetwork.MasterSubnetCIDR,
 			"workerSubnetCidr":              cluster.Spec.Azure.VirtualNetwork.WorkerSubnetCIDR,
-			"masterLoadBalancerCidr":        "10.1.1.0/25",
-			"workerLoadBalancerCidr":        "10.1.1.128/25",
+			"apiLoadBalancerCidr":           "10.1.1.0/25", // TODO Remove once LB resources are created,
+			"etcdLoadBalancerCidr":          "10.1.1.128/25",
+			"ingressLoadBalancerCidr":       "10.1.2.0/25",
 			"kubernetesAPISecurePort":       fmt.Sprintf("%d", cluster.Spec.Cluster.Kubernetes.API.SecurePort),
 			"etcdPort":                      fmt.Sprintf("%d", cluster.Spec.Cluster.Etcd.Port),
 			"kubernetesIngressSecurePort":   fmt.Sprintf("%d", cluster.Spec.Cluster.Kubernetes.IngressController.SecurePort),
 			"kubernetesIngressInsecurePort": fmt.Sprintf("%d", cluster.Spec.Cluster.Kubernetes.IngressController.InsecurePort),
+			"clusterSetupTemplate":          templateURI(clusterSetupTemplate),
+			"securityGroupsSetupTemplate":   templateURI(securityGroupsSetupTemplate),
+			"virtualNetworkSetupTemplate":   templateURI(virtualNetworkSetupTemplate),
 		},
 		ResourceGroup:   key.ClusterID(cluster),
 		TemplateURI:     templateURI(mainTemplate),
