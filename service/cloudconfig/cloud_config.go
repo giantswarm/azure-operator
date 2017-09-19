956f44fcc26d68342e0e2d08454333e11d7de463
Ross Fairbanks <ross@giantswarm.io>
2017-09-19 15:16:07 +0200

    Add GoDoc comments

--- service/cloudconfig/cloud_config.go
+++ service/cloudconfig/cloud_config.go
@@ -6,28 +6,38 @@ import (
 
 type MasterExtension struct{}
 
+// Files allows files to be injected into the master cloudconfig. It is
+// currently empty because the certificates will be stored in Key Vault.
 func (me *MasterExtension) Files() ([]k8scloudconfig.FileAsset, error) {
 	return nil, nil
 }
 
+// Units allows systemd units to be injected into the master cloudconfig.
+// TODO Add unit for downloading certificates from Key Vault.
 func (me *MasterExtension) Units() ([]k8scloudconfig.UnitAsset, error) {
 	return nil, nil
 }
 
+// VerbatimSections allows sections to be embedded in the master cloudconfig.
 func (me *MasterExtension) VerbatimSections() []k8scloudconfig.VerbatimSection {
 	return nil
 }
 
 type WorkerExtension struct{}
 
+// Files allows files to be injected into the worker cloudconfig. It is
+// currently empty because the certificates will be stored in Key Vault.
 func (we *WorkerExtension) Files() ([]k8scloudconfig.FileAsset, error) {
 	return nil, nil
 }
 
+// Units allows systemd units to be injected into the worker cloudconfig.
+// TODO Add unit for downloading certificates from Key Vault.
 func (we *WorkerExtension) Units() ([]k8scloudconfig.UnitAsset, error) {
 	return nil, nil
 }
 
+// VerbatimSections allows sections to be embedded in the worker cloudconfig.
 func (we *WorkerExtension) VerbatimSections() []k8scloudconfig.VerbatimSection {
 	return nil
 }
