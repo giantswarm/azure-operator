aeec4b7fc464110b798c1c16a7aee3e60dfebfca
cocotton <julien.parent.trudeau@gmail.com>
2017-11-05 19:40:15 -0500

    Update azuretpr vendor package

--- vendor/github.com/giantswarm/azuretpr/spec/azure/virtualnetwork/loadbalancer.go
+++ vendor/github.com/giantswarm/azuretpr/spec/azure/virtualnetwork/loadbalancer.go
@@ -1,10 +1,6 @@
 package virtualnetwork
 
 type LoadBalancer struct {
-	// APICIDR is the CIDR for the apiserver load balancer.
-	APICIDR string `json:"apiCidr" yaml:"apiCidr"`
 	// EtcdCidr is the CIDR for the etcd load balancer.
 	EtcdCIDR string `json:"etcdCidr" yaml:"etcdCidr"`
-	// IngressCidr is the CIDR for the ingress controller load balancer.
-	IngressCIDR string `json:"ingressCidr" yaml:"ingressCidr"`
 }
