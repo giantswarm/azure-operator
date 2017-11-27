440e3cdac5f7bf8b84a2a18793198b0cbe6a7247
Julien Parent-Trudeau <Cocotton@users.noreply.github.com>
2017-11-27 12:33:29 -0500

    secrets fetching script (#49)
    * Wait for auth backend and secrets to be created before querying them
    
    * Indentation fix

--- service/cloudconfig/template.go
+++ service/cloudconfig/template.go
@@ -6,6 +6,11 @@ const (
 	getKeyVaultSecretsFileName = "/opt/bin/get-keyvault-secrets"
 	getKeyVaultSecretsTemplate = `#!/bin/bash -e
 
+until $(curl --fail --output /dev/null http://localhost:50342/oauth2/token --data "resource=https://vault.azure.net" -H Metadata:true); do
+	printf 'Waiting auth backend'
+	sleep 5
+done
+
 KEY_VAULT_HOST={{ .VaultName }}.vault.azure.net
 AUTH_TOKEN=$(curl http://localhost:50342/oauth2/token --data "resource=https://vault.azure.net" -H Metadata:true | jq -r .access_token)
 API_VERSION=2016-10-01
@@ -14,7 +19,13 @@ mkdir -p /etc/kubernetes/ssl/calico
 mkdir -p /etc/kubernetes/ssl/etcd
 
 {{ range $secret := .Secrets }}
-curl https://$KEY_VAULT_HOST/secrets/{{ $secret.SecretName }}?api-version=$API_VERSION -H "Authorization: Bearer $AUTH_TOKEN" | jq -r .value > {{ $secret.FileName }} {{ end }}
+printf 'Waiting for secret {{ $secret.SecretName }}'
+until $(curl --fail --output /dev/null https://$KEY_VAULT_HOST/secrets/{{ $secret.SecretName }}?api-version=$API_VERSION -H "Authorization: Bearer $AUTH_TOKEN"); do
+	printf '.'
+	sleep 5
+done
+curl https://$KEY_VAULT_HOST/secrets/{{ $secret.SecretName }}?api-version=$API_VERSION -H "Authorization: Bearer $AUTH_TOKEN" | jq -r .value > {{ $secret.FileName }}
+{{ end }}
 
 chmod 0600 -R /etc/kubernetes/ssl/
 chown -R etcd:etcd /etc/kubernetes/ssl/etcd/
