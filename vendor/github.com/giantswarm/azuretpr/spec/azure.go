d0ede6dc85dc0e0008f682bcf8324c811608d74e
Julien Parent-Trudeau <Cocotton@users.noreply.github.com>
2017-12-01 11:37:04 -0500

    Ssh security rules (#62)
    * Pass the hostClusterCidr parameter to the main template
    
    * Add security rule allowing SSH traffic from host cluster cidr
    
    * Pass the hostClusterCidr parameter to the security_groups_setup template
    
    * Add test values
    
    * Remove empty line
    
    * Update azuretpr vendor package

--- vendor/github.com/giantswarm/azuretpr/spec/azure.go
+++ vendor/github.com/giantswarm/azuretpr/spec/azure.go
@@ -12,5 +12,6 @@ type Azure struct {
 	VirtualNetwork azure.VirtualNetwork `json:"virtualNetwork" yaml:"virtualNetwork"`
 	Masters        []azure.Node         `json:"masters" yaml:"masters"`
 	Workers        []azure.Node         `json:"workers" yaml:"workers"`
+	HostCluster    azure.HostCluster    `json:"hostCluster" yaml:"hostCluster"`
 	DNSZones       azure.DNSZones       `json:"dnsZones" yaml:"dnsZones"`
 }
