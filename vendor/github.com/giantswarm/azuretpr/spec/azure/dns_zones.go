b530ed47be1e9109926668740d4a53a93588ee9c
Roman Sokolkov <roman@giantswarm.io>
2017-12-03 19:41:13 +0100

    Vendor k8scc from azure-operator branch
    https://github.com/giantswarm/k8scloudconfig/commit/dbd8fda155202c75ac36a0115614d1cc3e90942d
    
    Cluster successfully provistioned with following:
    - https://github.com/giantswarm/azure-operator/pull/64
    - Manually added DNS entry for api - TODO
    - Manually added security rule to allow pod subnet - TODO

--- vendor/github.com/giantswarm/azuretpr/spec/azure/dns_zones.go
+++ vendor/github.com/giantswarm/azuretpr/spec/azure/dns_zones.go
@@ -1,11 +1,19 @@
 package azure
 
+// DNSZone points to a DNS Zone in Azure.
+type DNSZone struct {
+	// ResourceGroup is the resource group of the zone.
+	ResourceGroup string `json:"resourceGroup" yaml:"resourceGroup"`
+	// Name is the name of the zone.
+	Name string `json:"name" yaml:"name"`
+}
+
 // DNSZones contains the DNS Zones of the cluster.
 type DNSZones struct {
 	// API is the DNS Zone for the Kubernetes API.
-	API string `json:"api" yaml:"api"`
+	API DNSZone `json:"api" yaml:"api"`
 	// Etcd is the DNS Zone for the etcd cluster.
-	Etcd string `json:"etcd" yaml:"etcd"`
+	Etcd DNSZone `json:"etcd" yaml:"etcd"`
 	// Ingress is the DNS Zone for the Ingress resource, used for customer traffic.
-	Ingress string `json:"ingress" yaml:"ingress"`
+	Ingress DNSZone `json:"ingress" yaml:"ingress"`
 }
