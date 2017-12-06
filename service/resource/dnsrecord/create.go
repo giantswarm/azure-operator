4ac5163b2d4ccb546f9dd405b9ce5ae917e3dba9
Paweł Kopiczko <kopiczko@gmail.com>
2017-12-06 13:36:11 +0100

    split files

---
+++ service/resource/dnsrecord/create.go
@@ -0,0 +1,27 @@
+package dnsrecord
+
+import (
+	"context"
+
+	"github.com/giantswarm/azuretpr"
+	"github.com/giantswarm/microerror"
+)
+
+// ApplyCreateChange is never called. We do not like it. It is not idempotent.
+func (r *Resource) ApplyCreateChange(ctx context.Context, obj, change interface{}) error {
+	o, err := toCustomObject(obj)
+	if err != nil {
+		return microerror.Maskf(err, "creating DNS records in host cluster")
+	}
+
+	c, err := toDNSRecords(change)
+	if err != nil {
+		return microerror.Maskf(err, "creating DNS records in host cluster")
+	}
+
+	return r.applyCreateChange(ctx, o, c)
+}
+
+func (r *Resource) applyCreateChange(ctx context.Context, obj azuretpr.CustomObject, change dnsRecords) error {
+	return nil
+}
