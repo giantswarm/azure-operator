c9577286f41850eec38b73b99569fd97a59a0ea2
Roman Sokolkov <roman@giantswarm.io>
2018-01-04 18:44:41 +0100

    Fix calicoSubnetCidr key

--- service/key/key.go
+++ service/key/key.go
@@ -26,7 +26,7 @@ func AzureCloudType(customObject providerv1alpha1.AzureConfig) string {
 
 // CalicoSubnetCidr returns subnet for Calico in CIDR notation format.
 func CalicoSubnetCidr(customObject providerv1alpha1.AzureConfig) string {
-	return fmt.Sprintf("%s/%s", customObject.Spec.Cluster.Calico.Subnet, customObject.Spec.Cluster.Calico.CIDR)
+	return fmt.Sprintf("%s/%d", customObject.Spec.Cluster.Calico.Subnet, customObject.Spec.Cluster.Calico.CIDR)
 }
 
 // ClusterCustomer returns the customer ID for this cluster.
