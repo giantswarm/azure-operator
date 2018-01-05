b2d267cba9cb849491beb0c114903c1b9b5bb6b7
Roman Sokolkov <rsokolkov@gmail.com>
2018-01-05 14:29:51 +0100

    Network imporovements for Calico (#92)
    * Network imporovements for Calico
    
    - allow ip forwarding
    - add security rules to allow pods/calico traffic
    
    * Fix ingress load balancer backend port
    
    * Fix tests
    
    * Upgrade Calico to 3.0.1
    
    * Fix calico temlate
    
    * Pass virtualNetworkCIDR to sec group template
    
    * Fix calicoSubnetCidr key
    
    * dont pass calicoSubnetCidr
    
    * dont pass calicoSubnetCidr to the right template

--- service/key/key.go
+++ service/key/key.go
@@ -24,6 +24,11 @@ func AzureCloudType(customObject providerv1alpha1.AzureConfig) string {
 	return defaultAzureCloudType
 }
 
+// CalicoSubnetCidr returns subnet for Calico in CIDR notation format.
+func CalicoSubnetCidr(customObject providerv1alpha1.AzureConfig) string {
+	return fmt.Sprintf("%s/%d", customObject.Spec.Cluster.Calico.Subnet, customObject.Spec.Cluster.Calico.CIDR)
+}
+
 // ClusterCustomer returns the customer ID for this cluster.
 func ClusterCustomer(customObject providerv1alpha1.AzureConfig) string {
 	return customObject.Spec.Cluster.Customer.ID
