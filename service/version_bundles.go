c03b34ebaf1beb2a77d25a13d5564f55c65a71b6
Paweł Kopiczko <kopiczko@gmail.com>
2018-01-09 20:12:28 +0100

    add changelog

--- service/version_bundles.go
+++ service/version_bundles.go
@@ -9,7 +9,38 @@ import (
 func newVersionBundles() []versionbundle.Bundle {
 	return []versionbundle.Bundle{
 		{
-			Changelogs: []versionbundle.Changelog{},
+			Changelogs: []versionbundle.Changelog{
+				{
+					Component:   "kubernetes",
+					Description: "enable encryption at rest",
+					Kind:        versionbundle.KindAdded,
+				},
+				{
+					Component:   "kubernetes",
+					Description: "update to version 1.9.0",
+					Kind:        versionbundle.KindChanged,
+				},
+				{
+					Component:   "kubernetes",
+					Description: "use vanilla (previously coreos) hyperkube image",
+					Kind:        versionbundle.KindChanged,
+				},
+				{
+					Component:   "calico",
+					Description: "update to version 3.0.1",
+					Kind:        versionbundle.KindChanged,
+				},
+				{
+					Component:   "calico",
+					Description: "remove calico-ipip-pinger",
+					Kind:        versionbundle.KindRemoved,
+				},
+				{
+					Component:   "calico",
+					Description: "remove calico-node-controller",
+					Kind:        versionbundle.KindRemoved,
+				},
+			},
 			Components: []versionbundle.Component{
 				{
 					Name:    "calico",
