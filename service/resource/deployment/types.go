27993e00080d737fa54f8159957c653d02eb83a2
Paweł Kopiczko <kopiczko@gmail.com>
2018-01-10 08:53:35 +0100

    deployment: bake-in OS version
    This is step towards encapsulating OS version into the version bundle.

---
+++ service/resource/deployment/types.go
@@ -0,0 +1,45 @@
+package deployment
+
+import providerv1alpha1 "github.com/giantswarm/apiextensions/pkg/apis/provider/v1alpha1"
+
+func newNodes(azureNodes []providerv1alpha1.AzureConfigSpecAzureNode) []node {
+	var ns []node
+	for _, n := range azureNodes {
+		ns = append(ns, newNode(n))
+	}
+	return ns
+}
+
+type node struct {
+	providerv1alpha1.AzureConfigSpecAzureNode
+
+	// OSImage is the vm OS image object
+	OSImage nodeOSImage `json:"osImage" yaml:"osImage"`
+}
+
+func newNode(azureNode providerv1alpha1.AzureConfigSpecAzureNode) node {
+	return node{
+		AzureConfigSpecAzureNode: azureNode,
+		OSImage:                  newNodeOSImage(),
+	}
+}
+
+type nodeOSImage struct {
+	// Offer is the image offered by the publisher (e.g. CoreOS)
+	Offer string `json:"offer" yaml:"offer"`
+	// Publisher is the image publisher (e.g GiantSwarm)
+	Publisher string `json:"publisher" yaml:"publisher"`
+	// SKU is the image SKU (e.g. Alpha)
+	SKU string `json:"sku" yaml:"sku"`
+	// Version is the image version (e.g. 1465.7.0)
+	Version string `json:"version" yaml:"version"`
+}
+
+func newNodeOSImage() nodeOSImage {
+	return nodeOSImage{
+		Offer:     "CoreOS",
+		Publisher: "CoreOS",
+		Sku:       "Stable",
+		Version:   "1465.7.0",
+	}
+}
