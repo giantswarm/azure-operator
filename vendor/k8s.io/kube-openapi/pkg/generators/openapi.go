46094ec32fbf65a91b35941e4891f005b0616253
Tim Schindler <xh3b4sd@gmail.com>
2018-02-28 17:41:31 +0100

    updated vendor deps

--- vendor/k8s.io/kube-openapi/pkg/generators/openapi.go
+++ vendor/k8s.io/kube-openapi/pkg/generators/openapi.go
@@ -353,7 +353,18 @@ func (g openAPITypeWriter) generate(t *types.Type) error {
 			return nil
 		}
 		if hasOpenAPIDefinitionMethods(t) {
-			g.Do("common.OpenAPIDefinition{\n"+
+			// Since this generated snippet is part of a map:
+			//
+			//		map[string]common.OpenAPIDefinition: {
+			//			"TYPE_NAME": {
+			//				Schema: spec.Schema{ ... },
+			//			},
+			//		}
+			//
+			// For compliance with gofmt -s it's important we elide the
+			// struct type. The type is implied by the map and will be
+			// removed otherwise.
+			g.Do("{\n"+
 				"Schema: spec.Schema{\n"+
 				"SchemaProps: spec.SchemaProps{\n"+
 				"Type:$.type|raw${}.OpenAPISchemaType(),\n"+
@@ -623,6 +634,8 @@ func (g openAPITypeWriter) generateSliceProperty(t *types.Type) error {
 		return fmt.Errorf("please add type %v to getOpenAPITypeFormat function", elemType)
 	case types.Struct:
 		g.generateReferenceProperty(elemType)
+	case types.Slice, types.Array:
+		g.generateSliceProperty(elemType)
 	default:
 		return fmt.Errorf("slice Element kind %v is not supported in %v", elemType.Kind, t)
 	}
