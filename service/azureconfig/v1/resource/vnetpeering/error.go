211b976ad1d5e9798c30d15adbb11fd8faf86528
Theo Brigitte <theo.brigitte@gmail.com>
2018-03-14 16:43:44 +0100

    follow errors styling

--- service/azureconfig/v1/resource/vnetpeering/error.go
+++ service/azureconfig/v1/resource/vnetpeering/error.go
@@ -4,15 +4,16 @@ import (
 	"github.com/giantswarm/microerror"
 )
 
-var (
-	invalidConfigError = microerror.New("invalid config")
-	wrongTypeError     = microerror.New("wrong type")
-)
+var invalidConfigError = microerror.New("invalid config")
 
+// IsInvalidConfig asserts invalidConfigError.
 func IsInvalidConfig(err error) bool {
 	return microerror.Cause(err) == invalidConfigError
 }
 
+var wrongTypeError = microerror.New("wrong type")
+
+// IsWrongTypeError asserts wrongTypeError.
 func IsWrongTypeError(err error) bool {
 	return microerror.Cause(err) == wrongTypeError
 }
