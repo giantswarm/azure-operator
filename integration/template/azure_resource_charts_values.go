bad3d793554fc98345e87ce5c160286e71e1fe3f
Theo Brigitte <theo.brigitte@gmail.com>
2018-04-04 20:50:09 +0200

    add static CIDR for azure resource charts

--- integration/template/azure_resource_charts_values.go
+++ integration/template/azure_resource_charts_values.go
@@ -9,4 +9,8 @@ azure:
   location: ${AZURE_LOCATION}
   vmSizeMaster: "Standard_A1"
   vmSizeWorker: "Standard_A1"
+  calicoSubnetCIDR: "10.25.128.0/17"
+  cidr: "10.25.0.0/16"
+  masterSubnetCIDR: "10.25.0.0/24"
+  workerSubnetCIDR: "10.25.1.0/24"
 `
