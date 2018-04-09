568cded4915d3d923d5bc79f6d9baa314db5cb8d
Theo Brigitte <theo.brigitte@gmail.com>
2018-04-09 17:46:25 +0200

    setup.Setup signature

--- integration/setup/setup.go
+++ integration/setup/setup.go
@@ -40,7 +40,7 @@ func WrapTestMain(c *client.AzureClientSet, g *framework.Guest, h *framework.Hos
 }
 
 // Setup e2e testing environment.
-func Setup(c *client.AzureClientSet, g *framework.Guest, h *framework.Host, m *testing.M) error {
+func Setup(c *client.AzureClientSet, g *framework.Guest, h *framework.Host) error {
 	var v int
 	var err error
 
