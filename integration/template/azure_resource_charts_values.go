2e39201333e0cefeb8639709bffda4831e8ece5b
Theo Brigitte <theo.brigitte@gmail.com>
2018-04-10 18:27:10 +0200

    Revert "use smaller vmSize : Standard_DS1_v2"
    This reverts commit 76e95b1087d2bf5e9bd7d5c38fa4c4d07f332f14.

--- integration/template/azure_resource_charts_values.go
+++ integration/template/azure_resource_charts_values.go
@@ -5,8 +5,8 @@ commonDomain: ${COMMON_DOMAIN_GUEST_NO_K8S}
 commonDomainResourceGroup: ${COMMON_DOMAIN_RESOURCE_GROUP}
 azure:
   location: ${AZURE_LOCATION}
-  vmSizeMaster: "Standard_DS1_v2"
-  vmSizeWorker: "Standard_DS1_v2"
+  vmSizeMaster: "Standard_D2s_v3"
+  vmSizeWorker: "Standard_D2s_v3"
   calicoSubnetCIDR: ${AZURE_CALICO_SUBNET_CIDR}
   cidr: ${AZURE_CIDR}
   masterSubnetCIDR: ${AZURE_MASTER_SUBNET_CIDR}
