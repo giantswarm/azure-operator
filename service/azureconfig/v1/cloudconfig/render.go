aec6e4e56e3306c64e8a17dd557c9249eff47fe6
Paweł Kopiczko <kopiczko@gmail.com>
2018-04-12 12:49:21 +0200

    use k8scc 3.2.5 with 1.10

--- service/azureconfig/v1/cloudconfig/render.go
+++ service/azureconfig/v1/cloudconfig/render.go
@@ -2,7 +2,7 @@ package cloudconfig
 
 import (
 	"github.com/giantswarm/certs"
-	k8scloudconfig "github.com/giantswarm/k8scloudconfig/v_3_2_4"
+	k8scloudconfig "github.com/giantswarm/k8scloudconfig/v_3_2_5"
 	"github.com/giantswarm/microerror"
 )
 
