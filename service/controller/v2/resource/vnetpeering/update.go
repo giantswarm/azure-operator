cfa1068085509b227a2ae32af9eef581f3543f89
Roman Sokolkov <rsokolkov@gmail.com>
2018-04-30 12:40:56 +0200

    controller/v2: copy v1 (#198)

---
+++ service/controller/v2/resource/vnetpeering/update.go
@@ -0,0 +1,131 @@
+package vnetpeering
+
+import (
+	"context"
+
+	providerv1alpha1 "github.com/giantswarm/apiextensions/pkg/apis/provider/v1alpha1"
+	"github.com/giantswarm/azure-operator/service/controller/v1/key"
+	"github.com/giantswarm/microerror"
+	"github.com/giantswarm/operatorkit/controller"
+
+	"github.com/Azure/azure-sdk-for-go/services/network/mgmt/2017-09-01/network"
+)
+
+// NewUpdatePatch provide a controller.Patch holding the needed network.VirtualNetworkPeering update to have current comply with desired.
+func (r Resource) NewUpdatePatch(ctx context.Context, azureConfig, current, desired interface{}) (*controller.Patch, error) {
+	a, err := key.ToCustomObject(azureConfig)
+	if err != nil {
+		return nil, microerror.Maskf(err, "NewUpdatePatch")
+	}
+	c, err := toVnetPeering(current)
+	if err != nil {
+		return nil, microerror.Maskf(err, "NewUpdatePatch")
+	}
+	d, err := toVnetPeering(desired)
+	if err != nil {
+		return nil, microerror.Maskf(err, "NewUpdatePatch")
+	}
+
+	patch, err := r.newUpdatePatch(ctx, a, c, d)
+	if err != nil {
+		return nil, microerror.Maskf(err, "NewUpdatePatch")
+	}
+
+	return patch, nil
+}
+
+func (r Resource) newUpdatePatch(ctx context.Context, azureConfig providerv1alpha1.AzureConfig, current, desired network.VirtualNetworkPeering) (*controller.Patch, error) {
+	patch := controller.NewPatch()
+
+	change, err := r.newUpdateChange(ctx, azureConfig, current, desired)
+	if err != nil {
+		return nil, microerror.Maskf(err, "newUpdatePatch")
+	}
+
+	patch.SetUpdateChange(change)
+
+	return patch, nil
+}
+
+func (r Resource) newUpdateChange(ctx context.Context, azureConfig providerv1alpha1.AzureConfig, current, desired network.VirtualNetworkPeering) (network.VirtualNetworkPeering, error) {
+	var change network.VirtualNetworkPeering
+
+	if needUpdate(current, desired) {
+		change = desired
+	}
+
+	return change, nil
+}
+
+// needUpdate determine if current needs to be updated in order to comply with desired.
+// Following properties are compared (and must be present in desired)
+//     Name
+//     VirtualNetworkPeeringPropertiesFormat.AllowVirtualNetworkAccess
+//     VirtualNetworkPeeringPropertiesFormat.RemoteVirtualNetwork.ID
+func needUpdate(current, desired network.VirtualNetworkPeering) bool {
+	if current.Name == nil || *current.Name != *desired.Name {
+		return true
+	}
+
+	if current.VirtualNetworkPeeringPropertiesFormat == nil {
+		return true
+	}
+
+	if current.VirtualNetworkPeeringPropertiesFormat.AllowVirtualNetworkAccess == nil ||
+		*current.VirtualNetworkPeeringPropertiesFormat.AllowVirtualNetworkAccess != *desired.VirtualNetworkPeeringPropertiesFormat.AllowVirtualNetworkAccess {
+		return true
+	}
+
+	if current.VirtualNetworkPeeringPropertiesFormat.RemoteVirtualNetwork == nil ||
+		current.VirtualNetworkPeeringPropertiesFormat.RemoteVirtualNetwork.ID == nil ||
+		*current.VirtualNetworkPeeringPropertiesFormat.RemoteVirtualNetwork.ID != *desired.VirtualNetworkPeeringPropertiesFormat.RemoteVirtualNetwork.ID {
+		return true
+	}
+
+	if current.VirtualNetworkPeeringPropertiesFormat.PeeringState == network.Disconnected {
+		return true
+	}
+
+	return false
+}
+
+// ApplyUpdateChange perform the host cluster virtual network peering update against azure.
+func (r Resource) ApplyUpdateChange(ctx context.Context, azureConfig, change interface{}) error {
+	a, err := key.ToCustomObject(azureConfig)
+	if err != nil {
+		return microerror.Maskf(err, "ApplyUpdateChange")
+	}
+	c, err := toVnetPeering(change)
+	if err != nil {
+		return microerror.Maskf(err, "ApplyUpdateChange")
+	}
+
+	err = r.applyUpdateChange(ctx, a, c)
+	if err != nil {
+		return microerror.Maskf(err, "ApplyUpdateChange")
+	}
+
+	return nil
+}
+
+func (r *Resource) applyUpdateChange(ctx context.Context, azureConfig providerv1alpha1.AzureConfig, change network.VirtualNetworkPeering) error {
+	r.logger.LogCtx(ctx, "level", "debug", "message", "ensure host vnet peering")
+
+	vnetPeeringClient, err := r.getVnetPeeringClient()
+	if err != nil {
+		return microerror.Maskf(err, "ensure host vnet peering")
+	}
+
+	if isVNetPeeringEmpty(change) {
+		r.logger.LogCtx(ctx, "level", "debug", "message", "ensure host vnet peering: already ensured")
+		return nil
+	}
+
+	_, err = vnetPeeringClient.CreateOrUpdate(ctx, r.azure.HostCluster.ResourceGroup, r.azure.HostCluster.VirtualNetwork, *change.Name, change)
+	if err != nil {
+		return microerror.Maskf(err, "ensure host vnet peering %#v", change)
+	}
+
+	r.logger.LogCtx(ctx, "level", "debug", "message", "ensure host vnet peering: created")
+	return nil
+}
---
+++ service/controller/v2/version_bundle.go
@@ -0,0 +1,55 @@
+package v1
+
+import (
+	"time"
+
+	"github.com/giantswarm/versionbundle"
+)
+
+func VersionBundle() versionbundle.Bundle {
+	return versionbundle.Bundle{
+		Changelogs: []versionbundle.Changelog{
+			{
+				Component:   "azure-operator",
+				Description: "Added first release.",
+				Kind:        versionbundle.KindAdded,
+			},
+		},
+		Components: []versionbundle.Component{
+			{
+				Name:    "calico",
+				Version: "3.0.1",
+			},
+			{
+				Name:    "containerlinux",
+				Version: "1688.5.3",
+			},
+			{
+				Name:    "docker",
+				Version: "17.12.1",
+			},
+			{
+				Name:    "etcd",
+				Version: "3.3.1",
+			},
+			{
+				Name:    "coredns",
+				Version: "1.0.6",
+			},
+			{
+				Name:    "kubernetes",
+				Version: "1.10.1",
+			},
+			{
+				Name:    "nginx-ingress-controller",
+				Version: "0.12.0",
+			},
+		},
+		Dependencies: []versionbundle.Dependency{},
+		Deprecated:   false,
+		Name:         "azure-operator",
+		Time:         time.Date(2018, time.January, 7, 8, 35, 0, 0, time.UTC),
+		Version:      "0.1.0",
+		WIP:          false,
+	}
+}
