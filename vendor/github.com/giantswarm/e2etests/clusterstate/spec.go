f639594905109a1fb7900e7631f3f51937a20077
Ross Fairbanks <ross@giantswarm.io>
2018-06-20 10:26:23 +0200

    dep: updated e2etests

---
+++ vendor/github.com/giantswarm/e2etests/clusterstate/spec.go
@@ -0,0 +1,23 @@
+package clusterstate
+
+import (
+	"context"
+)
+
+type Interface interface {
+	// Test executes the cluster state test using the configured provider
+	// implementation. The provider implementation has to be aware of the guest
+	// cluster it has to act against. The test processes the following steps to
+	// ensure scaling works.
+	//
+	//  - Install test app.
+	//  - Wait for cluster to be ready.
+	//  - Reboot master node.
+	//  - Wait for API to be down.
+	//  - Wait for cluster to be ready.
+	//  - Replace master node.
+	//  - Wait for API to be down.
+	//  - Wait for cluster to be ready.
+	//
+	Test(ctx context.Context) error
+}
