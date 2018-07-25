8cd4fbc1a01ab1c8dcd391b15d1bb72dbd56a77c
Tim Schindler <xh3b4sd@gmail.com>
2018-07-25 12:53:57 +0200

    aligns logging based on recent advances

--- service/controller/v3/resource/instance/create.go
+++ service/controller/v3/resource/instance/create.go
@@ -38,7 +38,8 @@ func (r *Resource) EnsureCreated(ctx context.Context, obj interface{}) error {
 		err = cc.Validate()
 		if err != nil {
 			r.logger.LogCtx(ctx, "level", "debug", "message", "missing dispatched output values in controller context")
-			r.logger.LogCtx(ctx, "level", "debug", "message", "canceling resource for custom object")
+			r.logger.LogCtx(ctx, "level", "debug", "message", "canceling resource")
+
 			return nil
 		}
 	}
@@ -54,7 +55,7 @@ func (r *Resource) EnsureCreated(ctx context.Context, obj interface{}) error {
 			r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("deployment is in state '%s'", s))
 
 			if !key.IsFinalProvisioningState(s) {
-				r.logger.LogCtx(ctx, "level", "debug", "message", "canceling resource for custom object")
+				r.logger.LogCtx(ctx, "level", "debug", "message", "canceling resource")
 				return nil
 			}
 		}
