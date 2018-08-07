8d6fe7ecc69702fbcaec9b643f281628a4f4a49a
Roma Sokolkov <rsokolkov@gmail.com>
2018-08-07 07:01:33 +0200

    controller/v2patch1: copy v2 (#312)

---
+++ service/controller/v2patch1/resource/instance/resource.go
@@ -0,0 +1,72 @@
+package instance
+
+import (
+	"github.com/Azure/azure-sdk-for-go/services/compute/mgmt/2018-04-01/compute"
+	"github.com/giantswarm/microerror"
+	"github.com/giantswarm/micrologger"
+
+	"github.com/giantswarm/azure-operator/client"
+	"github.com/giantswarm/azure-operator/service/controller/setting"
+)
+
+const (
+	Name = "instancev2"
+)
+
+type Config struct {
+	Logger micrologger.Logger
+
+	Azure       setting.Azure
+	AzureConfig client.AzureClientSetConfig
+}
+
+type Resource struct {
+	logger micrologger.Logger
+
+	azure       setting.Azure
+	azureConfig client.AzureClientSetConfig
+}
+
+func New(config Config) (*Resource, error) {
+	if config.Logger == nil {
+		return nil, microerror.Maskf(invalidConfigError, "%T.Logger must not be empty", config)
+	}
+
+	if err := config.Azure.Validate(); err != nil {
+		return nil, microerror.Maskf(invalidConfigError, "%T.Azure.%s", config, err)
+	}
+	if err := config.AzureConfig.Validate(); err != nil {
+		return nil, microerror.Maskf(invalidConfigError, "%T.AzureConfig.%s", config, err)
+	}
+
+	r := &Resource{
+		logger: config.Logger,
+
+		azure:       config.Azure,
+		azureConfig: config.AzureConfig,
+	}
+
+	return r, nil
+}
+
+func (r *Resource) Name() string {
+	return Name
+}
+
+func (r *Resource) getScaleSetsClient() (*compute.VirtualMachineScaleSetsClient, error) {
+	cs, err := client.NewAzureClientSet(r.azureConfig)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	return cs.VirtualMachineScaleSetsClient, nil
+}
+
+func (r *Resource) getVMsClient() (*compute.VirtualMachineScaleSetVMsClient, error) {
+	cs, err := client.NewAzureClientSet(r.azureConfig)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	return cs.VirtualMachineScaleSetVMsClient, nil
+}
