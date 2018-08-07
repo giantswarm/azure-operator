2baa7a085940ab4a221d0b2cb6405d9873dab322
Tim Schindler <xh3b4sd@gmail.com>
2018-08-07 18:26:50 +0200

    adds e2e test for updates

--- integration/test/update/cluster_state_test.go
+++ integration/test/update/cluster_state_test.go
@@ -1,14 +1,14 @@
 // +build k8srequired
 
-package clusterstate
+package update
 
 import (
 	"context"
 	"testing"
 )
 
-func Test_ClusterState(t *testing.T) {
-	err := cs.Test(context.Background())
+func Test_Update(t *testing.T) {
+	err := u.Test(context.Background())
 	if err != nil {
 		t.Fatalf("%#v", err)
 	}
