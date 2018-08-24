4bbebc53547442d962d0e9ab78936a37fa632459
MarcelMue <marcel.mueller1@rwth-aachen.de>
2018-08-24 14:46:11 +0200

    set Cloud

--- service/controller/v3/resource_set.go
+++ service/controller/v3/resource_set.go
@@ -371,6 +371,7 @@ func NewResourceSet(config ResourceSetConfig) (*controller.ResourceSet, error) {
 		if err != nil {
 			return nil, microerror.Mask(err)
 		}
+		guestAzureClientSetConfig.Cloud = config.Azure.Cloud
 
 		azureClients, err := client.NewAzureClientSet(*guestAzureClientSetConfig)
 		if err != nil {
