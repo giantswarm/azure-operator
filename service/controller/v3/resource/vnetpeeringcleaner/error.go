7e4c91c9fca96073ed8d22f2766571351b846a86
Théo Brigitte <theo.brigitte@gmail.com>
2018-08-28 11:18:40 +0200

    Vnetpeering notfound (#335)
    * handle ResourceNotFound error in v3 and v4

--- service/controller/v3/resource/vnetpeeringcleaner/error.go
+++ service/controller/v3/resource/vnetpeeringcleaner/error.go
@@ -1,6 +1,8 @@
 package vnetpeeringcleaner
 
 import (
+	"github.com/Azure/go-autorest/autorest"
+	"github.com/Azure/go-autorest/autorest/azure"
 	"github.com/giantswarm/microerror"
 )
 
@@ -11,4 +13,35 @@ var invalidConfigError = &microerror.Error{
 // IsInvalidConfig asserts invalidConfigError.
 func IsInvalidConfig(err error) bool {
 	return microerror.Cause(err) == invalidConfigError
+}
+
+var notFoundError = &microerror.Error{
+	Kind: "notFoundError",
+}
+
+// IsNotFound asserts notFoundError.
+func IsNotFound(err error) bool {
+	if err == nil {
+		return false
+	}
+
+	c := microerror.Cause(err)
+
+	if c == notFoundError {
+		return true
+	}
+
+	{
+		dErr, ok := err.(autorest.DetailedError)
+		if ok {
+			rErr, ok := (dErr.Original).(*azure.RequestError)
+			if ok {
+				if rErr.ServiceError.Code == "ResourceNotFound" {
+					return true
+				}
+			}
+		}
+	}
+
+	return false
 }
