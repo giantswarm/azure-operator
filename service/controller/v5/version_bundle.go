f07c07dcba39d3e60765bde0dccb162c9f00000a
Federico Gimenez <federico.gimenez@gmail.com>
2018-09-28 10:45:14 +0200

    opsctl: copy v4 to v5

---
+++ service/controller/v5/version_bundle.go
@@ -0,0 +1,55 @@
+package v4
+
+import (
+	"github.com/giantswarm/versionbundle"
+)
+
+func VersionBundle() versionbundle.Bundle {
+	return versionbundle.Bundle{
+		Changelogs: []versionbundle.Changelog{
+			{
+				Component:   "kubernetes",
+				Description: "Updated to 1.11.1.",
+				Kind:        versionbundle.KindChanged,
+			},
+			{
+				Component:   "cloudconfig",
+				Description: "Calico updated to version 3.2.0",
+				Kind:        versionbundle.KindChanged,
+			},
+			{
+				Component:   "cloudconfig",
+				Description: "Removed CoreDNS related components (now managed by chart-operator).",
+				Kind:        versionbundle.KindRemoved,
+			},
+		},
+		Components: []versionbundle.Component{
+			{
+				Name:    "calico",
+				Version: "3.0.8",
+			},
+			{
+				Name:    "containerlinux",
+				Version: "1745.7.0",
+			},
+			{
+				Name:    "docker",
+				Version: "18.03.1",
+			},
+			{
+				Name:    "etcd",
+				Version: "3.3.3",
+			},
+			{
+				Name:    "coredns",
+				Version: "1.1.1",
+			},
+			{
+				Name:    "kubernetes",
+				Version: "1.11.1",
+			},
+		},
+		Name:    "azure-operator",
+		Version: "2.0.0",
+	}
+}
