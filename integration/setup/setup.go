18b75fa0e5b3a663e9095714b3072beafd89b176
Tim Schindler <xh3b4sd@gmail.com>
2018-10-12 18:40:30 +0200

    installs the azure-operator using the release package (#381)

--- integration/setup/setup.go
+++ integration/setup/setup.go
@@ -3,13 +3,17 @@
 package setup
 
 import (
+	"context"
 	"log"
 	"os"
 	"testing"
 	"time"
 
+	providerv1alpha1 "github.com/giantswarm/apiextensions/pkg/apis/provider/v1alpha1"
 	"github.com/giantswarm/backoff"
 	"github.com/giantswarm/e2e-harness/pkg/framework"
+	"github.com/giantswarm/e2e-harness/pkg/release"
+	"github.com/giantswarm/e2etemplates/pkg/chartvalues"
 	"github.com/giantswarm/e2etemplates/pkg/e2etemplates"
 	"github.com/giantswarm/microerror"
 	"k8s.io/api/core/v1"
@@ -69,36 +73,93 @@ func Setup(c Config) error {
 }
 
 // Resources install required charts.
-func Resources(c Config) error {
+func Resources(config Config) error {
 	var err error
 
 	{
-		err = c.Host.InstallBranchOperator("azure-operator", "azureconfig", template.AzureOperatorChartValues)
+		c := chartvalues.AzureOperatorConfig{
+			Provider: chartvalues.AzureOperatorConfigProvider{
+				Azure: chartvalues.AzureOperatorConfigProviderAzure{
+					Location: env.AzureLocation(),
+				},
+			},
+			Secret: chartvalues.AzureOperatorConfigSecret{
+				AzureOperator: chartvalues.AzureOperatorConfigSecretAzureOperator{
+					SecretYaml: chartvalues.AzureOperatorConfigSecretAzureOperatorSecretYaml{
+						Service: chartvalues.AzureOperatorConfigSecretAzureOperatorSecretYamlService{
+							Azure: chartvalues.AzureOperatorConfigSecretAzureOperatorSecretYamlServiceAzure{
+								ClientID:      env.AzureClientID(),
+								ClientSecret:  env.AzureClientSecret(),
+								SubsciptionID: env.AzureSubscriptionID(),
+								TenantID:      env.AzureTenantID(),
+								Template: chartvalues.AzureOperatorConfigSecretAzureOperatorSecretYamlServiceAzureTemplate{
+									URI: chartvalues.AzureOperatorConfigSecretAzureOperatorSecretYamlServiceAzureTemplateURI{
+										Version: env.CircleSHA(),
+									},
+								},
+							},
+						},
+					},
+				},
+				Registry: chartvalues.AzureOperatorConfigSecretRegistry{
+					PullSecret: chartvalues.AzureOperatorConfigSecretRegistryPullSecret{
+						DockerConfigJSON: env.RegistryPullSecret(),
+					},
+				},
+			},
+		}
+
+		values, err := chartvalues.NewAzureOperator(c)
 		if err != nil {
 			return microerror.Mask(err)
 		}
-		err = c.Host.InstallStableOperator("cert-operator", "certconfig", e2etemplates.CertOperatorChartValues)
-		if err != nil {
-			return microerror.Mask(err)
-		}
-		err = c.Host.InstallStableOperator("node-operator", "drainerconfig", e2etemplates.NodeOperatorChartValues)
+
+		err = config.Release.InstallOperator(context.Background(), "azure-operator", release.NewVersion(env.CircleSHA()), values, providerv1alpha1.NewAzureConfigCRD())
 		if err != nil {
 			return microerror.Mask(err)
 		}
 	}
 
 	{
-		err = c.Host.InstallCertResource()
+		c := chartvalues.CertOperatorConfig{
+			ClusterName:        env.ClusterID(),
+			CommonDomain:       env.CommonDomain(),
+			RegistryPullSecret: env.RegistryPullSecret(),
+			Vault: chartvalues.CertOperatorVault{
+				Token: env.VaultToken(),
+			},
+		}
+
+		values, err := chartvalues.NewCertOperator(c)
 		if err != nil {
 			return microerror.Mask(err)
 		}
 
-		err = installCredential(c.Host)
+		err = config.Host.InstallStableOperator("cert-operator", "certconfig", values)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+	}
+
+	{
+		err = config.Host.InstallStableOperator("node-operator", "drainerconfig", e2etemplates.NodeOperatorChartValues)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+	}
+
+	{
+		err = config.Host.InstallCertResource()
 		if err != nil {
 			return microerror.Mask(err)
 		}
 
-		err = c.Host.InstallResource("apiextensions-azure-config-e2e", template.AzureConfigE2EChartValues, ":stable")
+		err = installCredential(config.Host)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+
+		err = config.Host.InstallResource("apiextensions-azure-config-e2e", template.AzureConfigE2EChartValues, ":stable")
 		if err != nil {
 			return microerror.Mask(err)
 		}
