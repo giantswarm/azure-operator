0f9c05391af6d53a5781525949980b2bb133e63f
Timo Derstappen <teemow@gmail.com>
2018-10-27 16:42:52 +0200

    update v5 to k8s 1.12.2
    * update cloudconfig to 3.7.0
    * update container linux amis to 1855.5.0
    * edit changelog in versionbundle

--- service/controller/v5/resource/instance/types.go
+++ service/controller/v5/resource/instance/types.go
@@ -25,12 +25,12 @@ type nodeOSImage struct {
 	Version string `json:"version" yaml:"version"`
 }
 
-// newNodeOSImage provides OS information CoreOS 1745.7.0.
-func newNodeOSImageCoreOS_1745_7_0() nodeOSImage {
+// newNodeOSImage provides OS information for Container Linux
+func newNodeOSImageCoreOS() nodeOSImage {
 	return nodeOSImage{
 		Offer:     "CoreOS",
 		Publisher: "CoreOS",
 		SKU:       "Stable",
-		Version:   "1745.7.0",
+		Version:   "1855.5.0",
 	}
 }
