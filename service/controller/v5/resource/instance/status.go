e9eb7e430d8451af9d6dd68507dcbeb9b7203431
Tim Schindler <xh3b4sd@gmail.com>
2018-11-15 18:58:33 +0100

    fixes duplicated resource status from older versions (#407)

--- service/controller/v5/resource/instance/status.go
+++ service/controller/v5/resource/instance/status.go
@@ -1,6 +1,8 @@
 package instance
 
 import (
+	"strings"
+
 	providerv1alpha1 "github.com/giantswarm/apiextensions/pkg/apis/provider/v1alpha1"
 	"github.com/giantswarm/microerror"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -17,7 +19,7 @@ const (
 )
 
 func (r *Resource) deleteResourceStatus(customObject providerv1alpha1.AzureConfig, t string, s string) error {
-	customObject = computeForDeleteResourceStatus(customObject, t, s)
+	customObject = computeForDeleteResourceStatus(customObject, Name, t, s)
 
 	n := customObject.GetNamespace()
 	_, err := r.g8sClient.ProviderV1alpha1().AzureConfigs(n).UpdateStatus(&customObject)
@@ -85,46 +87,45 @@ func (r *Resource) setResourceStatus(customObject providerv1alpha1.AzureConfig, 
 	return nil
 }
 
-func computeForDeleteResourceStatus(customObject providerv1alpha1.AzureConfig, t string, s string) providerv1alpha1.AzureConfig {
-	var cleanup bool
-	{
+func computeForDeleteResourceStatus(customObject providerv1alpha1.AzureConfig, n string, t string, s string) providerv1alpha1.AzureConfig {
+	var allResources []providerv1alpha1.StatusClusterResource
+
+	for _, r := range customObject.Status.Cluster.Resources {
 		resourceStatus := providerv1alpha1.StatusClusterResource{
 			Conditions: nil,
-			Name:       Name,
+			Name:       n,
 		}
 
-		for i, r := range customObject.Status.Cluster.Resources {
-			if r.Name != Name {
+		// At this point we ensure resource statuses of other resources are
+		// preserved as they are. When we want to remove a status of resource A, but
+		// find a status of resource B, we keep B, because we were not asked to
+		// remove it.
+		if !unversionedNameMatches(r.Name, n) {
+			allResources = append(allResources, r)
+			continue
+		}
+
+		// At this point we have a status of a resource we were asked to filter for
+		// to some extend.
+		for _, c := range r.Conditions {
+			if c.Type == t && c.Status == s {
 				continue
 			}
 
-			for _, c := range r.Conditions {
-				if c.Type == t && c.Status == s {
-					continue
-				}
-				resourceStatus.Conditions = append(resourceStatus.Conditions, c)
-			}
+			resourceStatus.Conditions = append(resourceStatus.Conditions, c)
+		}
 
-			if len(resourceStatus.Conditions) > 0 {
-				customObject.Status.Cluster.Resources[i] = resourceStatus
-			} else {
-				cleanup = true
-			}
-
-			break
+		// At this point we add the filtered resource status to the list of all
+		// resource statuses we want to keep. In case the filter mechanism above
+		// decided to filter all conditions, we do not add it to the list of
+		// resource statuses, because we do not want to track resource statuses that
+		// are essentially empty.
+		if len(resourceStatus.Conditions) > 0 {
+			allResources = append(allResources, resourceStatus)
 		}
 	}
 
-	if cleanup {
-		var list []providerv1alpha1.StatusClusterResource
-		for _, r := range customObject.Status.Cluster.Resources {
-			if r.Name == Name {
-				continue
-			}
-			list = append(list, r)
-		}
-		customObject.Status.Cluster.Resources = list
-	}
+	customObject.Status.Cluster.Resources = allResources
 
 	return customObject
 }
@@ -159,4 +160,20 @@ func resourceStatusExists(customObject providerv1alpha1.AzureConfig, t string) b
 	}
 
 	return false
+}
+
+// unversionedName provides a comparable name without the exact version number
+// suffix. When the resource name should ever change, the mechanism of managing
+// the resource status has to be adapted accordingly. Otherwise unversionedName
+// will return an empty string and the mechanism will break in unexpected ways.
+func unversionedName(name string) string {
+	if !strings.HasPrefix(name, "instancev") {
+		return ""
+	}
+
+	return "instancev"
+}
+
+func unversionedNameMatches(a string, b string) bool {
+	return unversionedName(a) == unversionedName(b)
 }
