fb320ecbc3029aafb68f5622210ef74e376f25c0
Timo Derstappen <teemow@gmail.com>
2018-12-03 22:52:28 +0100

    v3patch2: copy v3patch1

---
+++ service/controller/v3patch2/credential/credential.go
@@ -0,0 +1,82 @@
+package credential
+
+import (
+	"github.com/giantswarm/microerror"
+	"k8s.io/api/core/v1"
+	apismetav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+
+	"github.com/giantswarm/azure-operator/client"
+	"github.com/giantswarm/azure-operator/service/controller/v3patch1/key"
+)
+
+const (
+	ClientIDKey       = "azure.azureoperator.clientid"
+	ClientSecretKey   = "azure.azureoperator.clientsecret"
+	SubscriptionIDKey = "azure.azureoperator.subscriptionid"
+	TenantIDKey       = "azure.azureoperator.tenantid"
+)
+
+func GetAzureConfig(k8sClient kubernetes.Interface, obj interface{}) (*client.AzureClientSetConfig, error) {
+	credential, err := readCredential(k8sClient, obj)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	config, err := getAzureConfig(credential)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	return config, nil
+}
+
+func getAzureConfig(credential *v1.Secret) (*client.AzureClientSetConfig, error) {
+	errorFormat := "%s not found in credential"
+
+	clientID, ok := credential.Data[ClientIDKey]
+	if !ok {
+		return nil, microerror.Maskf(invalidConfig, errorFormat, ClientIDKey)
+	}
+
+	clientSecret, ok := credential.Data[ClientSecretKey]
+	if !ok {
+		return nil, microerror.Maskf(invalidConfig, errorFormat, ClientSecretKey)
+	}
+
+	subscriptionID, ok := credential.Data[SubscriptionIDKey]
+	if !ok {
+		return nil, microerror.Maskf(invalidConfig, errorFormat, SubscriptionIDKey)
+	}
+
+	tenantID, ok := credential.Data[TenantIDKey]
+	if !ok {
+		return nil, microerror.Maskf(invalidConfig, errorFormat, TenantIDKey)
+	}
+
+	c := &client.AzureClientSetConfig{
+		ClientID:       string(clientID),
+		ClientSecret:   string(clientSecret),
+		SubscriptionID: string(subscriptionID),
+		TenantID:       string(tenantID),
+	}
+
+	return c, nil
+}
+
+func readCredential(k8sClient kubernetes.Interface, obj interface{}) (*v1.Secret, error) {
+	customObject, err := key.ToCustomObject(obj)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	credentialName := key.CredentialName(customObject)
+	credentialNamespace := key.CredentialNamespace(customObject)
+
+	credential, err := k8sClient.CoreV1().Secrets(credentialNamespace).Get(credentialName, apismetav1.GetOptions{})
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	return credential, nil
+}
