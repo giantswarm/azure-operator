b18ebabd30f291a3c8f628831bd8b011ac89c823
Théo Brigitte <theo.brigitte@gmail.com>
2018-12-04 15:33:27 +0100

    add vpn resource (#419)

---
+++ service/controller/v5/resource/vpn/create.go
@@ -0,0 +1,10 @@
+package vpn
+
+import (
+	"context"
+)
+
+// EnsureCreated ensures the resource is created.
+func (r *Resource) EnsureCreated(ctx context.Context, obj interface{}) error {
+	return nil
+}
