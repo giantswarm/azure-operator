4cd20e8a38c4c55d7769a5180b8a6b5ff9195ae1
calvix <rozsypalek.vaclav@gmail.com>
2019-01-22 16:41:35 +0100

    integrate node-operator v2 (#451)
    * test-node-operator-changes
    
    * everse v4
    
    * add Changelog

--- service/controller/v5/version_bundle.go
+++ service/controller/v5/version_bundle.go
@@ -22,6 +22,11 @@ func VersionBundle() versionbundle.Bundle {
 				Description: "Enabled admission plugins: DefaultTolerationSeconds, MutatingAdmissionWebhook, ValidatingAdmissionWebhook.",
 				Kind:        versionbundle.KindChanged,
 			},
+			{
+				Component:   "node-operator",
+				Description: "Improved node draining during updates and scaling.",
+				Kind:        versionbundle.KindChanged,
+			},
 		},
 		Components: []versionbundle.Component{
 			{
