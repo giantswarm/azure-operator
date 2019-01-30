83b1c2c455360b30ee27f903cf9033695947e8b2
Tim Schindler <xh3b4sd@gmail.com>
2019-01-30 18:37:40 +0100

    renames guest to tenant in logs

--- service/controller/v5/resource/vpnconnection/current.go
+++ service/controller/v5/resource/vpnconnection/current.go
@@ -10,7 +10,8 @@ import (
 	"github.com/giantswarm/azure-operator/service/controller/v5/key"
 )
 
-// GetCurrentState retrieve current vpn gateway connection from host to guest cluster.
+// GetCurrentState retrieve current vpn gateway connection from host to tenant
+// cluster.
 func (r *Resource) GetCurrentState(ctx context.Context, obj interface{}) (interface{}, error) {
 	customObject, err := key.ToCustomObject(obj)
 	if err != nil {
@@ -46,21 +47,21 @@ func (r *Resource) GetCurrentState(ctx context.Context, obj interface{}) (interf
 	}
 
 	{
-		r.logger.LogCtx(ctx, "level", "debug", "message", "finding guest vpn gateway connection")
+		r.logger.LogCtx(ctx, "level", "debug", "message", "finding tenant vpn gateway connection")
 
 		resourceGroup := key.ResourceGroupName(customObject)
 		connectionName := r.azure.HostCluster.ResourceGroup
 
 		g, err := r.getGuestVirtualNetworkGatewayConnection(ctx, resourceGroup, connectionName)
 		if IsVPNGatewayConnectionNotFound(err) {
-			r.logger.LogCtx(ctx, "level", "debug", "message", "did not find guest vpn gateway connection")
+			r.logger.LogCtx(ctx, "level", "debug", "message", "did not find tenant vpn gateway connection")
 			return c, nil
 		} else if err != nil {
 			return c, microerror.Mask(err)
 		}
 
 		if provisioningState := *g.ProvisioningState; !key.IsFinalProvisioningState(provisioningState) {
-			r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("guest vpn gateway connection is in state '%s'", provisioningState))
+			r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("tenant vpn gateway connection is in state '%s'", provisioningState))
 			resourcecanceledcontext.SetCanceled(ctx)
 			r.logger.LogCtx(ctx, "level", "debug", "message", "canceling resource")
 
@@ -69,7 +70,7 @@ func (r *Resource) GetCurrentState(ctx context.Context, obj interface{}) (interf
 
 		c.Guest = *g
 
-		r.logger.LogCtx(ctx, "level", "debug", "message", "found guest vpn gateway connection")
+		r.logger.LogCtx(ctx, "level", "debug", "message", "found tenant vpn gateway connection")
 	}
 
 	return c, nil
