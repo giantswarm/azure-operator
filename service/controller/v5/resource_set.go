3d8c72a6d54863f85b7b12061fe535926b1127fa
Paweł Kopiczko <kopiczko@gmail.com>
2019-02-12 08:35:51 +0000

    v5: drop migration resource (#459)
    If CRs were migrated in v4patch1 they don't need to be migrated again.
    
    ```diff
    diff --git a/./service/controller/v4patch1/resource/migration/resource.go b/./service/controller/v5/resource/migration/resource.go
    index 6de1ebdd..f4d1d99c 100644
    --- a/./service/controller/v4patch1/resource/migration/resource.go
    +++ b/./service/controller/v5/resource/migration/resource.go
    @@ -12,11 +12,11 @@ import (
            "github.com/giantswarm/micrologger"
            "github.com/giantswarm/operatorkit/controller/context/reconciliationcanceledcontext"
    
    -       "github.com/giantswarm/azure-operator/service/controller/v4patch1/key"
    +       "github.com/giantswarm/azure-operator/service/controller/v5/key"
     )
    
     const (
    -       name = "migrationv4patch1"
    +       name = "migrationv5"
    
            azureConfigNamespace             = "default"
            credentialSecretDefaultNamespace = "giantswarm"
    ```

--- service/controller/v5/resource_set.go
+++ service/controller/v5/resource_set.go
@@ -19,7 +19,6 @@ import (
 	"github.com/giantswarm/azure-operator/service/controller/v5/resource/dnsrecord"
 	"github.com/giantswarm/azure-operator/service/controller/v5/resource/endpoints"
 	"github.com/giantswarm/azure-operator/service/controller/v5/resource/instance"
-	"github.com/giantswarm/azure-operator/service/controller/v5/resource/migration"
 	"github.com/giantswarm/azure-operator/service/controller/v5/resource/namespace"
 	"github.com/giantswarm/azure-operator/service/controller/v5/resource/resourcegroup"
 	"github.com/giantswarm/azure-operator/service/controller/v5/resource/service"
@@ -135,19 +134,6 @@ func NewResourceSet(config ResourceSetConfig) (*controller.ResourceSet, error) {
 		}
 
 		statusResource, err = statusresource.NewResource(c)
-		if err != nil {
-			return nil, microerror.Mask(err)
-		}
-	}
-
-	var migrationResource controller.Resource
-	{
-		c := migration.Config{
-			G8sClient: config.G8sClient,
-			Logger:    config.Logger,
-		}
-
-		migrationResource, err = migration.New(c)
 		if err != nil {
 			return nil, microerror.Mask(err)
 		}
@@ -356,7 +342,6 @@ func NewResourceSet(config ResourceSetConfig) (*controller.ResourceSet, error) {
 
 	resources := []controller.Resource{
 		statusResource,
-		migrationResource,
 		namespaceResource,
 		serviceResource,
 		resourceGroupResource,
