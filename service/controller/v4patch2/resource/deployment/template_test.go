14ce15afa45ce8071f1e0f35f59ec0dfd92eefbc
Volodymyr Stoiko <me@volodymyrstoiko.com>
2019-02-12 11:49:01 +0200

    Add controller for release 2.0.2 (#460)

---
+++ service/controller/v4patch2/resource/deployment/template_test.go
@@ -0,0 +1,30 @@
+package deployment
+
+import (
+	"strings"
+	"testing"
+)
+
+func Test_TemplateURI(t *testing.T) {
+	uri := templateURI("dev", "worker.json")
+	euri := "https://raw.githubusercontent.com/giantswarm/azure-operator/dev/service/controller/v4patch2/arm_templates/worker.json"
+
+	if uri != euri {
+		t.Errorf("expected '%s' got '%s'", euri, uri)
+	}
+}
+
+func Test_BaseTemplateURI(t *testing.T) {
+	uri := baseTemplateURI("master")
+	euri := "https://raw.githubusercontent.com/giantswarm/azure-operator/master/service/controller/v4patch2/arm_templates/"
+
+	if uri != euri {
+		t.Errorf("expected '%s', got '%s'", euri, uri)
+	}
+
+	// Additionaly make sure base URI ends with slash. This is important.
+	// See main.json ARM template.
+	if !strings.HasSuffix(uri, "/") {
+		t.Errorf("expected '/' suffix, got '%s'", uri)
+	}
+}
