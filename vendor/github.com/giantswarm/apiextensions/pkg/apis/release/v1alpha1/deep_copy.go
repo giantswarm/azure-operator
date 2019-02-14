c9e6777a0f6b304b0e2ba7633130192b547f7198
Volodymyr Stoiko <me@volodymyrstoiko.com>
2019-02-14 15:36:08 +0200

    Vendor latest

--- vendor/github.com/giantswarm/apiextensions/pkg/apis/release/v1alpha1/deep_copy.go
+++ vendor/github.com/giantswarm/apiextensions/pkg/apis/release/v1alpha1/deep_copy.go
@@ -4,12 +4,27 @@ import (
 	"time"
 )
 
-// DeepCopyTime implements the deep copy logic for time.Time which the k8s
-// codegen is not able to generate out of the box.
-type DeepCopyTime struct {
+// DeepCopyDate is a date type designed to be validated with json-schema date
+// type.
+type DeepCopyDate struct {
 	time.Time
 }
 
-func (in *DeepCopyTime) DeepCopyInto(out *DeepCopyTime) {
-	*out = *in
+// UnmarshalJSON implements the json.Unmarshaler interface. The time is
+// expected to be a quoted string in yyyy-mm-dd format.
+func (d *DeepCopyDate) UnmarshalJSON(data []byte) error {
+	// Ignore null, like in the main JSON package.
+	if string(data) == "null" {
+		return nil
+	}
+
+	// Error masking is skipped here as this will go trough generated
+	// unmarshaling code.
+	var err error
+	d.Time, err = time.Parse(`"2006-01-02"`, string(data))
+	return err
+}
+
+func (d *DeepCopyDate) DeepCopyInto(out *DeepCopyDate) {
+	*out = *d
 }
