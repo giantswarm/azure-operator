625dc810f13d84a20cd6f758fa46ff2b60d42e66
Volodymyr Stoiko <me@volodymyrstoiko.com>
2019-02-21 18:33:47 +0200

    Add delete secret

--- service/controller/v5/resource/encryptionkey/delete.go
+++ service/controller/v5/resource/encryptionkey/delete.go
@@ -2,8 +2,27 @@ package encryptionkey
 
 import (
 	"context"
+
+	"github.com/giantswarm/microerror"
+	apismetav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"github.com/giantswarm/azure-operator/service/controller/v5/key"
 )
 
 func (r *Resource) EnsureDeleted(ctx context.Context, obj interface{}) error {
+	customObject, err := key.ToCustomObject(obj)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	r.logger.LogCtx(ctx, "level", "debug", "message", "deleting encryptionkey secret")
+
+	err = r.k8sClient.CoreV1().Secrets(key.CertificateEncryptionNamespace).Delete(key.CertificateEncryptionName(customObject), &apismetav1.DeleteOptions{})
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	r.logger.LogCtx(ctx, "level", "debug", "message", "deleted encryptionkey secret")
+
 	return nil
 }
