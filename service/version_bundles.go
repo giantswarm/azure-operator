7648aec8e437f922a5f412822218f8162318fe25
Timo Derstappen <teemow@gmail.com>
2019-03-12 11:46:58 +0100

    v5patch1 is not used in a release
    this version bundle was created due to a misunderstanding. no need to
    carry this along further.

--- service/version_bundles.go
+++ service/version_bundles.go
@@ -5,9 +5,8 @@ import (
 
 	"github.com/giantswarm/azure-operator/service/controller/v4patch1"
 	"github.com/giantswarm/azure-operator/service/controller/v4patch2"
-	v5 "github.com/giantswarm/azure-operator/service/controller/v5"
-	v5patch1 "github.com/giantswarm/azure-operator/service/controller/v5patch1"
-	v6 "github.com/giantswarm/azure-operator/service/controller/v6"
+	"github.com/giantswarm/azure-operator/service/controller/v5"
+	"github.com/giantswarm/azure-operator/service/controller/v6"
 )
 
 func NewVersionBundles() []versionbundle.Bundle {
@@ -16,7 +15,6 @@ func NewVersionBundles() []versionbundle.Bundle {
 	versionBundles = append(versionBundles, v4patch1.VersionBundle())
 	versionBundles = append(versionBundles, v4patch2.VersionBundle())
 	versionBundles = append(versionBundles, v5.VersionBundle())
-	versionBundles = append(versionBundles, v5patch1.VersionBundle())
 	versionBundles = append(versionBundles, v6.VersionBundle())
 
 	return versionBundles
