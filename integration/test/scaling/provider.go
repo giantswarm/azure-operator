a0041f6e673963cc093b2c02e605793093a3ce6b
Tim Schindler <xh3b4sd@gmail.com>
2019-04-02 11:30:39 +0200

    move scaling e2e provider implementation

---
+++ integration/test/scaling/provider.go
@@ -0,0 +1,135 @@
+// +build k8srequired
+
+package scaling
+
+import (
+	"context"
+	"encoding/json"
+
+	"github.com/giantswarm/e2e-harness/pkg/framework"
+	"github.com/giantswarm/microerror"
+	"github.com/giantswarm/micrologger"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+)
+
+type ProviderConfig struct {
+	GuestFramework *framework.Guest
+	HostFramework  *framework.Host
+	Logger         micrologger.Logger
+
+	ClusterID string
+}
+
+type Provider struct {
+	guestFramework *framework.Guest
+	hostFramework  *framework.Host
+	logger         micrologger.Logger
+
+	clusterID string
+}
+
+func NewProvider(config ProviderConfig) (*Provider, error) {
+	if config.GuestFramework == nil {
+		return nil, microerror.Maskf(invalidConfigError, "%T.GuestFramework must not be empty", config)
+	}
+	if config.HostFramework == nil {
+		return nil, microerror.Maskf(invalidConfigError, "%T.HostFramework must not be empty", config)
+	}
+	if config.Logger == nil {
+		return nil, microerror.Maskf(invalidConfigError, "%T.Logger must not be empty", config)
+	}
+
+	if config.ClusterID == "" {
+		return nil, microerror.Maskf(invalidConfigError, "%T.ClusterID must not be empty", config)
+	}
+
+	p := &Provider{
+		guestFramework: config.GuestFramework,
+		hostFramework:  config.HostFramework,
+		logger:         config.Logger,
+
+		clusterID: config.ClusterID,
+	}
+
+	return p, nil
+}
+
+func (p *Provider) AddWorker() error {
+	customObject, err := p.hostFramework.G8sClient().ProviderV1alpha1().AzureConfigs("default").Get(p.clusterID, metav1.GetOptions{})
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	patches := []Patch{
+		{
+			Op:    "add",
+			Path:  "/spec/azure/workers/-",
+			Value: customObject.Spec.Azure.Workers[0],
+		},
+	}
+
+	b, err := json.Marshal(patches)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	_, err = p.hostFramework.G8sClient().ProviderV1alpha1().AzureConfigs("default").Patch(p.clusterID, types.JSONPatchType, b)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	return nil
+}
+
+func (p *Provider) NumMasters() (int, error) {
+	customObject, err := p.hostFramework.G8sClient().ProviderV1alpha1().AzureConfigs("default").Get(p.clusterID, metav1.GetOptions{})
+	if err != nil {
+		return 0, microerror.Mask(err)
+	}
+
+	num := len(customObject.Spec.Azure.Masters)
+
+	return num, nil
+}
+
+func (p *Provider) NumWorkers() (int, error) {
+	customObject, err := p.hostFramework.G8sClient().ProviderV1alpha1().AzureConfigs("default").Get(p.clusterID, metav1.GetOptions{})
+	if err != nil {
+		return 0, microerror.Mask(err)
+	}
+
+	num := len(customObject.Spec.Azure.Workers)
+
+	return num, nil
+}
+
+func (p *Provider) RemoveWorker() error {
+	patches := []Patch{
+		{
+			Op:   "remove",
+			Path: "/spec/azure/workers/1",
+		},
+	}
+
+	b, err := json.Marshal(patches)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	_, err = p.hostFramework.G8sClient().ProviderV1alpha1().AzureConfigs("default").Patch(p.clusterID, types.JSONPatchType, b)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	return nil
+}
+
+func (p *Provider) WaitForNodes(ctx context.Context, num int) error {
+	err := p.guestFramework.WaitForNodesReady(ctx, num)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	return nil
+}
