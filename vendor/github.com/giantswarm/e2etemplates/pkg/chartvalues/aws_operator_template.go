f04df626bb0302fe1393e98eb2debe062e2b3f68
Tim Schindler <xh3b4sd@gmail.com>
2019-04-10 13:34:43 +0200

    update vendor deps

--- vendor/github.com/giantswarm/e2etemplates/pkg/chartvalues/aws_operator_template.go
+++ vendor/github.com/giantswarm/e2etemplates/pkg/chartvalues/aws_operator_template.go
@@ -36,6 +36,7 @@ const awsOperatorTemplate = `Installation:
         IncludeTags: true
         Route53:
           Enabled: true
+        RouteTableNames: '{{ .Provider.AWS.RouteTableNames }}'
         Encrypter: '{{ .Provider.AWS.Encrypter }}'
         TrustedAdvisor:
           Enabled: false
