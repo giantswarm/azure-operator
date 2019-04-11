46f893249a546d067e6306cbab33117c940dd2c9
Tim Schindler <xh3b4sd@gmail.com>
2019-04-11 11:23:05 +0200

    make error handling of encryption key deletion more graceful

--- service/controller/v6/resource/encryptionkey/delete.go
+++ service/controller/v6/resource/encryptionkey/delete.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/giantswarm/microerror"
+	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"github.com/giantswarm/azure-operator/service/controller/v6/key"
@@ -18,7 +19,9 @@ func (r *Resource) EnsureDeleted(ctx context.Context, obj interface{}) error {
 	r.logger.LogCtx(ctx, "level", "debug", "message", "deleting encryptionkey secret")
 
 	err = r.k8sClient.CoreV1().Secrets(key.CertificateEncryptionNamespace).Delete(key.CertificateEncryptionSecretName(customObject), &metav1.DeleteOptions{})
-	if err != nil {
+	if errors.IsNotFound(err) {
+		// fall through
+	} else if err != nil {
 		return microerror.Mask(err)
 	}
 
--- service/controller/v7/resource/encryptionkey/delete.go
+++ service/controller/v7/resource/encryptionkey/delete.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/giantswarm/microerror"
+	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"github.com/giantswarm/azure-operator/service/controller/v7/key"
@@ -15,14 +16,18 @@ func (r *Resource) EnsureDeleted(ctx context.Context, obj interface{}) error {
 		return microerror.Mask(err)
 	}
 
-	r.logger.LogCtx(ctx, "level", "debug", "message", "deleting encryptionkey secret")
+	{
+		r.logger.LogCtx(ctx, "level", "debug", "message", "deleting encryptionkey secret upon delete event")
 
-	err = r.k8sClient.CoreV1().Secrets(key.CertificateEncryptionNamespace).Delete(key.CertificateEncryptionSecretName(customObject), &metav1.DeleteOptions{})
-	if err != nil {
-		return microerror.Mask(err)
+		err = r.k8sClient.CoreV1().Secrets(key.CertificateEncryptionNamespace).Delete(key.CertificateEncryptionSecretName(customObject), &metav1.DeleteOptions{})
+		if errors.IsNotFound(err) {
+			r.logger.LogCtx(ctx, "level", "debug", "message", "encryptionkey secret already deleted")
+		} else if err != nil {
+			return microerror.Mask(err)
+		} else {
+			r.logger.LogCtx(ctx, "level", "debug", "message", "deleted encryptionkey secret upon delete event")
+		}
 	}
-
-	r.logger.LogCtx(ctx, "level", "debug", "message", "deleted encryptionkey secret")
 
 	return nil
 }
