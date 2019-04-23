eea5a49ffa08b18da0e5dca3cad6695216be31ef
Paweł Kopiczko <kopiczko@gmail.com>
2019-04-23 16:39:07 +0100

    [WIP] scrape BYOC accounts

--- service/collector/azure_ad.go
+++ service/collector/azure_ad.go
@@ -2,9 +2,7 @@ package collector
 
 import (
 	"context"
-	"fmt"
 
-	"github.com/Azure/azure-sdk-for-go/services/graphrbac/1.6/graphrbac"
 	"github.com/giantswarm/microerror"
 	"github.com/giantswarm/micrologger"
 	"github.com/prometheus/client_golang/prometheus"
@@ -15,36 +13,38 @@ import (
 )
 
 var (
-	AzureAdDesc *prometheus.Desc = prometheus.NewDesc(
-		prometheus.BuildFQName("azure_operator", "credentials", "info"),
+	azureADDesc *prometheus.Desc = prometheus.NewDesc(
+		prometheus.BuildFQName("azure_operator", "credentials", "expiration_timestamp"),
 		"Azure credentials informations.",
 		[]string{
-			"id",
-			"name",
-			"service_principal",
-			"expiration_timestamp",
+			"application_id",
+			"key_id",
 		},
 		nil,
 	)
 )
 
-type AzureAdConfig struct {
+type AzureADConfig struct {
 	K8sClient kubernetes.Interface
 	Logger    micrologger.Logger
 
-	AzureSetting             setting.Azure
+	AzureSetting setting.Azure
+	// EnvironmentName is the name of the Azure environment used to compute the
+	// azure.Environment type. See also
+	// https://godoc.org/github.com/Azure/go-autorest/autorest/azure#Environment.
+	EnvironmentName          string
 	HostAzureClientSetConfig client.AzureClientSetConfig
 }
 
-type AzureAd struct {
+type AzureAD struct {
 	k8sClient kubernetes.Interface
 	logger    micrologger.Logger
 
-	azureSetting             setting.Azure
+	environmentName          string
 	hostAzureClientSetConfig client.AzureClientSetConfig
 }
 
-func NewAzureAd(config AzureAdConfig) (*AzureAd, error) {
+func NewAzureAD(config AzureADConfig) (*AzureAD, error) {
 	if config.K8sClient == nil {
 		return nil, microerror.Maskf(invalidConfigError, "%T.K8sClient must not be empty", config)
 	}
@@ -52,56 +52,79 @@ func NewAzureAd(config AzureAdConfig) (*AzureAd, error) {
 		return nil, microerror.Maskf(invalidConfigError, "%T.Logger must not be empty", config)
 	}
 
-	if err := config.AzureSetting.Validate(); err != nil {
-		return nil, microerror.Maskf(invalidConfigError, "%T.AzureSetting.%s", config, err)
+	if config.EnvironmentName == "" {
+		return nil, microerror.Maskf(invalidConfigError, "%T.EnvironmentName must not be empty", config)
 	}
 	if err := config.HostAzureClientSetConfig.Validate(); err != nil {
 		return nil, microerror.Maskf(invalidConfigError, "%T.HostAzureClientSetConfig.%s", config, err)
 	}
 
-	a := &AzureAd{
+	a := &AzureAD{
 		k8sClient: config.K8sClient,
 		logger:    config.Logger,
 
-		azureSetting:             config.AzureSetting,
+		environmentName:          config.EnvironmentName,
 		hostAzureClientSetConfig: config.HostAzureClientSetConfig,
 	}
 
 	return a, nil
 }
 
-func (r *AzureAd) Collect(ch chan<- prometheus.Metric) error {
-	fmt.Print("ey")
+func (a *AzureAD) Collect(ch chan<- prometheus.Metric) error {
+	ctx := context.Background()
+
+	// TODO call for CP client set
+	clientSetConfigs, err := getAzureClientSetConfigs(a.k8sClient, a.environmentName)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	for _, c := range clientSetConfigs {
+		err := a.collectForClientSetConfig(ctx, ch, c)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+	}
 
 	return nil
 }
 
-func (r *AzureAd) Describe(ch chan<- *prometheus.Desc) error {
+func (a *AzureAD) Describe(ch chan<- *prometheus.Desc) error {
 	ch <- resourceGroupDesc
 
 	return nil
 }
 
-func (a *AzureAd) getApplicationsClient() (*graphrbac.ApplicationsClient, error) {
-	azureClients, err := client.NewAzureClientSet(a.hostAzureClientSetConfig)
+func (a *AzureAD) collectForClientSetConfig(ctx context.Context, ch chan<- prometheus.Metric, clientSetConfig *client.AzureClientSetConfig) error {
+	clientSet, err := client.NewAzureClientSet(*clientSetConfig)
 	if err != nil {
-		return nil, microerror.Mask(err)
+		return microerror.Mask(err)
 	}
 
-	return azureClients.ApplicationsClient, nil
-}
-
-func (a *AzureAd) getApplicationExpirationDate(ctx context.Context) (string, error) {
-	appClient, err := a.getApplicationsClient()
+	// TODO handle non-authorized.
+	result, err := clientSet.ApplicationsClient.ListPasswordCredentials(ctx, clientSetConfig.ClientID)
 	if err != nil {
-		return "", microerror.Mask(err)
+		return microerror.Mask(err)
 	}
 
-	appPasswords, err := appClient.ListPasswordCredentials(ctx, a.hostAzureClientSetConfig.ClientID)
-	if err != nil {
-		return "", microerror.Mask(err)
+	if result.Value == nil {
+		return nil
 	}
 
+	for _, credential := range *result.Value {
+		var keyID string
+		if credential.KeyID != nil {
+			keyID = *credential.KeyID
+		}
 
-	return , nil
+		ch <- prometheus.MustNewConstMetric(
+			azureADDesc,
+			prometheus.GaugeValue,
+			float64(credential.EndDate.Unix()),
+			clientSetConfig.ClientID,
+			keyID,
+		)
+	}
+
+	return nil
 }
--- service/collector/client.go
+++ service/collector/client.go
@@ -4,6 +4,7 @@ import (
 	"github.com/giantswarm/azure-operator/client"
 	"github.com/giantswarm/azure-operator/service/credential"
 	"github.com/giantswarm/microerror"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 )
@@ -16,26 +17,16 @@ const (
 	credentialLabelSelector = "app=credentiald"
 )
 
-// getClientSets fetches all unique Azure clients.
+// getClientSets fetches all unique Azure tenant clusters clients.
 func getClientSets(k8sClient kubernetes.Interface, environmentName string) ([]*client.AzureClientSet, error) {
-	credentialList, err := k8sClient.CoreV1().Secrets(credentialNamespace).List(metav1.ListOptions{
-		LabelSelector: credentialLabelSelector,
-	})
+	configs, err := getAzureClientSetConfigs(k8sClient, environmentName)
 	if err != nil {
 		return nil, microerror.Mask(err)
 	}
 
-	clientSets := []*client.AzureClientSet{}
-
-	for _, secret := range credentialList.Items {
-		config, err := credential.GetAzureConfig(k8sClient, secret.Name, secret.Namespace)
-		if err != nil {
-			return nil, microerror.Mask(err)
-		}
-
-		config.EnvironmentName = environmentName
-
-		clientSet, err := client.NewAzureClientSet(*config)
+	var clientSets []*client.AzureClientSet
+	for _, c := range configs {
+		clientSet, err := client.NewAzureClientSet(*c)
 		if err != nil {
 			return nil, microerror.Mask(err)
 		}
@@ -44,4 +35,44 @@ func getClientSets(k8sClient kubernetes.Interface, environmentName string) ([]*c
 	}
 
 	return clientSets, nil
+}
+
+func getAzureClientSetConfigs(k8sClient kubernetes.Interface, environmentName string) ([]*client.AzureClientSetConfig, error) {
+	var credentials []corev1.Secret
+	{
+		var mark string
+		for {
+			opts := metav1.ListOptions{
+				Continue:      mark,
+				LabelSelector: credentialLabelSelector,
+			}
+
+			list, err := k8sClient.CoreV1().Secrets(credentialNamespace).List(opts)
+			if err != nil {
+				return nil, microerror.Mask(err)
+			}
+
+			credentials = append(credentials, list.Items...)
+
+			mark = list.Continue
+			if mark == "" {
+				break
+			}
+		}
+	}
+
+	configs := []*client.AzureClientSetConfig{}
+
+	for _, secret := range credentials {
+		c, err := credential.GetAzureConfig(k8sClient, secret.Name, secret.Namespace)
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+
+		c.EnvironmentName = environmentName
+
+		configs = append(configs, c)
+	}
+
+	return configs, nil
 }
