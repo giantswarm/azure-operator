9b68346b40de3525cb06c1954435043dbc5927bf
Salvatore Mazzarino <dev@mazzarino.cz>
2019-04-29 17:11:55 +0200

    update bundle

--- service/controller/v7/version_bundle.go
+++ service/controller/v7/version_bundle.go
@@ -19,14 +19,19 @@ func VersionBundle() versionbundle.Bundle {
 			},
 			{
 				Component:   "containerlinux",
-				Description: "Update CoreOS to 2023.5.0. ",
+				Description: "Update CoreOS to 2023.5.0.",
+				Kind:        versionbundle.KindChanged,
+			},
+			{
+				Component:   "calico",
+				Description: "Update calico to 3.6.1",
 				Kind:        versionbundle.KindChanged,
 			},
 		},
 		Components: []versionbundle.Component{
 			{
 				Name:    "calico",
-				Version: "3.5.1",
+				Version: "3.6.1",
 			},
 			{
 				Name:    "containerlinux",
