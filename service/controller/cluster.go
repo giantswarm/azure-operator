1fb5ddc3c4889bce3170643b562e87ec839c9846
mazzy89 <salvo@giantswarm.io>
2019-04-29 19:51:01 +0200

    remove

--- service/controller/cluster.go
+++ service/controller/cluster.go
@@ -18,10 +18,10 @@ import (
 	"github.com/giantswarm/azure-operator/service/controller/setting"
 	"github.com/giantswarm/azure-operator/service/controller/v4patch1"
 	"github.com/giantswarm/azure-operator/service/controller/v4patch2"
-	v5 "github.com/giantswarm/azure-operator/service/controller/v5"
-	v6 "github.com/giantswarm/azure-operator/service/controller/v6"
-	v7 "github.com/giantswarm/azure-operator/service/controller/v7"
-	v8 "github.com/giantswarm/azure-operator/service/controller/v8"
+	"github.com/giantswarm/azure-operator/service/controller/v5"
+	"github.com/giantswarm/azure-operator/service/controller/v6"
+	"github.com/giantswarm/azure-operator/service/controller/v7"
+	"github.com/giantswarm/azure-operator/service/controller/v8"
 )
 
 type ClusterConfig struct {
