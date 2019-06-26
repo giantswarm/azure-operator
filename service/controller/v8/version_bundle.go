f7fbf41a788d063095b6b61fdac7c4492eacd7a9
mazzy89 <salvo@giantswarm.io>
2019-06-26 09:42:13 +0200

    Use imperative

--- service/controller/v8/version_bundle.go
+++ service/controller/v8/version_bundle.go
@@ -9,22 +9,22 @@ func VersionBundle() versionbundle.Bundle {
 		Changelogs: []versionbundle.Changelog{
 			{
 				Component:   "kubernetes",
-				Description: "Updated kubernetes to 1.14.3. More info here: https://github.com/kubernetes/kubernetes/blob/master/CHANGELOG-1.14.md",
+				Description: "Update kubernetes to 1.14.3. More info here: https://github.com/kubernetes/kubernetes/blob/master/CHANGELOG-1.14.md",
 				Kind:        versionbundle.KindChanged,
 			},
 			{
 				Component:   "calico",
-				Description: "Updated calico to 3.7.2. More info here: https://docs.projectcalico.org/v3.7/release-notes/",
+				Description: "Update calico to 3.7.2. More info here: https://docs.projectcalico.org/v3.7/release-notes/",
 				Kind:        versionbundle.KindChanged,
 			},
 			{
 				Component:   "containerlinux",
-				Description: "Updated to 2079.5.1. More info here: https://github.com/coreos/manifest/releases/tag/v2079.5.1",
+				Description: "Update to 2079.5.1. More info here: https://github.com/coreos/manifest/releases/tag/v2079.5.1",
 				Kind:        versionbundle.KindChanged,
 			},
 			{
 				Component:   "etcd",
-				Description: "Updated to 3.3.13. More info here: https://github.com/etcd-io/etcd/blob/master/CHANGELOG-3.3.md#v3313-2019-05-02",
+				Description: "Update to 3.3.13. More info here: https://github.com/etcd-io/etcd/blob/master/CHANGELOG-3.3.md#v3313-2019-05-02",
 				Kind:        versionbundle.KindChanged,
 			},
 		},
