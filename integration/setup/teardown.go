6297c7f51dfe277e64afad126a1d3ab8f6752914
Tim Schindler <tim@giantswarm.io>
2019-06-28 16:34:34 +0200

    update vendor deps (#526)

--- integration/setup/teardown.go
+++ integration/setup/teardown.go
@@ -2,58 +2,22 @@ package setup
 
 import (
 	"context"
-	"fmt"
 
-	"github.com/giantswarm/e2e-harness/pkg/framework"
 	"github.com/giantswarm/microerror"
-
-	"github.com/giantswarm/azure-operator/integration/env"
 )
 
 // Teardown e2e testing environment.
 func Teardown(c Config) error {
 	ctx := context.Background()
 
-	var err error
-
-	{
-		// TODO the deletion detection should rather happen based on the cluster
-		// status or even based on the CR being gone.
-		//
-		//     https://github.com/giantswarm/giantswarm/issues/3839
-		//
-		c.Host.DeleteGuestCluster(ctx, "azure")
-
-		// only do full teardown when not on CI
-		if env.CircleCI() == "true" {
-			return nil
-		}
-	}
-
-	{
-		err = framework.HelmCmd(fmt.Sprintf("delete %s-azure-operator --purge", c.Host.TargetNamespace()))
-		if err != nil {
-			return microerror.Mask(err)
-		}
-		err = framework.HelmCmd(fmt.Sprintf("delete %s-cert-operator --purge", c.Host.TargetNamespace()))
-		if err != nil {
-			return microerror.Mask(err)
-		}
-		err = framework.HelmCmd(fmt.Sprintf("delete %s-node-operator --purge", c.Host.TargetNamespace()))
-		if err != nil {
-			return microerror.Mask(err)
-		}
-	}
-
-	{
-		err = framework.HelmCmd(fmt.Sprintf("delete %s-cert-config-e2e --purge", c.Host.TargetNamespace()))
-		if err != nil {
-			return microerror.Mask(err)
-		}
-		err = framework.HelmCmd(fmt.Sprintf("delete %s-apiextensions-azure-config-e2e --purge", c.Host.TargetNamespace()))
-		if err != nil {
-			return microerror.Mask(err)
-		}
+	// TODO the deletion detection should rather happen based on the cluster
+	// status or even based on the CR being gone.
+	//
+	//     https://github.com/giantswarm/giantswarm/issues/3839
+	//
+	err := c.Host.DeleteGuestCluster(ctx, "azure")
+	if err != nil {
+		return microerror.Mask(err)
 	}
 
 	return nil
