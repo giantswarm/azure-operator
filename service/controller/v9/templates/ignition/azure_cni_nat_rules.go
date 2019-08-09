ecb0c31b6c03042091442995555d80f479a27299
calvix <vaclav@giantswarm.io>
2019-08-09 20:45:13 +0800

    Update service/controller/v9/templates/ignition/azure_cni_nat_rules.go
    Co-Authored-By: Paweł Kopiczko <pawel@giantswarm.io>

--- service/controller/v9/templates/ignition/azure_cni_nat_rules.go
+++ service/controller/v9/templates/ignition/azure_cni_nat_rules.go
@@ -10,6 +10,6 @@ Type=oneshot
 ExecStartPre=/bin/sh -c "iptables -I INPUT 1 -m udp -p udp --source-port 80 -j DROP"
 ExecStartPre=/bin/sh -c "iptables -I INPUT 1 -m udp -p udp --source-port 443 -j DROP"
 ExecStart=/bin/sh -c "iptables -t nat -A POSTROUTING -m addrtype ! --dst-type local ! -d {{.VnetCIDR}} -j MASQUERADE"
- [Install]
+[Install]
 WantedBy=multi-user.target
 `
