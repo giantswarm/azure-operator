d3a66a770ac6d9718607348a2f76c8fc6732292a
Jannis Schaefer <j.schaefer@estwx.de>
2019-08-09 11:26:37 +0200

    revert changelog

--- service/controller/v8patch1/version_bundle.go
+++ service/controller/v8patch1/version_bundle.go
@@ -8,15 +8,31 @@ func VersionBundle() versionbundle.Bundle {
 	return versionbundle.Bundle{
 		Changelogs: []versionbundle.Changelog{
 			{
-				Component:   "kubernetes",
-				Description: "Update kubernetes to 1.14.5 to fix CVE-2019-11247. https://github.com/kubernetes/kubernetes/blob/master/CHANGELOG-1.14.md",
+				Component:   "TODO",
+				Description: "TODO",
 				Kind:        versionbundle.KindChanged,
 			},
 		},
 		Components: []versionbundle.Component{
 			{
+				Name:    "calico",
+				Version: "3.7.2",
+			},
+			{
+				Name:    "containerlinux",
+				Version: "2135.4.0",
+			},
+			{
+				Name:    "docker",
+				Version: "18.06.1",
+			},
+			{
+				Name:    "etcd",
+				Version: "3.3.13",
+			},
+			{
 				Name:    "kubernetes",
-				Version: "1.14.5",
+				Version: "1.14.3",
 			},
 		},
 		Name:    "azure-operator",
