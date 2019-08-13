0d25b66dd098b45a3a094b9838cb08887ccf9131
Salvatore Mazzarino <salvo@giantswarm.io>
2019-08-13 17:16:10 +0200

    Update version_bundle.go

--- service/controller/v9/version_bundle.go
+++ service/controller/v9/version_bundle.go
@@ -32,6 +32,11 @@ func VersionBundle() versionbundle.Bundle {
 				Description: "Add VNET service endpoint for accessing Azure Storage.",
 				Kind:        versionbundle.KindAdded,
 			},
+			{
+				Component:   "kubernetes",
+				Description: "Update kubernetes to 1.14.5 (CVE-2019-11247) https://github.com/kubernetes/kubernetes/blob/master/CHANGELOG-1.14.md#v1145",
+				Kind:        versionbundle.KindChanged,
+			},
 		},
 		Components: []versionbundle.Component{
 			{
@@ -52,7 +57,7 @@ func VersionBundle() versionbundle.Bundle {
 			},
 			{
 				Name:    "kubernetes",
-				Version: "1.14.3",
+				Version: "1.14.5",
 			},
 		},
 		Name:    "azure-operator",
