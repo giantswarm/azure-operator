5d283a74fc710868112901a3454303b57fb57e6b
Thomas Fussell <thomas@fussell.io>
2019-10-25 15:17:02 -0400

    Copy v11 to v12

---
+++ service/controller/v12/cloudconfig/base_extension.go
@@ -0,0 +1,60 @@
+package cloudconfig
+
+import (
+	providerv1alpha1 "github.com/giantswarm/apiextensions/pkg/apis/provider/v1alpha1"
+	"github.com/giantswarm/certs"
+
+	"github.com/giantswarm/azure-operator/client"
+	"github.com/giantswarm/azure-operator/service/controller/setting"
+	"github.com/giantswarm/azure-operator/service/controller/v11/encrypter"
+	"github.com/giantswarm/azure-operator/service/controller/v11/key"
+)
+
+type baseExtension struct {
+	azure        setting.Azure
+	azureConfig  client.AzureClientSetConfig
+	calicoCIDR   string
+	clusterCerts certs.Cluster
+	customObject providerv1alpha1.AzureConfig
+	encrypter    encrypter.Interface
+	vnetCIDR     string
+}
+
+func (e *baseExtension) templateData(certFiles certs.Files) templateData {
+	var certsPaths []string
+
+	for _, file := range certFiles {
+		certsPaths = append(certsPaths, file.AbsolutePath)
+	}
+
+	return templateData{
+		azureCNIFileParams{
+			VnetCIDR: e.vnetCIDR,
+		},
+		calicoAzureFileParams{
+			Cluster:    e.customObject.Spec.Cluster,
+			CalicoCIDR: e.calicoCIDR,
+		},
+		cloudProviderConfFileParams{
+			AADClientID:                 e.azureConfig.ClientID,
+			AADClientSecret:             e.azureConfig.ClientSecret,
+			EnvironmentName:             e.azure.EnvironmentName,
+			Location:                    e.azure.Location,
+			PrimaryScaleSetName:         key.WorkerVMSSName(e.customObject),
+			ResourceGroup:               key.ResourceGroupName(e.customObject),
+			RouteTableName:              key.RouteTableName(e.customObject),
+			SecurityGroupName:           key.WorkerSecurityGroupName(e.customObject),
+			SubnetName:                  key.WorkerSubnetName(e.customObject),
+			SubscriptionID:              e.azureConfig.SubscriptionID,
+			TenantID:                    e.azureConfig.TenantID,
+			VnetName:                    key.VnetName(e.customObject),
+			UseManagedIdentityExtension: e.azure.MSI.Enabled,
+		},
+		certificateDecrypterUnitParams{
+			CertsPaths: certsPaths,
+		},
+		ingressLBFileParams{
+			ClusterDNSDomain: key.ClusterDNSDomain(e.customObject),
+		},
+	}
+}
---
+++ service/controller/v12/resource/instance/resource.go
@@ -0,0 +1,149 @@
+package instance
+
+import (
+	"context"
+
+	"github.com/Azure/azure-sdk-for-go/services/compute/mgmt/2018-06-01/compute"
+	azureresource "github.com/Azure/azure-sdk-for-go/services/resources/mgmt/2018-05-01/resources"
+	"github.com/Azure/azure-sdk-for-go/services/storage/mgmt/2018-07-01/storage"
+	"github.com/giantswarm/apiextensions/pkg/clientset/versioned"
+	"github.com/giantswarm/microerror"
+	"github.com/giantswarm/micrologger"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+
+	"github.com/giantswarm/azure-operator/service/controller/setting"
+	"github.com/giantswarm/azure-operator/service/controller/v11/controllercontext"
+	"github.com/giantswarm/azure-operator/service/controller/v11/debugger"
+	"github.com/giantswarm/azure-operator/service/controller/v11/encrypter"
+	"github.com/giantswarm/azure-operator/service/controller/v11/key"
+)
+
+const (
+	Name = "instancev11"
+)
+
+type Config struct {
+	Debugger  *debugger.Debugger
+	G8sClient versioned.Interface
+	K8sClient kubernetes.Interface
+	Logger    micrologger.Logger
+
+	Azure           setting.Azure
+	TemplateVersion string
+}
+
+type Resource struct {
+	debugger  *debugger.Debugger
+	g8sClient versioned.Interface
+	k8sClient kubernetes.Interface
+	logger    micrologger.Logger
+
+	azure           setting.Azure
+	templateVersion string
+}
+
+func New(config Config) (*Resource, error) {
+	if config.Debugger == nil {
+		return nil, microerror.Maskf(invalidConfigError, "%T.Debugger must not be empty", config)
+	}
+	if config.G8sClient == nil {
+		return nil, microerror.Maskf(invalidConfigError, "%T.G8sClient must not be empty", config)
+	}
+	if config.K8sClient == nil {
+		return nil, microerror.Maskf(invalidConfigError, "%T.K8sClient must not be empty", config)
+	}
+	if config.Logger == nil {
+		return nil, microerror.Maskf(invalidConfigError, "%T.Logger must not be empty", config)
+	}
+
+	if err := config.Azure.Validate(); err != nil {
+		return nil, microerror.Maskf(invalidConfigError, "%T.Azure.%s", config, err)
+	}
+	if config.TemplateVersion == "" {
+		return nil, microerror.Maskf(invalidConfigError, "%T.TemplateVersion must not be empty", config)
+	}
+
+	r := &Resource{
+		debugger:  config.Debugger,
+		g8sClient: config.G8sClient,
+		k8sClient: config.K8sClient,
+		logger:    config.Logger,
+
+		azure:           config.Azure,
+		templateVersion: config.TemplateVersion,
+	}
+
+	return r, nil
+}
+
+func (r *Resource) Name() string {
+	return Name
+}
+
+func (r *Resource) getDeploymentsClient(ctx context.Context) (*azureresource.DeploymentsClient, error) {
+	sc, err := controllercontext.FromContext(ctx)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	return sc.AzureClientSet.DeploymentsClient, nil
+}
+
+func (r *Resource) getScaleSetsClient(ctx context.Context) (*compute.VirtualMachineScaleSetsClient, error) {
+	sc, err := controllercontext.FromContext(ctx)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	return sc.AzureClientSet.VirtualMachineScaleSetsClient, nil
+}
+
+func (r *Resource) getStorageAccountsClient(ctx context.Context) (*storage.AccountsClient, error) {
+	sc, err := controllercontext.FromContext(ctx)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	return sc.AzureClientSet.StorageAccountsClient, nil
+}
+
+func (r *Resource) getVMsClient(ctx context.Context) (*compute.VirtualMachineScaleSetVMsClient, error) {
+	sc, err := controllercontext.FromContext(ctx)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	return sc.AzureClientSet.VirtualMachineScaleSetVMsClient, nil
+}
+
+func (r *Resource) getEncrypterObject(ctx context.Context, secretName string) (encrypter.Interface, error) {
+	r.logger.LogCtx(ctx, "level", "debug", "message", "retrieving encryptionkey")
+
+	secret, err := r.k8sClient.CoreV1().Secrets(key.CertificateEncryptionNamespace).Get(secretName, metav1.GetOptions{})
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	var enc *encrypter.Encrypter
+	{
+		if _, ok := secret.Data[key.CertificateEncryptionKeyName]; !ok {
+			return nil, microerror.Maskf(invalidConfigError, "encryption key not found in secret", secret.Name)
+		}
+		if _, ok := secret.Data[key.CertificateEncryptionIVName]; !ok {
+			return nil, microerror.Maskf(invalidConfigError, "encryption iv not found in secret", secret.Name)
+		}
+		c := encrypter.Config{
+			Key: secret.Data[key.CertificateEncryptionKeyName],
+			IV:  secret.Data[key.CertificateEncryptionIVName],
+		}
+
+		enc, err = encrypter.New(c)
+		if err != nil {
+			return nil, microerror.Mask(err)
+
+		}
+	}
+
+	return enc, nil
+}
