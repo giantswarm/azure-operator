9fe113096ca18d931a9adff924d86bf5740bf0a0
Whites11 <whites11@gmail.com>
2019-11-20 16:57:09 +0100

    Added more unit tests for the checksum functions

--- service/controller/v12/resource/instance/checksum.go
+++ service/controller/v12/resource/instance/checksum.go
@@ -2,10 +2,10 @@ package instance
 
 import (
 	"crypto/sha256"
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"github.com/Azure/azure-sdk-for-go/services/resources/mgmt/2018-05-01/resources"
-	"github.com/giantswarm/azure-operator/service/controller/v12/key"
 	"io/ioutil"
 	"net/http"
 )
@@ -36,16 +36,59 @@ func getDeploymentTemplateChecksum(deployment resources.Deployment) (string, err
 }
 
 func getDeploymentParametersChecksum(deployment resources.Deployment) (string, error) {
-	// I can't use deployment.Properties.Parameters because it contains some dynamic data that changes at every loop
-	// (at least the 'masterCloudConfigData' and 'workerCloudConfigData' but possibly other fields as well).
-	// Currently I'm using just the ignition template to calculate the checksum.
-	// This is error prone and should be fixed somehow.
-	// todo read above
-	jsonStr, err := json.Marshal(key.CloudConfigSmallTemplates())
+	params := deployment.Properties.Parameters.(map[string]interface{})
+
+	filteredParams := map[string]interface{}{}
+
+	// some of the fields in the Parameters property change at every iteration
+	// I have to filter out the changing data in order to be able to calculate a replicatable checksum
+	for k, v := range params {
+		switch k {
+		case "masterCloudConfigData":
+		case "workerCloudConfigData":
+			// these two fields are base64 encoded, so first decode 'em
+			decoded, err := base64.StdEncoding.DecodeString(v.(struct{ Value interface{} }).Value.(string))
+			if err != nil {
+				return "", err
+			}
+
+			// the decoded content is a json, convert it into a map to manipulate it
+			var m map[string]interface{}
+			err = json.Unmarshal(decoded, &m)
+			if err != nil {
+				return "", err
+			}
+
+			// delete the [ignition][config] field which is the one that changes at every loop.
+			// It is not semantically important and it can be ignored for the sake of checksum calculation
+			m["ignition"].(map[string]interface{})["config"] = nil
+
+			// convert the modified map back to json
+			jsonStr, err := json.Marshal(m)
+			if err != nil {
+				return "", err
+			}
+
+			// encode back the json into base64
+			encoded := base64.StdEncoding.EncodeToString(jsonStr)
+
+			// add the new parameter into the filteredParameters
+			filteredParams[k] = encoded
+
+			continue
+		default:
+			// all other fields are kept as-is
+			filteredParams[k] = v
+		}
+	}
+
+	// create a json with the whole adjusted parameters
+	jsonStr, err := json.Marshal(filteredParams)
 	if err != nil {
 		return "", err
 	}
 
+	// calculate the sha256 hash of the json
 	hash := fmt.Sprintf("%x", sha256.Sum256(jsonStr))
 
 	return hash, nil
--- service/controller/v12/resource/instance/checksum_test.go
+++ service/controller/v12/resource/instance/checksum_test.go
@@ -1,9 +1,12 @@
 package instance
 
 import (
+	"encoding/base64"
 	"fmt"
 	"github.com/Azure/azure-sdk-for-go/services/resources/mgmt/2018-05-01/resources"
 	"github.com/Azure/go-autorest/autorest/to"
+	"github.com/giantswarm/azure-operator/service/controller/v12/key"
+	"github.com/giantswarm/azure-operator/service/controller/v12/templates"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -82,4 +85,80 @@ func Test_getDeploymentTemplateChecksum(t *testing.T) {
 			}
 		})
 	}
+}
+
+func Test_getDeploymentParametersChecksum(t *testing.T) {
+	deployment, err := getDeployment()
+	if err != nil {
+		t.Fatalf("Unable to construct a deployment: %v", err)
+	}
+
+	chk, err := getDeploymentParametersChecksum(*deployment)
+
+	if err != nil {
+		t.Fatalf("Unexpected error")
+	}
+
+	if chk != "dd10f2e2f6877ba305a2af480687f06402ac0cb9835516662835dd0884337a44" {
+		t.Fatalf("Checksum calculation invalid")
+	}
+}
+
+func getDeployment() (*resources.Deployment, error) {
+	nodes := []node{
+		{
+			AdminUsername:   "giantswarm",
+			AdminSSHKeyData: "ssh-rsa AAAAB3NzaC1yc...k+y+ls2D0xJfqxw==",
+			OSImage: nodeOSImage{
+				Offer:     "CoreOS",
+				Publisher: "CoreOS",
+				SKU:       "Stable",
+				Version:   "2191.5.0",
+			},
+			VMSize:             "Standard_D4s_v3",
+			DockerVolumeSizeGB: 50,
+		},
+	}
+
+	c := SmallCloudconfigConfig{
+		BlobURL:       "https://gssatjb62.blob.core.windows.net/ignition/2.8.0-v4.7.0-worker?se=2020-05-18T13%3A60%3A03Z&sig=9tXJCWxsZb6MxBQZDDbVykB3VMs0CxxoIDHJtpKs10g%3D&sp=r&spr=https&sr=b&sv=2018-03-28",
+		EncryptionKey: "00112233445566778899aabbccddeeff00112233445566778899aabbccddee",
+		InitialVector: "0011223344556677889900aabbccddee",
+		InstanceRole:  "master",
+	}
+	masterCloudConfig, err := templates.Render(key.CloudConfigSmallTemplates(), c)
+	if err != nil {
+		return nil, err
+	}
+	encodedMasterCloudConfig := base64.StdEncoding.EncodeToString([]byte(masterCloudConfig))
+
+	c = SmallCloudconfigConfig{
+		BlobURL:       "https://gssatjb62.blob.core.windows.net/ignition/2.8.0-v4.7.0-worker?se=2020-05-18T13%3A61%3A03Z&sig=9tXJCWxsZb6MxBQZDDbVykB3VMs0CxxoIDHJtpKs10g%3D&sp=r&spr=https&sr=b&sv=2018-03-28",
+		EncryptionKey: "eeddccbbaa99887766554433221100ffeeddccbbaa99887766554433221100",
+		InitialVector: "eeddccbbaa0099887766554433221100",
+		InstanceRole:  "worker",
+	}
+	workerCloudConfig, err := templates.Render(key.CloudConfigSmallTemplates(), c)
+	if err != nil {
+		return nil, err
+	}
+	encodedWorkerCloudConfig := base64.StdEncoding.EncodeToString([]byte(workerCloudConfig))
+
+	parameters := map[string]interface{}{
+		"apiLBBackendPoolID":    "/subscriptions/746379f9-ad35-1d92-1829-cba8579d71e6/resourceGroups/tjb62/providers/Microsoft.Network/loadBalancers/tjb62-API-PublicLoadBalancer/backendAddressPools/tjb62-API-PublicLoadBalancer-BackendPool",
+		"clusterID":             "tjb62",
+		"etcdLBBackendPoolID":   "/subscriptions/746379f9-ad35-1d92-1829-cba8579d71e6/resourceGroups/tjb62/providers/Microsoft.Network/loadBalancers/tjb62-ETCD-PrivateLoadBalancer/backendAddressPools/tjb62-ETCD-PrivateLoadBalancer-BackendPool", // string
+		"masterCloudConfigData": struct{ Value interface{} }{Value: encodedMasterCloudConfig},
+		"masterNodes":           nodes,
+		"masterSubnetID":        "/subscriptions/746379f9-ad35-1d92-1829-cba8579d71e6/resourceGroups/tjb62/providers/Microsoft.Network/virtualNetworks/tjb62-VirtualNetwork/subnets/tjb62-VirtualNetwork-MasterSubnet",
+		"vmssMSIEnabled":        true,
+		"workerCloudConfigData": struct{ Value interface{} }{Value: encodedWorkerCloudConfig},
+		"workerNodes":           nodes,
+		"workerSubnetID":        "/subscriptions/746379f9-ad35-1d92-1829-cba8579d71e6/resourceGroups/tjb62/providers/Microsoft.Network/virtualNetworks/tjb62-VirtualNetwork/subnets/tjb62-VirtualNetwork-WorkerSubnet",
+	}
+
+	properties := resources.DeploymentProperties{
+		Parameters: parameters,
+	}
+	return &resources.Deployment{Properties: &properties}, nil
 }
