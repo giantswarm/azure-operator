89642191c3ac977d60dd8f15d102a125cae3c1d1
Stevo Slavić <stevo@giantswarm.io>
2019-12-13 18:25:30 +0100

    Vendor dependencies, update k8s to 1.16.3 (#603)
    * Vendor dependencies, update k8s to 1.16.3
    
    * Read AZs from env and use it when creating tenant cluster
    
    * Panic if AZ's value is not correct
    
    * Updated e2etemplates dependency
    
    * Use AZ 1 for e2e integration tests
    
    * Vendor dependencies, use latests errors and statusresource libs

--- integration/env/azure.go
+++ integration/env/azure.go
@@ -4,11 +4,13 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 
 	"github.com/giantswarm/azure-operator/integration/network"
 )
 
 const (
+	EnvVarAzureAZs              = "AZURE_AZS"
 	EnvVarAzureCIDR             = "AZURE_CIDR"
 	EnvVarAzureCalicoSubnetCIDR = "AZURE_CALICO_SUBNET_CIDR"
 	EnvVarAzureMasterSubnetCIDR = "AZURE_MASTER_SUBNET_CIDR"
@@ -32,11 +34,12 @@ var (
 	azureSubscriptionID string
 	azureTenantID       string
 
-	azureCIDR             string
-	azureCalicoSubnetCIDR string
-	azureMasterSubnetCIDR string
-	azureVPNSubnetCIDR    string
-	azureWorkerSubnetCIDR string
+	azureAvailabilityZones string
+	azureCIDR              string
+	azureCalicoSubnetCIDR  string
+	azureMasterSubnetCIDR  string
+	azureVPNSubnetCIDR     string
+	azureWorkerSubnetCIDR  string
 
 	commonDomainResourceGroup string
 )
@@ -55,6 +58,11 @@ func init() {
 	azureLocation = os.Getenv(EnvVarAzureLocation)
 	if azureLocation == "" {
 		panic(fmt.Sprintf("env var '%s' must not be empty", EnvVarAzureLocation))
+	}
+
+	azureAvailabilityZones = os.Getenv(EnvVarAzureAZs)
+	if azureAvailabilityZones == "" {
+		panic(fmt.Sprintf("env var '%s' must not be empty", EnvVarAzureAZs))
 	}
 
 	azureSubscriptionID = os.Getenv(EnvVarAzureSubscriptionID)
@@ -92,6 +100,27 @@ func init() {
 		azureVPNSubnetCIDR = subnets.VPN.String()
 		azureWorkerSubnetCIDR = subnets.Worker.String()
 	}
+}
+
+func AzureAvailabilityZones() []int {
+	azs := strings.Split(strings.TrimSpace(azureAvailabilityZones), " ")
+	zones := make([]int, len(azs))
+
+	for i, s := range azs {
+		zone, err := strconv.Atoi(s)
+		if err != nil {
+			panic(fmt.Sprintf("AvailabilityZones valid numbers are 1, "+
+				"2, 3. Your '%s' env var contains %s",
+				EnvVarAzureAZs, azureAvailabilityZones))
+		}
+		if zone < 1 || zone > 3 {
+			panic(fmt.Sprintf("AvailabilityZones valid numbers are 1, "+
+				"2, 3. Your '%s' env var contains %s",
+				EnvVarAzureAZs, azureAvailabilityZones))
+		}
+		zones[i] = zone
+	}
+	return zones
 }
 
 func AzureCalicoSubnetCIDR() string {
