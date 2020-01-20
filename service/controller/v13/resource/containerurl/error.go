77ba6afe2426f911602ad5edbbb7b2807956b2cb
Bricktop <marcel.mueller1@rwth-aachen.de>
2020-01-20 15:05:33 +0100

    v13: copy (#627)

---
+++ service/controller/v13/resource/containerurl/error.go
@@ -0,0 +1,44 @@
+package containerurl
+
+import (
+	"strings"
+
+	"github.com/giantswarm/microerror"
+)
+
+// executionFailedError is an error type for situations where Resource
+// execution cannot continue and must always fall back to operatorkit.
+//
+// This error should never be matched against and therefore there is no matcher
+// implement. For further information see:
+//
+//     https://github.com/giantswarm/fmt/blob/master/go/errors.md#matching-errors
+//
+var executionFailedError = &microerror.Error{
+	Kind: "executionFailedError",
+}
+
+var invalidConfigError = &microerror.Error{
+	Kind: "invalidConfigError",
+}
+
+// IsInvalidConfig asserts invalidConfigError.
+func IsInvalidConfig(err error) bool {
+	return microerror.Cause(err) == invalidConfigError
+}
+
+// IsStorageAccountNotFound asserts storage account not found error from upstream's API message.
+func IsStorageAccountNotFound(err error) bool {
+	if err == nil {
+		return false
+	}
+	return strings.Contains(microerror.Cause(err).Error(), "ResourceNotFound")
+}
+
+// IsStorageAccountNotProvisioned asserts storage account not provisioned error from upstream's API message.
+func IsStorageAccountNotProvisioned(err error) bool {
+	if err == nil {
+		return false
+	}
+	return strings.Contains(microerror.Cause(err).Error(), "StorageAccountIsNotProvisioned")
+}
---
+++ service/controller/v13/templates/ignition/small.go
@@ -0,0 +1,65 @@
+package ignition
+
+const Small = `{
+  "ignition": {
+    "version": "2.2.0",
+    "config": {
+      "append": [
+	    {
+          "source": "{{ .BlobURL }}"
+        }
+	  ]
+    }
+  },
+  "storage": {
+    "files": [
+      {
+        "path": "/etc/.enc/key",
+        "filesystem": "root",
+        "mode": 256,
+        "contents": {
+          "source": "data:text/plain,ENCRYPTION_KEY={{ .EncryptionKey }}"
+        }
+      },
+      {
+        "path": "/etc/.enc/iv",
+        "filesystem": "root",
+        "mode": 256,
+        "contents": {
+          "source": "data:text/plain,INITIAL_VECTOR={{ .InitialVector }}"
+        }
+      }
+    ],
+  "filesystems": [
+      { 
+        "name": "docker",
+        "mount": {
+          "device": "/dev/disk/azure/scsi1/{{ if eq .InstanceRole "master"}}lun1{{ else }}lun0{{end}}",
+          "wipeFilesystem": true,
+          "label": "docker",
+          "format": "xfs"
+        }
+      },
+      { 
+        "name": "kubelet",
+        "mount": {
+          "device": "/dev/disk/azure/scsi1/{{ if eq .InstanceRole "master"}}lun2{{ else }}lun1{{end}}",
+          "wipeFilesystem": true,
+          "label": "kubelet",
+          "format": "xfs"
+        }
+      }{{ if eq .InstanceRole "master" -}},
+      {
+        "name": "etcd",
+        "mount": {
+          "device": "/dev/disk/azure/scsi1/lun0",
+          "wipeFilesystem": false,
+          "label": "etcd",
+          "format": "ext4"
+        }
+      }
+	   {{- end }}
+    ]
+  }
+}
+`
