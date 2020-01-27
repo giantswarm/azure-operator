95e22516cefe6ce057fc6f666c5b3177fd61e9d6
Jose Armesto <github@armesto.net>
2020-01-27 09:42:13 +0100

    Remove AzureConfig to avoid reconciling while deleting

--- integration/setup/teardown.go
+++ integration/setup/teardown.go
@@ -4,15 +4,26 @@ import (
 	"context"
 
 	"github.com/giantswarm/microerror"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"github.com/giantswarm/azure-operator/integration/env"
 )
 
 // Teardown e2e testing environment.
 func Teardown(c Config) error {
+	var err error
 	ctx := context.Background()
+	clusterID := env.ClusterID()
 
-	_, err := c.AzureClient.ResourceGroupsClient.Delete(ctx, env.ClusterID())
+	// Delete AzureConfig from control plane to avoid reconciling while deleting the resources on Azure.
+	// We don't wait for the resources to be deleted, since we are going to delete them below.
+	err = c.Host.G8sClient().ProviderV1alpha1().AzureConfigs(c.Host.TargetNamespace()).Delete(clusterID, &metav1.DeleteOptions{})
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	// Delete resources on Azure.
+	_, err = c.AzureClient.ResourceGroupsClient.Delete(ctx, clusterID)
 	if err != nil {
 		return microerror.Mask(err)
 	}
