1ac35bfd0aee11b075581f738ce7d69f13871dc4
Tuomas Mäkinen <tuomas@giantswarm.io>
2020-02-03 12:20:15 +0200

    address review comments

--- service/controller/v13/resource/ingresspipname/resource.go
+++ service/controller/v13/resource/ingresspipname/resource.go
@@ -8,15 +8,12 @@ package ingresspipname
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/Azure/azure-sdk-for-go/services/network/mgmt/2018-06-01/network"
 	"github.com/giantswarm/apiextensions/pkg/clientset/versioned"
 	"github.com/giantswarm/azure-operator/service/controller/v13/controllercontext"
-	"github.com/giantswarm/azure-operator/service/controller/v13/key"
 	"github.com/giantswarm/microerror"
 	"github.com/giantswarm/micrologger"
-	"github.com/giantswarm/operatorkit/controller/context/reconciliationcanceledcontext"
 )
 
 const (
@@ -41,94 +38,16 @@ func New(config Config) (*Resource, error) {
 		return nil, microerror.Maskf(invalidConfigError, "%T.Logger must not be empty", config)
 	}
 
-	newResource := &Resource{
+	r := &Resource{
 		g8sClient: config.G8sClient,
 		logger:    config.Logger,
 	}
 
-	return newResource, nil
+	return r, nil
 }
 
 func (r *Resource) Name() string {
 	return Name
-}
-
-func (r *Resource) EnsureCreated(ctx context.Context, obj interface{}) error {
-	cr, err := key.ToCustomObject(obj)
-	if err != nil {
-		return microerror.Mask(err)
-	}
-
-	{
-		r.logger.LogCtx(ctx, "level", "debug", "message", "finding if ingress PIP name must be set to status")
-
-		if key.IngressPIPName(cr) != "" {
-			r.logger.LogCtx(ctx, "level", "debug", "message", "found out that ingress PIP name is already set to status")
-			r.logger.LogCtx(ctx, "level", "debug", "message", "canceling resource")
-
-			return nil
-		}
-	}
-
-	var ingressPIPName string
-	{
-		r.logger.LogCtx(ctx, "level", "debug", "message", "finding if ingress PIP has been already created")
-
-		groupName := key.ClusterID(cr)
-		pipClient, err := r.getPIPClient(ctx)
-		if err != nil {
-			return microerror.Mask(err)
-		}
-
-		// First try to look for legacy PIP name (dummmy-pip).
-		_, err = pipClient.Get(ctx, groupName, key.LegacyIngressLBPIPName, "")
-		if IsPIPNotFound(err) {
-			// This is ok. Maybe PIP is created with currently desired name?
-
-			_, err = pipClient.Get(ctx, groupName, key.DefaultIngressPIPName(cr), "")
-			if IsPIPNotFound(err) {
-				// This is ok as well. The given Public IP is not created yet.
-			} else if err != nil {
-				return microerror.Mask(err)
-			}
-
-			ingressPIPName = key.DefaultIngressPIPName(cr)
-
-		} else if err != nil {
-			return microerror.Mask(err)
-		} else {
-			ingressPIPName = key.LegacyIngressLBPIPName
-		}
-
-		if ingressPIPName == "" {
-			r.logger.LogCtx(ctx, "level", "debug", "message", "did not find already created ingress PIP")
-			ingressPIPName = key.DefaultIngressPIPName(cr)
-			r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("defaulting to %q", ingressPIPName))
-		} else {
-			r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("found ingress PIP with name %q", ingressPIPName))
-		}
-	}
-
-	{
-		r.logger.LogCtx(ctx, "level", "debug", "message", "updating CR status with ingress PIP name")
-
-		cr.Status.Provider.Ingress.LoadBalancer.PublicIPName = ingressPIPName
-
-		_, err := r.g8sClient.ProviderV1alpha1().AzureConfigs(cr.Namespace).UpdateStatus(&cr)
-		if err != nil {
-			return microerror.Mask(err)
-		}
-
-		r.logger.LogCtx(ctx, "level", "debug", "message", "updated CR status with ingress PIP name")
-		r.logger.LogCtx(ctx, "level", "debug", "message", "canceling reconciliation")
-		reconciliationcanceledcontext.SetCanceled(ctx)
-	}
-
-	return nil
-}
-
-func (r *Resource) EnsureDeleted(ctx context.Context, obj interface{}) error {
-	return nil
 }
 
 func (r *Resource) getPIPClient(ctx context.Context) (*network.PublicIPAddressesClient, error) {
