38900a8a51191596d24a558d52797c10686253a1
Jose Armesto <github@armesto.net>
2020-02-12 22:20:03 +0100

    Test

--- vendor/github.com/giantswarm/e2etests/clusterstate/clusterstate.go
+++ vendor/github.com/giantswarm/e2etests/clusterstate/clusterstate.go
@@ -11,6 +11,8 @@ import (
 	"github.com/giantswarm/microerror"
 	"github.com/giantswarm/micrologger"
 	"github.com/spf13/afero"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"github.com/giantswarm/e2etests/clusterstate/provider"
@@ -161,6 +163,17 @@ func (c *ClusterState) Test(ctx context.Context) error {
 		c.logger.LogCtx(ctx, "level", "debug", "message", "test app is installed")
 	}
 
+	{
+		c.logger.LogCtx(ctx, "level", "debug", "message", "test if PVC is bound")
+
+		err = c.CreatePVC(ctx)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+
+		c.logger.LogCtx(ctx, "level", "debug", "message", "tested if PVC is bound")
+	}
+
 	return nil
 }
 
@@ -252,6 +265,55 @@ func (c *ClusterState) CheckTestAppIsInstalled(ctx context.Context) error {
 	}
 
 	c.logger.Log("level", "debug", "message", fmt.Sprintf("found %d pods of the e2e-app", podCount))
+
+	return nil
+}
+
+func (c *ClusterState) CreatePVC(ctx context.Context) error {
+	pvcName := "e2e-pvc"
+	pvc := &v1.PersistentVolumeClaim{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      pvcName,
+			Namespace: "default",
+		},
+		Spec: v1.PersistentVolumeClaimSpec{
+			AccessModes: []v1.PersistentVolumeAccessMode{v1.ReadWriteOnce},
+			Resources: v1.ResourceRequirements{
+				Requests: v1.ResourceList{
+					v1.ResourceName(v1.ResourceStorage): resource.MustParse("1Gi"),
+				},
+			},
+		},
+	}
+
+	c.logger.LogCtx(ctx, "level", "debug", "message", "creating pvc '%s'", pvcName)
+	_, err := c.legacyFramework.K8sClient().CoreV1().PersistentVolumeClaims("default").Create(pvc)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	o := func() error {
+		pvc, err := c.legacyFramework.K8sClient().CoreV1().PersistentVolumeClaims("default").Get(pvcName, metav1.GetOptions{})
+		if err != nil {
+			return microerror.Mask(err)
+		}
+
+		if pvc.Status.Phase != v1.ClaimBound {
+			return microerror.Maskf(waitError, "PVC '%s' is not bound yet", pvcName)
+		}
+
+		return nil
+	}
+
+	b := backoff.NewConstant(backoff.ShortMaxWait, backoff.ShortMaxInterval)
+	n := func(err error, delay time.Duration) {
+		c.logger.Log("level", "debug", "message", err.Error())
+	}
+
+	err = backoff.RetryNotify(o, b, n)
+	if err != nil {
+		return microerror.Mask(err)
+	}
 
 	return nil
 }
