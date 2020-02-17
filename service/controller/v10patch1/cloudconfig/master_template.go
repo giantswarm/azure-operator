3ee81f9629c2c8eb55247544e7a43e9ef3598262
Whites11 <whites11@gmail.com>
2020-02-17 13:19:38 +0100

    Removed the localhost hardcoding for etcd address

--- service/controller/v10patch1/cloudconfig/master_template.go
+++ service/controller/v10patch1/cloudconfig/master_template.go
@@ -21,10 +21,6 @@ func (c CloudConfig) NewMasterCloudConfig(customObject providerv1alpha1.AzureCon
 		return "", microerror.Mask(err)
 	}
 
-	// On Azure only master nodes access etcd, so it is locked down.
-	customObject.Spec.Cluster.Etcd.Domain = "localhost"
-	customObject.Spec.Cluster.Etcd.Port = 2379
-
 	var k8sAPIExtraArgs []string
 	{
 		k8sAPIExtraArgs = append(k8sAPIExtraArgs, "--cloud-config=/etc/kubernetes/config/azure.yaml")
