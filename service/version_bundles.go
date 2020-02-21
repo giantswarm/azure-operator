f2bfc90fc608ef0dfebb128893efe7cd5e0f1cb0
Jose Armesto <github@armesto.net>
2020-02-21 16:45:34 +0100

    Removed current version bundle

--- service/version_bundles.go
+++ service/version_bundles.go
@@ -7,7 +7,6 @@ import (
 	v11 "github.com/giantswarm/azure-operator/service/controller/v11"
 	v12 "github.com/giantswarm/azure-operator/service/controller/v12"
 	v13 "github.com/giantswarm/azure-operator/service/controller/v13"
-	v14 "github.com/giantswarm/azure-operator/service/controller/v14"
 	v7 "github.com/giantswarm/azure-operator/service/controller/v7"
 )
 
@@ -19,7 +18,6 @@ func NewVersionBundles() []versionbundle.Bundle {
 	versionBundles = append(versionBundles, v11.VersionBundle())
 	versionBundles = append(versionBundles, v12.VersionBundle())
 	versionBundles = append(versionBundles, v13.VersionBundle())
-	versionBundles = append(versionBundles, v14.VersionBundle())
 
 	return versionBundles
 }
