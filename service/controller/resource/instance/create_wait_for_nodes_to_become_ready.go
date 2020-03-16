d8796e38d0d7201e2e4f6a171d69b14fcdae00ac
Tuomas Mäkinen <tuomas@giantswarm.io>
2020-03-16 15:05:07 +0200

    Fix isReady() when ensuring node is ready during instance upgrades
    Node status conditions have type and reason and both must be matched correctly
    in order to detect node readiness correctly. This change mainly affects cluster
    upgrade.

--- service/controller/resource/instance/create_wait_for_nodes_to_become_ready.go
+++ service/controller/resource/instance/create_wait_for_nodes_to_become_ready.go
@@ -124,7 +124,7 @@ func isWorker(n corev1.Node) bool {
 
 func isReady(n corev1.Node) bool {
 	for _, c := range n.Status.Conditions {
-		if c.Type == corev1.NodeReady {
+		if c.Type == corev1.NodeReady && c.Reason == "KubeletReady" {
 			return true
 		}
 	}
