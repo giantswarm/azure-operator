744fd330fce6ce2ae4100ff64567e8c4193b2333
Jose Armesto <github@armesto.net>
2020-03-17 12:46:57 +0100

    Both root and e2eharness folder

--- integration/config/setup.sh
+++ integration/config/setup.sh
@@ -4,3 +4,9 @@ shipyard_dir=".e2e-harness/workdir/.shipyard"
 mkdir -p "${shipyard_dir}"
 chmod -R 777 ".e2e-harness"
 kind get kubeconfig --name="kind" > "${shipyard_dir}/config"
+
+
+shipyard_dir="/workdir/.shipyard"
+mkdir -p "${shipyard_dir}"
+chmod -R 777 "/workdir"
+kind get kubeconfig --name="kind" > "${shipyard_dir}/config"
