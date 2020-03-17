028c9e2ab15b35eb3fc4c09f44d028a823d5013a
Jose Armesto <github@armesto.net>
2020-03-17 12:28:30 +0100

    Use kind get kubeconfig output to generate kubeconfig file

--- integration/config/setup.sh
+++ integration/config/setup.sh
@@ -3,4 +3,4 @@
 shipyard_dir=".e2e-harness/workdir/.shipyard"
 mkdir -p "${shipyard_dir}"
 chmod -R 777 ".e2e-harness"
-cp "$(kind get kubeconfig-path --name="kind")" "${shipyard_dir}/config"
+kind get kubeconfig --name="kind" > "${shipyard_dir}/config"
