3e464e9a9fa1627b39b31fc2d4a03c4657a9eede
Jose Armesto <github@armesto.net>
2020-03-17 11:18:56 +0100

    No need to send to metrics channel already registered metrics

--- service/collector/usage.go
+++ service/collector/usage.go
@@ -149,7 +149,6 @@ func (u *Usage) Collect(ch chan<- prometheus.Metric) error {
 		if err != nil {
 			u.logger.Log("level", "warning", "message", "an error occurred during the scraping of current compute resource usage information", "stack", fmt.Sprintf("%v", err))
 			u.usageScrapeError.Inc()
-			ch <- u.usageScrapeError
 		} else {
 			for r.NotDone() {
 				for _, v := range r.Values() {
