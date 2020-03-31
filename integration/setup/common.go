3f958adc87075d6c22d738d878f22bf8bf7042c1
Jose Armesto <github@armesto.net>
2020-03-31 20:03:18 +0200

    Create release CRD's

--- integration/setup/common.go
+++ integration/setup/common.go
@@ -4,9 +4,14 @@ import (
 	"context"
 
 	corev1alpha1 "github.com/giantswarm/apiextensions/pkg/apis/core/v1alpha1"
+	releasev1alpha1 "github.com/giantswarm/apiextensions/pkg/apis/release/v1alpha1"
+	"github.com/giantswarm/backoff"
 	"github.com/giantswarm/e2e-harness/pkg/release"
 	"github.com/giantswarm/e2etemplates/pkg/chartvalues"
 	"github.com/giantswarm/microerror"
+	apiextensionsv1beta1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"github.com/giantswarm/azure-operator/integration/env"
 	"github.com/giantswarm/azure-operator/integration/key"
@@ -80,6 +85,38 @@ func common(ctx context.Context, config Config) error {
 		if err != nil {
 			return microerror.Mask(err)
 		}
+	}
+
+	{
+		err := ensureCRDCreated(ctx, config, *releasev1alpha1.NewReleaseCRD())
+		if err != nil {
+			return microerror.Mask(err)
+		}
+
+		err = ensureCRDCreated(ctx, config, *releasev1alpha1.NewReleaseCycleCRD())
+		if err != nil {
+			return microerror.Mask(err)
+		}
+	}
+
+	return nil
+}
+
+func ensureCRDCreated(ctx context.Context, config Config, crd apiextensionsv1beta1.CustomResourceDefinition) error {
+	o := func() error {
+		_, err := config.K8sClients.ExtClient().ApiextensionsV1beta1().CustomResourceDefinitions().Get(crd.Name, metav1.GetOptions{})
+		if apierrors.IsNotFound(err) {
+			return microerror.Mask(err)
+		} else if err != nil {
+			return backoff.Permanent(microerror.Mask(err))
+		}
+		return nil
+	}
+	b := backoff.NewExponential(backoff.ShortMaxWait, backoff.ShortMaxInterval)
+	n := backoff.NewNotifier(config.Logger, ctx)
+	err := backoff.RetryNotify(o, b, n)
+	if err != nil {
+		return microerror.Mask(err)
 	}
 
 	return nil
