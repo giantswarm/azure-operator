4846fecbe98c7cdeec9fcfc8f07f37f3fd3e71f2
Christian Bianchi <christian@giantswarm.io>
2020-04-17 11:20:08 +0200

    CHeck for VMSS rates before scaling up worker nodes (#780)
    Check for VMSS rates before scaling up worker nodes

--- service/controller/resource/instance/internal/vmsscheck/vmss.go
+++ service/controller/resource/instance/internal/vmsscheck/vmss.go
@@ -14,6 +14,31 @@ import (
 	"github.com/giantswarm/azure-operator/service/controller/controllercontext"
 )
 
+const (
+	// Max number of HighCostGetVMScaleSet calls that can be made during a 30-minute period
+	remainingCallsMax30m = 900
+
+	// Max number of HighCostGetVMScaleSet calls that can be made during a 3-minute period
+	remainingCallsMax3m = 190
+
+	// Key used to extract remaining number of calls for 30 minutes from remainingCallsHeaderName
+	remainingCallsHeaderKey30m = "Microsoft.Compute/HighCostGetVMScaleSet30Min"
+
+	// Key used to extract remaining number of calls for 3 minutes from remainingCallsHeaderName
+	remainingCallsHeaderKey3m = "Microsoft.Compute/HighCostGetVMScaleSet3Min"
+
+	// Response header name that has info about remaining number of HighCostGetVMScaleSet calls.
+	// Header example:
+	// Microsoft.Compute/HighCostGetVMScaleSet3Min;107,Microsoft.Compute/HighCostGetVMScaleSet30Min;827
+	remainingCallsHeaderName = "X-Ms-Ratelimit-Remaining-Resource"
+
+	// If the number of remaining calls for 30min drops below this threshold, we do not proceed
+	remainingCallsThreshold30m = remainingCallsMax30m * 0.5
+
+	// If the number of remaining calls for 3min drops below this threshold, we do not proceed
+	remainingCallsThreshold3m = remainingCallsMax3m * 0.5
+)
+
 // Find out provisioning state of all VMSS instances and return true if all are
 // Succeeded.
 func InstancesAreRunning(ctx context.Context, logger micrologger.Logger, rg string, vmssName string) (bool, error) {
@@ -45,6 +70,16 @@ func InstancesAreRunning(ctx context.Context, logger micrologger.Logger, rg stri
 
 		if err := iterator.Next(); err != nil {
 			return false, microerror.Mask(err)
+		}
+	}
+
+	if allSucceeded {
+		// All instances are succeeded, let's check the VMSS rate is safe.
+		// If current remaining API calls are less than the desired threshold, we don't proceed.
+		rl3m, rl30m := rateLimitThresholdsFromResponse(iterator.Response().Response)
+		if rl3m < remainingCallsThreshold3m || rl30m < remainingCallsThreshold30m {
+			logger.LogCtx(ctx, "level", "warning", "message", fmt.Sprintf("The VMSS API remaining calls are not safe to continue (3m %d/%d, 30m %d/%d)", rl3m, remainingCallsMax3m, rl30m, remainingCallsMax30m)) // nolint: errcheck
+			return false, vmssUnsafeError
 		}
 	}
 
