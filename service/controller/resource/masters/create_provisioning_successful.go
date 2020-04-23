16c9ad37bc05c424ec023c597a98255e8be9f937
Christian Bianchi <christian@giantswarm.io>
2020-04-23 10:43:51 +0200

    Added implementation to 3 more transition functions for the new `masters` resource (#797)

---
+++ service/controller/resource/masters/create_provisioning_successful.go
@@ -0,0 +1,12 @@
+package masters
+
+import (
+	"context"
+
+	"github.com/giantswarm/azure-operator/service/controller/internal/state"
+)
+
+func (r *Resource) provisioningSuccessfulTransition(ctx context.Context, obj interface{}, currentState state.State) (state.State, error) {
+	r.logger.LogCtx(ctx, "level", "debug", "message", "Master VMSS deployment successfully provisioned")
+	return ClusterUpgradeRequirementCheck, nil
+}
