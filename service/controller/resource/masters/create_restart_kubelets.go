d95b39262f9a5ef5d14aa8dd8a962bf908e53b8d
Whites11 <whites11@gmail.com>
2020-04-27 15:58:46 +0200

    Check if API server is up before restarting kubelets on old workers during flatcar migration

--- service/controller/resource/masters/create_restart_kubelets.go
+++ service/controller/resource/masters/create_restart_kubelets.go
@@ -6,12 +6,28 @@ import (
 
 	"github.com/Azure/azure-sdk-for-go/services/compute/mgmt/2019-07-01/compute"
 	"github.com/giantswarm/microerror"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
+	"github.com/giantswarm/azure-operator/service/controller/controllercontext"
 	"github.com/giantswarm/azure-operator/service/controller/internal/state"
 	"github.com/giantswarm/azure-operator/service/controller/key"
 )
 
 func (r *Resource) restartKubeletOnWorkersTransition(ctx context.Context, obj interface{}, currentState state.State) (state.State, error) {
+	// Check if API server is up or wait.
+	r.logger.LogCtx(ctx, "level", "debug", "message", "Checking if API server is up")
+	up, err := r.isApiServerUP(ctx)
+	if err != nil || !up {
+		msg := "API server is NOT up."
+		if err != nil {
+			msg = fmt.Sprintf("%s Original error was %s", msg, err.Error())
+		}
+		r.logger.LogCtx(ctx, "level", "debug", "message", msg)
+
+		return currentState, nil
+	}
+	r.logger.LogCtx(ctx, "level", "debug", "message", "API server is up")
+
 	cr, err := key.ToCustomResource(obj)
 	if err != nil {
 		return "", microerror.Mask(err)
@@ -62,4 +78,22 @@ func (r *Resource) restartKubeletOnWorkersTransition(ctx context.Context, obj in
 	}
 
 	return DeploymentCompleted, nil
+}
+
+func (r *Resource) isApiServerUP(ctx context.Context) (bool, error) {
+	cc, err := controllercontext.FromContext(ctx)
+	if err != nil {
+		return false, microerror.Mask(err)
+	}
+
+	if cc.Client.TenantCluster.K8s == nil {
+		return false, clientNotFoundError
+	}
+
+	_, err = cc.Client.TenantCluster.K8s.CoreV1().Nodes().List(metav1.ListOptions{})
+	if err != nil {
+		return false, microerror.Mask(err)
+	}
+
+	return true, nil
 }
