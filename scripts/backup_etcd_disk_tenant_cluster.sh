71928fdc2100fca570083b59b58014e27ab2468c
Whites11 <whites11@gmail.com>
2020-04-27 12:23:22 +0200

    Added scripts

---
+++ scripts/backup_etcd_disk_tenant_cluster.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+set -e
+
+installation=$1
+cluster=$2
+
+if [ "$installation" == "" ] || [ "$cluster" == "" ]
+then
+    echo "Usage $0 <installation name> <cluster name>"
+    exit 1
+fi
+
+read -p "Installation: $installation, cluster: $cluster. Hit enter to continue, or ctrl+c to abort. "
+
+echo -n "Creating kubeconfig for $installation: "
+opsctl create kubeconfig -i $installation >/dev/null
+echo "OK"
+echo -n "Creating kubeconfig for $cluster @ $installation: "
+gsctl create kubeconfig --cluster=$cluster --certificate-organizations system:masters -e $installation >/dev/null
+echo "OK"
+
+echo -n "Getting master node name: "
+master="`kubectl --context=giantswarm-$cluster get no -l role=master|grep -v NAME|awk '{print $1}'`"
+
+if [ "$master" == "" ]
+then
+    echo "ERR"
+    exit 2
+fi
+
+echo "OK"
+
+echo -n "Stopping API server: "
+opsctl ssh $installation $master --cmd "sudo mv /etc/kubernetes/manifests/k8s-api-server.yaml /root || true" 2>&1 >/dev/null
+echo "OK"
+echo -n "Stopping ETCD: "
+opsctl ssh $installation $master --cmd "sudo systemctl stop etcd3" 2>&1 >/dev/null
+echo "OK"
+
+tar_filename="etcd-backup-$cluster.tar.gz"
+remote_tar_path="/tmp/$tar_filename"
+local_tar_path="./$tar_filename"
+
+echo -n "Creating tar archive from the ETCD directory on $master: "
+rm -f $local_tar_path
+opsctl ssh $installation $master --cmd "sudo tar -C /var/lib/etcd -czf $remote_tar_path ." 2>&1 >/dev/null
+echo "OK"
+echo "Copying ETCD tar archive locally: "
+opsctl scp $installation $master:$remote_tar_path "./$tar_filename"
+echo "Archive copied correctly in $local_tar_path"
