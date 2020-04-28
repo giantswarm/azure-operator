943a6b0e44c66d978ac2246992eda0cb6ba972ba
Nikola Prokopić <nikola@prokopic.rs>
2020-04-28 16:30:47 +0200

    Version bump to v3.1.0-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA             = "n/a"
 	name        string = "azure-operator"
 	source      string = "https://github.com/giantswarm/azure-operator"
-	version            = "3.0.7"
+	version            = "3.1.0-dev"
 )
 
 func Description() string {
