96e058d71d33c47ab114d54e7952f809601fcd48
Whites11 <whites11@gmail.com>
2020-04-29 18:02:10 +0200

    Block master connection while upgrading to flatcar

--- scripts/restore_etcd_disk_tenant_cluster.sh
+++ scripts/restore_etcd_disk_tenant_cluster.sh
@@ -28,7 +28,7 @@ read -p "Installation: $installation, cluster: $cluster. Hit enter to continue, 
 
 # check for needed commands
 echo "Checking for presence of mandatory commands in the system."
-for required in gsctl opsctl
+for required in gsctl opsctl az
 do
   echo -n "Looking for command $required: "
   if [ ! -x "$(command -v $required)" ]
@@ -41,70 +41,32 @@ do
   echo "OK"
 done
 
-echo -n "Creating kubeconfig for $installation: "
-opsctl create kubeconfig -i $installation >/dev/null
-check_err $?
-echo -n "Creating kubeconfig for $cluster @ $installation: "
-output="$(gsctl create kubeconfig --cluster=$cluster --certificate-organizations system:masters -e $installation --ttl 1d 2>&1)"
+echo -n "Connecting to azure: "
+output="$(az account list -o table)"
 check_err $? "${output}"
 
-echo -n "Getting master node name from kubernetes API: "
-# try with kubectl
-master=""
-output="$(kubectl --context=giantswarm-$cluster get no -l role=master 2>&1)"
-if [ $? -eq 0 ]
+echo "Check if the correct subscription is selected."
+echo "$output"
+
+echo ""
+echo "Check in the table above if the subscription your cluster is running on is the one with 'IsDefault' as True"
+read -p "Hit enter to continue or ctrl+c to abort"
+
+# try with az
+echo -n "Getting master node IP address from azure CLI: "
+output="$(az vmss nic list -g "${cluster}" --vmss-name "${cluster}-master-${cluster}" 2>&1)"
+check_err $? "$output"
+master="$(echo "$output" | jq -r '.[0].ipConfigurations[0].privateIpAddress')"
+if [ "$master" == "null" ]
 then
-  master="$(echo "${output}"|grep -v NAME|awk '{print $1}')"
-  if [ $? -ne 0 ] || [ "$master" == "" ]
-  then
-    echo "WARN"
-    echo "Failed getting master node name from kubernetes API, trying with azure CLI client."
-  else
-    echo "OK"
-  fi
-else
-  echo "WARN"
-  echo "${output}"
+    echo "Unexpected ip address got from Azure, aborting."
+    exit 2
 fi
 
-if [ "$master" == "" ]
-then
-  echo -n "Checking if 'az' is available: "
-  if [ -x "$(command -v az)" ]
-  then
-    echo "OK"
-    echo -n "Connecting to azure: "
-    output="$(az account list -o table)"
-    check_err $? "${output}"
-
-    echo "Check if the correct subscription is selected."
-    echo "$output"
-
-    echo ""
-    echo "Check in the table above if the subscription your cluster is running on is the one with 'IsDefault' as True"
-    read -p "Hit enter to continue or ctrl+c to abort"
-  else
-    echo "ERR"
-    echo "The az command was not found in your system, can't get master node hostname."
-    exit 1
-  fi
-
-  # try with az
-  echo -n "Getting master node name from azure CLI: "
-  output="$(az vmss list-instances -g "${cluster}" --name "${cluster}-master-${cluster}" 2>&1)"
-  check_err $? "$output"
-  master="$(echo "$output" | jq -r '.[0].osProfile.computerName')"
-  if [ "$master" == "null" ]
-  then
-      echo "Unexpected server name got from Azure, aborting."
-      exit 2
-  fi
-fi
-
-echo "Master node name is '$master'"
+echo "Master node IP is '$master'"
 
 echo -n "Stopping API server: "
-output="$(opsctl ssh $installation $master --cmd "sudo mv /etc/kubernetes/manifests/k8s-api-server.yaml /root || true" 2>&1)"
+output="$(opsctl ssh $installation master1 --cmd "ssh $master \"sudo mv /etc/kubernetes/manifests/k8s-api-server.yaml /root || true\"" 2>&1)"
 check_err $? "$output"
 
 echo -n "Waiting 10 seconds to ensure the api server goes down: "
@@ -112,10 +74,10 @@ sleep 10
 echo "OK"
 
 echo -n "Stopping Kubelet: "
-output="$(opsctl ssh $installation $master --cmd "sudo systemctl stop k8s-kubelet" 2>&1)"
+output="$(opsctl ssh $installation master1 --cmd "ssh $master \"sudo systemctl stop k8s-kubelet\"" 2>&1)"
 check_err $? "$output"
 echo -n "Stopping ETCD: "
-output="$(opsctl ssh $installation $master --cmd "sudo systemctl stop etcd3" 2>&1)"
+output="$(opsctl ssh $installation master1 --cmd "ssh $master \"sudo systemctl stop etcd3\"" 2>&1)"
 check_err $? "$output"
 
 tar_filename="etcd-backup-$cluster.tar.gz"
@@ -125,28 +87,29 @@ local_tar_path="./$tar_filename"
 set -e
 
 echo "Copying ETCD tar archive remotely: "
-opsctl scp $installation "$local_tar_path" $master:$remote_tar_path
+opsctl scp $installation "$local_tar_path" master1:$remote_tar_path
+opsctl ssh $installation master1 --cmd "scp $remote_tar_path $master:$remote_tar_path"
 echo "Archive copied correctly in $master:$remote_tar_path"
 
 set +e
 
 echo -n "Clearing etcd directory: "
-output="$(opsctl ssh $installation $master --cmd "sudo rm -rf /var/lib/etcd/*" 2>&1)"
+output="$(opsctl ssh $installation master1 --cmd "ssh $master \"sudo rm -rf /var/lib/etcd/*\"" 2>&1)"
 check_err $? "$output"
 
 echo -n "Restoring archive to ETCD directory on $master: "
-output="$(opsctl ssh $installation $master --cmd "sudo tar -xf $remote_tar_path -C /var/lib/etcd/" 2>&1)"
+output="$(opsctl ssh $installation master1 --cmd "ssh $master \"sudo tar -xf $remote_tar_path -C /var/lib/etcd/\"" 2>&1)"
 check_err $? "$output"
 
 echo -n "Starting API server: "
-output="$(opsctl ssh $installation $master --cmd "sudo mv /root/k8s-api-server.yaml /etc/kubernetes/manifests/ || true" 2>&1)"
+output="$(opsctl ssh $installation master1 --cmd "ssh $master \"sudo mv /root/k8s-api-server.yaml /etc/kubernetes/manifests/ || true\"" 2>&1)"
 check_err $? "$output"
 
 set +e
 
-echo -n "Rebooting node "
-output="$(opsctl ssh $installation $master --cmd "sudo reboot" 2>&1)"
-echo "OK"
+echo -n "Rebooting node: "
+output="$(opsctl ssh $installation master1 --cmd "ssh $master \"sudo reboot\"" 2>&1)"
+check_err $? "$output"
 
 cmd="opsctl update status -i $installation -p apis/provider.giantswarm.io/v1alpha1/namespaces/default/azureconfigs/${cluster}/status"
 echo "The restore process is completed."
--- service/controller/resource/masters/create_block_api_calls.go
+++ service/controller/resource/masters/create_block_api_calls.go
@@ -13,7 +13,7 @@ import (
 )
 
 const (
-	temporarySecurityRuleName = "temporarilyBlockApiAccess"
+	temporarySecurityRuleName = "temporaryFlatcarMigration"
 )
 
 func (r *Resource) blockAPICallsTransition(ctx context.Context, obj interface{}, currentState state.State) (state.State, error) {
@@ -22,66 +22,7 @@ func (r *Resource) blockAPICallsTransition(ctx context.Context, obj interface{},
 		return currentState, microerror.Mask(err)
 	}
 
-	r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("Looking for security rule %s in security group %s", temporarySecurityRuleName, key.WorkerSecurityGroupName(cr)))
-
-	exists, err := r.securityRuleExists(ctx, key.ResourceGroupName(cr), key.WorkerSecurityGroupName(cr), temporarySecurityRuleName)
-	if err != nil {
-		// In case of error just retry.
-		return currentState, microerror.Mask(err)
-	}
-
-	if !exists {
-		// Create security rule
-		r.logger.LogCtx(ctx, "level", "debug", "message", "Creating security rule")
-		err = r.createSecurityRule(ctx, key.ResourceGroupName(cr), key.WorkerSecurityGroupName(cr), temporarySecurityRuleName)
-		if err != nil {
-			// In case of error just retry.
-			return currentState, microerror.Mask(err)
-		}
-
-		r.logger.LogCtx(ctx, "level", "debug", "message", "Security rule created")
-
-		// Wait for security rule to be in place.
-		return currentState, nil
-	}
-
-	r.logger.LogCtx(ctx, "level", "debug", "message", "Security rule found")
-
-	return DeploymentUninitialized, nil
-}
-
-func (r *Resource) unblockAPICallsTransition(ctx context.Context, obj interface{}, currentState state.State) (state.State, error) {
-	cr, err := key.ToCustomResource(obj)
-	if err != nil {
-		return currentState, microerror.Mask(err)
-	}
-
-	r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("deleting security rule %s from security group %s", temporarySecurityRuleName, key.WorkerSecurityGroupName(cr)))
-
-	// Delete security rule
-	err = r.deleteSecurityRule(ctx, key.ResourceGroupName(cr), key.WorkerSecurityGroupName(cr), temporarySecurityRuleName)
-	if IsNotFound(err) {
-		// Rule not exists, ok to continue.
-		r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("security rule %s from security group %s was not found", temporarySecurityRuleName, key.WorkerSecurityGroupName(cr)))
-
-		return RestartKubeletOnWorkers, nil
-	} else if err != nil {
-		// In case of error just retry.
-		return currentState, microerror.Mask(err)
-	}
-
-	r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("deleted security rule %s from security group %s", temporarySecurityRuleName, key.WorkerSecurityGroupName(cr)))
-
-	return RestartKubeletOnWorkers, nil
-}
-
-func (r *Resource) createSecurityRule(ctx context.Context, resourceGroup string, securityGroupname string, securityRuleName string) error {
-	c, err := r.getSecurityRulesClient(ctx)
-	if err != nil {
-		return microerror.Mask(err)
-	}
-
-	_, err = c.CreateOrUpdate(context.Background(), resourceGroup, securityGroupname, securityRuleName, network.SecurityRule{
+	workersRule := network.SecurityRule{
 		SecurityRulePropertiesFormat: &network.SecurityRulePropertiesFormat{
 			Access:                   network.SecurityRuleAccessDeny,
 			Description:              to.StringPtr("Temporarily block API access from workers during flatcar migration"),
@@ -93,7 +34,108 @@ func (r *Resource) createSecurityRule(ctx context.Context, resourceGroup string,
 			SourceAddressPrefix:      to.StringPtr("*"),
 			SourcePortRange:          to.StringPtr("*"),
 		},
-	})
+	}
+
+	mastersRule := network.SecurityRule{
+		SecurityRulePropertiesFormat: &network.SecurityRulePropertiesFormat{
+			Access:                   network.SecurityRuleAccessDeny,
+			Description:              to.StringPtr("Temporarily block internet access to masters during flatcar migration"),
+			DestinationPortRange:     to.StringPtr("*"),
+			DestinationAddressPrefix: to.StringPtr("*"),
+			Direction:                network.SecurityRuleDirectionOutbound,
+			Protocol:                 network.SecurityRuleProtocolAsterisk,
+			Priority:                 to.Int32Ptr(3000),
+			SourceAddressPrefix:      to.StringPtr("*"),
+			SourcePortRange:          to.StringPtr("*"),
+		},
+	}
+
+	workerFound, err := r.ensureSecurityRule(ctx, key.ResourceGroupName(cr), key.WorkerSecurityGroupName(cr), temporarySecurityRuleName, workersRule)
+	if err != nil {
+		return currentState, microerror.Mask(err)
+	}
+
+	masterFound, err := r.ensureSecurityRule(ctx, key.ResourceGroupName(cr), key.MasterSecurityGroupName(cr), temporarySecurityRuleName, mastersRule)
+	if err != nil {
+		return currentState, microerror.Mask(err)
+	}
+
+	if !masterFound || !workerFound {
+		r.logger.LogCtx(ctx, "level", "debug", "message", "Security rules not in place yet")
+		return currentState, nil
+	}
+
+	r.logger.LogCtx(ctx, "level", "debug", "message", "Security rules found")
+
+	return DeploymentUninitialized, nil
+}
+
+func (r *Resource) ensureSecurityRule(ctx context.Context, resourceGroup string, securityGroupName string, ruleName string, rule network.SecurityRule) (bool, error) {
+	r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("Looking for security rule %s in security group %s", ruleName, securityGroupName))
+
+	exists, err := r.securityRuleExists(ctx, resourceGroup, securityGroupName, ruleName)
+	if err != nil {
+		return false, microerror.Mask(err)
+	}
+
+	if !exists {
+		// Create security rule
+		r.logger.LogCtx(ctx, "level", "debug", "message", "Creating security rule")
+		err = r.createSecurityRule(ctx, resourceGroup, securityGroupName, ruleName, rule)
+		if err != nil {
+			return false, microerror.Mask(err)
+		}
+
+		r.logger.LogCtx(ctx, "level", "debug", "message", "Security rule created")
+
+		// Wait for security rule to be in place.
+		return false, nil
+	}
+
+	// Security rule in place
+	return true, nil
+}
+
+func (r *Resource) unblockAPICallsTransition(ctx context.Context, obj interface{}, currentState state.State) (state.State, error) {
+	cr, err := key.ToCustomResource(obj)
+	if err != nil {
+		return currentState, microerror.Mask(err)
+	}
+
+	r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("deleting security rule %s from security group %s", temporarySecurityRuleName, key.WorkerSecurityGroupName(cr)))
+
+	// Delete security rule for workers
+	err = r.deleteSecurityRule(ctx, key.ResourceGroupName(cr), key.WorkerSecurityGroupName(cr), temporarySecurityRuleName)
+	if IsNotFound(err) {
+		// Rule not exists, ok to continue.
+		r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("security rule %s from security group %s was not found", temporarySecurityRuleName, key.WorkerSecurityGroupName(cr)))
+	} else if err != nil {
+		// In case of error just retry.
+		return currentState, microerror.Mask(err)
+	}
+
+	// Delete security rule for masters
+	err = r.deleteSecurityRule(ctx, key.ResourceGroupName(cr), key.MasterSecurityGroupName(cr), temporarySecurityRuleName)
+	if IsNotFound(err) {
+		// Rule not exists, ok to continue.
+		r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("security rule %s from security group %s was not found", temporarySecurityRuleName, key.MasterSecurityGroupName(cr)))
+	} else if err != nil {
+		// In case of error just retry.
+		return currentState, microerror.Mask(err)
+	}
+
+	r.logger.LogCtx(ctx, "level", "debug", "message", "deleted temporary security rules")
+
+	return RestartKubeletOnWorkers, nil
+}
+
+func (r *Resource) createSecurityRule(ctx context.Context, resourceGroup string, securityGroupname string, securityRuleName string, securityRule network.SecurityRule) error {
+	c, err := r.getSecurityRulesClient(ctx)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	_, err = c.CreateOrUpdate(context.Background(), resourceGroup, securityGroupname, securityRuleName, securityRule)
 	if err != nil {
 		return microerror.Mask(err)
 	}
