3317075faa398c7cdaa052e6f1f2d32b7525c0db
jihyuk <jhbok@giantswarm.io>
2020-04-29 16:32:35 +0200

    update version

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA             = "n/a"
 	name        string = "azure-operator"
 	source      string = "https://github.com/giantswarm/azure-operator"
-	version            = "3.0.4"
+	version            = "3.0.4-1"
 )
 
 func Description() string {
