d1fc79106e2b69ddd107c79d9e6b7490f5e2c219
Whites11 <whites11@gmail.com>
2020-04-30 17:42:17 +0200

    Removed wrong comment

--- service/controller/resource/masters/create_manual_intervention_needed.go
+++ service/controller/resource/masters/create_manual_intervention_needed.go
@@ -6,7 +6,6 @@ import (
 	"github.com/giantswarm/azure-operator/service/controller/internal/state"
 )
 
-// This transition function aims at detecting if the master VMSS needs to be migrated from CoreOS to flatcar.
 func (r *Resource) manualInterventionRequiredTransition(ctx context.Context, obj interface{}, currentState state.State) (state.State, error) {
 	r.logger.LogCtx(ctx, "level", "error", "message", "The reconciliation on the masters resource can't continute. Manual intervention needed.")
 	return currentState, nil
