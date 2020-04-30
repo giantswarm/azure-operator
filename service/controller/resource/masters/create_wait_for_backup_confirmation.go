ed58dc0d09c17a8fcca3a4b50f991e44c71efac8
Whites11 <whites11@gmail.com>
2020-04-30 17:46:10 +0200

    Removed wrong comment

--- service/controller/resource/masters/create_wait_for_backup_confirmation.go
+++ service/controller/resource/masters/create_wait_for_backup_confirmation.go
@@ -7,7 +7,6 @@ import (
 	"github.com/giantswarm/azure-operator/service/controller/internal/state"
 )
 
-// This transition function aims at detecting if the master VMSS needs to be migrated from CoreOS to flatcar.
 func (r *Resource) waitForBackupConfirmationTransition(ctx context.Context, obj interface{}, currentState state.State) (state.State, error) {
 	r.logger.LogCtx(ctx, "level", "warning", "message", fmt.Sprintf("The reconciliation on the masters resource is stopped until the legacy VMSS instance ETCD backup is completed. When you completed the backup, set the masters's resource status to '%s'", DeallocateLegacyInstance))
 	return currentState, nil
