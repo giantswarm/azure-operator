a1f7bc1e8e3977066c7a3268874919afea955059
Whites11 <whites11@gmail.com>
2020-04-30 12:22:11 +0200

    use az to get instance name

--- scripts/backup_etcd_disk_tenant_cluster.sh
+++ scripts/backup_etcd_disk_tenant_cluster.sh
@@ -18,6 +18,29 @@ check_err () {
   fi
 }
 
+check_warn () {
+  r=$?
+  if [ $r -eq 0 ]
+  then
+    echo "OK"
+  else
+    echo "WARN"
+    if [ "$2" != "" ]
+    then
+      echo "$2"
+    fi
+  fi
+}
+
+run_cmd_on_server () {
+  desc=$1
+  master=$2
+  cmd=$3
+  echo -n "$desc: "
+  output="$(opsctl ssh $installation master1 --cmd "ssh -oStrictHostKeyChecking=no $master \"$cmd\"" 2>&1)"
+  check_err $? "$output"
+}
+
 if [ "$installation" == "" ] || [ "$cluster" == "" ]
 then
     echo "Usage $0 <installation name> <cluster name>"
@@ -28,7 +51,7 @@ read -p "Installation: $installation, cluster: $cluster. Hit enter to continue, 
 
 # check for needed commands
 echo "Checking for presence of mandatory commands in the system."
-for required in gsctl opsctl
+for required in gsctl opsctl az jq
 do
   echo -n "Looking for command $required: "
   if [ ! -x "$(command -v $required)" ]
@@ -41,92 +64,57 @@ do
   echo "OK"
 done
 
-echo -n "Creating kubeconfig for $installation: "
-opsctl create kubeconfig -i $installation >/dev/null
-check_err $?
 echo -n "Creating kubeconfig for $cluster @ $installation: "
 output="$(gsctl create kubeconfig --cluster=$cluster --certificate-organizations system:masters -e $installation --ttl 1d 2>&1)"
 check_err $? "${output}"
 
-echo -n "Getting master node name from kubernetes API: "
-# try with kubectl
-master=""
-output="$(kubectl --context=giantswarm-$cluster get no -l role=master 2>&1)"
-if [ $? -eq 0 ]
+echo -n "Connecting to azure: "
+output="$(az account list -o table)"
+check_err $? "${output}"
+
+echo "Check if the correct subscription is selected."
+echo "$output"
+
+echo ""
+echo "Check in the table above if the subscription your cluster is running on is the one with 'IsDefault' as True"
+read -p "Hit enter to continue or ctrl+c to abort"
+
+# try with az
+echo -n "Getting master node IP address from azure CLI: "
+output="$(az vmss nic list -g "${cluster}" --vmss-name "${cluster}-master" 2>&1)"
+check_err $? "$output"
+master="$(echo "$output" | jq -r '.[0].ipConfigurations[0].privateIpAddress')"
+if [ "$master" == "null" ]
 then
-  master="$(echo "${output}"|grep -v NAME|awk '{print $1}')"
-  if [ $? -ne 0 ] || [ "$master" == "" ]
-  then
-    echo "WARN"
-    echo "Failed getting master node name from kubernetes API, trying with azure CLI client."
-  else
-    echo "OK"
-  fi
-else
-  echo "WARN"
-  echo "${output}"
+    echo "Unexpected ip address got from Azure, aborting."
+    exit 2
 fi
 
-if [ "$master" == "" ]
-then
-  echo -n "Checking if 'az' is available: "
-  if [ -x "$(command -v az)" ]
-  then
-    echo "OK"
-    echo -n "Connecting to azure: "
-    output="$(az account list -o table)"
-    check_err $? "${output}"
+echo "Master node IP is '$master'"
 
-    echo "Check if the correct subscription is selected."
-    echo "$output"
-
-    echo ""
-    echo "Check in the table above if the subscription your cluster is running on is the one with 'IsDefault' as True"
-    read -p "Hit enter to continue or ctrl+c to abort"
-  else
-    echo "ERR"
-    echo "The az command was not found in your system, can't get master node hostname."
-    exit 1
-  fi
-
-  # try with az
-  echo -n "Getting master node name from azure CLI: "
-  output="$(az vmss list-instances -g "${cluster}" --name "${cluster}-master" 2>&1)"
-  check_err $? "$output"
-  master="$(echo "$output" | jq -r '.[0].osProfile.computerName')"
-  if [ "$master" == "null" ]
-  then
-      echo "Unexpected server name got from Azure, aborting."
-      exit 2
-  fi
-fi
-
-echo "Master node name is '$master'"
-
-echo "Removing master node from cluster"
+# This step fails if the API server is down.
+echo -n "Removing master node from cluster: "
 output="$(kubectl --context=giantswarm-$cluster delete node -l role=master 2>&1)"
+check_warn $? "$output"
 
-echo -n "Stopping API server: "
-output="$(opsctl ssh $installation $master --cmd "sudo mv /etc/kubernetes/manifests/k8s-api-server.yaml /root || true" 2>&1)"
-check_err $? "$output"
-echo -n "Stopping ETCD: "
-output="$(opsctl ssh $installation $master --cmd "sudo systemctl stop etcd3" 2>&1)"
-check_err $? "$output"
+run_cmd_on_server "Stopping API server" $master "sudo mv /etc/kubernetes/manifests/k8s-api-server.yaml /root || true"
+run_cmd_on_server "Stopping ETCD" $master "sudo systemctl stop etcd3"
 
 tar_filename="etcd-backup-$cluster.tar.gz"
 remote_tar_path="/tmp/$tar_filename"
 local_tar_path="./$tar_filename"
-
-echo -n "Creating tar archive from the ETCD directory on $master: "
 rm -f $local_tar_path
-output="$(opsctl ssh $installation $master --cmd "sudo tar -C /var/lib/etcd -czf $remote_tar_path ." 2>&1)"
-check_err $? "$output"
+
+run_cmd_on_server "Creating tar archive from the ETCD directory on $master" $master "sudo tar -C /var/lib/etcd -czf $remote_tar_path ."
 
 set -e
 
 echo "Copying ETCD tar archive locally: "
-opsctl scp $installation $master:$remote_tar_path "./$tar_filename"
+opsctl ssh $installation master1 --cmd "scp $master:$remote_tar_path $remote_tar_path"
+opsctl scp $installation master1:$remote_tar_path "$local_tar_path"
 echo "Archive copied correctly in $local_tar_path"
+
+set +e
 
 cmd="opsctl update status -i $installation -p apis/provider.giantswarm.io/v1alpha1/namespaces/default/azureconfigs/${cluster}/status"
 echo "The restore process is completed."
