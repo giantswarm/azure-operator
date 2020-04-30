4b5c3242644eaf4b85b86b8bd6ba0e1e640f7af7
Whites11 <whites11@gmail.com>
2020-04-30 14:29:30 +0200

    use az to get instance name

--- scripts/restore_etcd_disk_tenant_cluster.sh
+++ scripts/restore_etcd_disk_tenant_cluster.sh
@@ -18,6 +18,29 @@ check_err () {
   fi
 }
 
+check_warn () {
+  r=$?
+  if [ $r -eq 0 ]
+  then
+    echo "OK"
+  else
+    echo "WARN"
+    if [ "$2" != "" ]
+    then
+      echo "$2"
+    fi
+  fi
+}
+
+run_cmd_on_server () {
+  desc=$1
+  master=$2
+  cmd=$3
+  echo -n "$desc: "
+  output="$(opsctl ssh $installation master1 --cmd "ssh -oStrictHostKeyChecking=no $master \"$cmd\"" 2>&1)"
+  check_err $? "$output"
+}
+
 if [ "$installation" == "" ] || [ "$cluster" == "" ]
 then
     echo "Usage $0 <installation name> <cluster name>"
@@ -28,7 +51,7 @@ read -p "Installation: $installation, cluster: $cluster. Hit enter to continue, 
 
 # check for needed commands
 echo "Checking for presence of mandatory commands in the system."
-for required in gsctl opsctl az
+for required in gsctl opsctl az jq
 do
   echo -n "Looking for command $required: "
   if [ ! -x "$(command -v $required)" ]
@@ -65,20 +88,14 @@ fi
 
 echo "Master node IP is '$master'"
 
-echo -n "Stopping API server: "
-output="$(opsctl ssh $installation master1 --cmd "ssh -oStrictHostKeyChecking=no $master \"sudo mv /etc/kubernetes/manifests/k8s-api-server.yaml /root || true\"" 2>&1)"
-check_err $? "$output"
+run_cmd_on_server "Stopping API server" $master "sudo mv /etc/kubernetes/manifests/k8s-api-server.yaml /root"
 
 echo -n "Waiting 10 seconds to ensure the api server goes down: "
 sleep 10
 echo "OK"
 
-echo -n "Stopping Kubelet: "
-output="$(opsctl ssh $installation master1 --cmd "ssh -oStrictHostKeyChecking=no $master \"sudo systemctl stop k8s-kubelet\"" 2>&1)"
-check_err $? "$output"
-echo -n "Stopping ETCD: "
-output="$(opsctl ssh $installation master1 --cmd "ssh -oStrictHostKeyChecking=no $master \"sudo systemctl stop etcd3\"" 2>&1)"
-check_err $? "$output"
+run_cmd_on_server "Stopping Kubelet" $master "sudo systemctl stop k8s-kubelet"
+run_cmd_on_server "Stopping ETCD" $master "sudo systemctl stop etcd3"
 
 tar_filename="etcd-backup-$cluster.tar.gz"
 remote_tar_path="/tmp/$tar_filename"
@@ -91,25 +108,15 @@ opsctl scp $installation "$local_tar_path" master1:$remote_tar_path
 opsctl ssh $installation master1 --cmd "scp $remote_tar_path $master:$remote_tar_path"
 echo "Archive copied correctly in $master:$remote_tar_path"
 
-set +e
-
-echo -n "Clearing etcd directory: "
-output="$(opsctl ssh $installation master1 --cmd "ssh -oStrictHostKeyChecking=no $master \"sudo rm -rf /var/lib/etcd/*\"" 2>&1)"
-check_err $? "$output"
-
-echo -n "Restoring archive to ETCD directory on $master: "
-output="$(opsctl ssh $installation master1 --cmd "ssh -oStrictHostKeyChecking=no $master \"sudo tar -xf $remote_tar_path -C /var/lib/etcd/\"" 2>&1)"
-check_err $? "$output"
-
-echo -n "Starting API server: "
-output="$(opsctl ssh $installation master1 --cmd "ssh -oStrictHostKeyChecking=no $master \"sudo mv /root/k8s-api-server.yaml /etc/kubernetes/manifests/ || true\"" 2>&1)"
-check_err $? "$output"
+run_cmd_on_server "Clearing etcd directory" $master "sudo rm -rf /var/lib/etcd/*"
+run_cmd_on_server "Restoring archive to ETCD directory on $master" $master "sudo tar -xf $remote_tar_path -C /var/lib/etcd/"
+run_cmd_on_server "Starting API server" $master "sudo mv /root/k8s-api-server.yaml /etc/kubernetes/manifests/ || true"
 
 set +e
 
 echo -n "Rebooting node: "
 output="$(opsctl ssh $installation master1 --cmd "ssh -oStrictHostKeyChecking=no $master \"sudo reboot\"" 2>&1)"
-check_err $? "$output"
+check_warn $? "$output"
 
 cmd="opsctl update status -i $installation -p apis/provider.giantswarm.io/v1alpha1/namespaces/default/azureconfigs/${cluster}/status"
 echo "The restore process is completed."
