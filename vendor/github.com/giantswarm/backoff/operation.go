d61acc768f66503f28f743f1116b34066a53515b
Jose Armesto <github@armesto.net>
2020-05-05 15:00:12 +0200

    Removed vendor folder

--- vendor/github.com/giantswarm/backoff/operation.go
+++
@@ -1,5 +0,0 @@
-package backoff
-
-// An Operation is executing by Retry() or RetryNotify(). The operation will be
-// retried using a backoff policy if it returns an error.
-type Operation func() error
--- vendor/github.com/giantswarm/e2etemplates/pkg/chartvalues/chartvalues.go
+++
@@ -1,5 +0,0 @@
-package chartvalues
-
-const (
-	namespaceGiantswarm = "giantswarm"
-)
