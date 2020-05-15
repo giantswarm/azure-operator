a30736bfcadb48c502c6d4d34418f980bfc37a66
Jose Armesto <github@armesto.net>
2020-05-15 17:09:40 +0200

    Set versions to bypass architect-orb

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA             = "n/a"
 	name        string = "azure-operator"
 	source      string = "https://github.com/giantswarm/azure-operator"
-	//version            = "4.0.0-1"
+	//version            = "4.0.0-2"
 )
 
 func Description() string {
