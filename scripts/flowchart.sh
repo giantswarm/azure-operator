3579c82be4477755a348b5e593bfe53e1a0b6247
Nikola Prokopić <nikola@prokopic.rs>
2020-05-18 15:35:17 +0200

    Added bash script for generating state machine flowcharts

---
+++ scripts/flowchart.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+#
+# Example usage:
+#   flowchart.sh azure-operator-master --tenant-cluster xyz99 --open-browser
+#
+# where 'azure-operator-master' is App CR name and 'xyz99' is tenant cluster ID
+# and '--open-browser' flag indicates that a default browser will open the
+# generated flowchart.
+#
+
+args=()
+
+while [[ $# -gt 0 ]]; do
+    flag="$1"
+    case $flag in
+        -b|--open-browser)
+            flag_open_browser=1
+            shift
+            ;;
+        -t|--tenant-cluster)
+            tenant_cluster_id="$2"
+            shift
+            shift
+            ;;
+        *)
+            args+=("$1")
+            shift
+            ;;
+    esac
+done
+
+# restore positional arguments
+set -- "${args[@]}"
+azure_operator_app=$1
+
+if [ -z "$azure_operator_app" ]; then
+    echo [err] azure-operator CR name must be specified
+    exit 1
+fi
+
+# get logs
+logfile="azop.logs"
+if ! kubectl -n giantswarm logs deployment/${azure_operator_app} > "${logfile}"; then
+    echo "[err] azure-operator app '$azure_operator_app' not found"
+    exit 1
+fi
+
+# filter by event message, get only state change events
+query='. | select(.message | test("state changed")) '
+
+# filter by tenant cluster ID
+if ! [ -z "$tenant_cluster_id" ]; then
+    query+="| select(.object | endswith(\"/azureconfigs/$tenant_cluster_id\")) "
+fi
+
+# echo state transition in format 'stateX --> stateY'
+query+='| "    " +
+    (if (.oldState | length) > 0 then .oldState else "DeploymentUninitialized" end) +
+    " --> " +
+    (if (.newState | length) > 0 then .newState else "DeploymentUninitialized" end)'
+
+# idented transition lines: "    stateX --> stateY"
+transitions=$(cat "${logfile}" \
+    | jq -r "$query" \
+    | sort \
+    | uniq)
+
+mermaid="graph TD
+${transitions}"
+
+template=$(cat flowchart.template.html)
+generated_flowchart="${azure_operator_app}-flowchart.html"
+
+# generate flowchart
+echo "${template/_FLOWCHART_DATA_/$mermaid}" > "${generated_flowchart}"
+echo "Generated flowchart in file '${generated_flowchart}'."
+
+# open default browser
+if [ "$flag_open_browser" == 1 ]; then
+    if which xdg-open > /dev/null; then
+        xdg-open "${generated_flowchart}"
+    elif which gnome-open > /dev/null; then
+        gnome-open "${generated_flowchart}"
+    fi
+fi
