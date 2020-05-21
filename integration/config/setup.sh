9a9349378b848bc8b82e03f18048e401156693b1
Jose Armesto <github@armesto.net>
2020-05-21 15:51:58 +0200

    No need to install Helm anymore

--- integration/config/setup.sh
+++ integration/config/setup.sh
@@ -7,10 +7,5 @@ kind get kubeconfig --name="kind" >"${shipyard_dir}/config"
 
 GO111MODULE=on go get github.com/giantswarm/architect
 
-curl -L https://get.helm.sh/helm-v2.16.1-linux-amd64.tar.gz >./helm.tar.gz
-tar xzvf helm.tar.gz
-chmod u+x linux-amd64/helm
-sudo mv linux-amd64/helm /usr/local/bin/
-
 kubectl --kubeconfig="${shipyard_dir}/config" create clusterrolebinding add-on-cluster-admin --clusterrole=cluster-admin --serviceaccount=kube-system:default
 helm --kubeconfig="${shipyard_dir}/config" init --history-max 5 --wait
