0a4019a9aaa3d0963e77a7c359fc9c8f5a21c5b3
Nikola Prokopić <nikola@prokopic.rs>
2020-05-28 18:04:01 +0200

    Getting StorageAccountsClient from the factory, instead of from controllercontext

--- service/controller/resource/nodes/clients.go
+++ service/controller/resource/nodes/clients.go
@@ -4,20 +4,10 @@ import (
 	"context"
 
 	"github.com/Azure/azure-sdk-for-go/services/compute/mgmt/2019-07-01/compute"
-	"github.com/Azure/azure-sdk-for-go/services/storage/mgmt/2019-04-01/storage"
 	"github.com/giantswarm/microerror"
 
 	"github.com/giantswarm/azure-operator/v4/service/controller/controllercontext"
 )
-
-func (r *Resource) GetStorageAccountsClient(ctx context.Context) (*storage.AccountsClient, error) {
-	cc, err := controllercontext.FromContext(ctx)
-	if err != nil {
-		return nil, microerror.Mask(err)
-	}
-
-	return cc.AzureClientSet.StorageAccountsClient, nil
-}
 
 func (r *Resource) GetVMsClient(ctx context.Context) (*compute.VirtualMachineScaleSetVMsClient, error) {
 	cc, err := controllercontext.FromContext(ctx)
