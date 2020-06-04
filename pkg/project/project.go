242ef5431704d0513234d61f6fe910671d5ae2a7
Nikola Prokopić <nikola@prokopic.rs>
2020-06-04 19:36:29 +0200

    Reverted version

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA             = "n/a"
 	name        string = "azure-operator"
 	source      string = "https://github.com/giantswarm/azure-operator"
-	version            = "4.1.0-clientfactory"
+	version            = "4.1.0-dev"
 )
 
 func Description() string {
