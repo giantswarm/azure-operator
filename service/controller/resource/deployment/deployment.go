157e5443640124d07e73e8ff30eba6e30b8b1186
Christian Bianchi <christian@giantswarm.io>
2020-06-11 11:16:38 +0200

    Add CP subnet to the allowed subnets for storage account in TC (#860)
    * Add CP subnet to the allowed subnets for storage account in TC

--- service/controller/resource/deployment/deployment.go
+++ service/controller/resource/deployment/deployment.go
@@ -2,6 +2,7 @@ package deployment
 
 import (
 	"context"
+	"fmt"
 	"net"
 
 	azureresource "github.com/Azure/azure-sdk-for-go/services/resources/mgmt/2019-05-01/resources"
@@ -22,37 +23,29 @@ func (r Resource) newDeployment(ctx context.Context, customObject providerv1alph
 		return azureresource.Deployment{}, microerror.Mask(err)
 	}
 
-	// We need to authorize the azure operator public IP on the storage account in the tenant cluster.
-	// The azure operator can be running in any of the master or worker nodes in the control plane, so
-	// I have to retrieve the public IP addresses of both node types.
-	var storageAccountIpRules []StorageAccountIpRule
-	publicIps, err := r.getCPPublicIPAddresses(ctx)
-	if err != nil {
-		return azureresource.Deployment{}, microerror.Mask(err)
-	}
-
-	for _, ip := range publicIps {
-		storageAccountIpRules = append(storageAccountIpRules, StorageAccountIpRule{
-			Value:  ip,
-			Action: "Allow",
-		})
-	}
+	controlPlaneWorkerSubnetID := fmt.Sprintf(
+		"/subscriptions/%s/resourceGroups/%s/providers/Microsoft.Network/virtualNetworks/%s/subnets/%s_worker_subnet",
+		r.controlPlaneSubscriptionID,
+		r.installationName,
+		r.azure.HostCluster.VirtualNetwork,
+		r.installationName,
+	)
 
 	defaultParams := map[string]interface{}{
-		"blobContainerName":       key.BlobContainerName(),
-		"calicoSubnetCidr":        key.CalicoCIDR(customObject),
-		"clusterID":               key.ClusterID(customObject),
-		"dnsZones":                key.DNSZones(customObject),
-		"hostClusterCidr":         r.azure.HostCluster.CIDR,
-		"kubernetesAPISecurePort": key.APISecurePort(customObject),
-		"masterSubnetCidr":        key.MastersSubnetCIDR(customObject),
-		"storageAccountName":      key.StorageAccountName(customObject),
-		"storageAccountIpRules":   storageAccountIpRules,
-		"virtualNetworkCidr":      key.VnetCIDR(customObject),
-		"virtualNetworkName":      key.VnetName(customObject),
-		"vnetGatewaySubnetName":   key.VNetGatewaySubnetName(),
-		"vpnSubnetCidr":           vpnSubnet.String(),
-		"workerSubnetCidr":        key.WorkersSubnetCIDR(customObject),
+		"blobContainerName":          key.BlobContainerName(),
+		"calicoSubnetCidr":           key.CalicoCIDR(customObject),
+		"controlPlaneWorkerSubnetID": controlPlaneWorkerSubnetID,
+		"clusterID":                  key.ClusterID(customObject),
+		"dnsZones":                   key.DNSZones(customObject),
+		"hostClusterCidr":            r.azure.HostCluster.CIDR,
+		"kubernetesAPISecurePort":    key.APISecurePort(customObject),
+		"masterSubnetCidr":           key.MastersSubnetCIDR(customObject),
+		"storageAccountName":         key.StorageAccountName(customObject),
+		"virtualNetworkCidr":         key.VnetCIDR(customObject),
+		"virtualNetworkName":         key.VnetName(customObject),
+		"vnetGatewaySubnetName":      key.VNetGatewaySubnetName(),
+		"vpnSubnetCidr":              vpnSubnet.String(),
+		"workerSubnetCidr":           key.WorkersSubnetCIDR(customObject),
 	}
 
 	armTemplate, err := template.GetARMTemplate()
