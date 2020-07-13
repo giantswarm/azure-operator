3ea99a5f787212502bd459680073ffbb84cec3da
Jose Armesto <github@armesto.net>
2020-07-13 17:53:01 +0200

    Add error matchers

--- service/controller/resource/subnet/error.go
+++ service/controller/resource/subnet/error.go
@@ -9,6 +9,11 @@ var invalidConfigError = &microerror.Error{
 	Kind: "invalidConfigError",
 }
 
+// IsInvalidConfigError asserts invalidConfigError.
+func IsInvalidConfigError(err error) bool {
+	return microerror.Cause(err) == invalidConfigError
+}
+
 var notFoundError = &microerror.Error{
 	Kind: "notFoundError",
 }
@@ -50,10 +55,25 @@ var tooManyCredentialsError = &microerror.Error{
 	Kind: "tooManyCredentialsError",
 }
 
+// IsTooManyCredentialsError asserts tooManyCredentialsError.
+func IsTooManyCredentialsError(err error) bool {
+	return microerror.Cause(err) == tooManyCredentialsError
+}
+
 var missingOrganizationLabel = &microerror.Error{
 	Kind: "missingOrganizationLabel",
 }
 
+// IsMissingOrganizationLabel asserts missingOrganizationLabel.
+func IsMissingOrganizationLabel(err error) bool {
+	return microerror.Cause(err) == missingOrganizationLabel
+}
+
 var wrongTypeError = &microerror.Error{
 	Kind: "wrongTypeError",
+}
+
+// IsWrongTypeError asserts wrongTypeError.
+func IsWrongTypeError(err error) bool {
+	return microerror.Cause(err) == wrongTypeError
 }
