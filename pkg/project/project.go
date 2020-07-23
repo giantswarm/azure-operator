6ec0513d6b7bd654433c67cbe4c3ecc17aba3470
Paweł Kopiczko <kopiczko@gmail.com>
2020-07-23 14:19:23 +0100

    resotre version

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA             = "n/a"
 	name        string = "azure-operator"
 	source      string = "https://github.com/giantswarm/azure-operator"
-	version            = "4.2.0-kopiczko"
+	version            = "4.2.0-dev"
 )
 
 func Description() string {
