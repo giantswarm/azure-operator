bbe337f20ac0e944f70a23ea3d8e25de845b1664
Jose Armesto <github@armesto.net>
2020-08-03 12:17:16 +0200

    Package scalestrategy got duplicated after merge, cleaning

--- service/controller/resource/instance/internal/scalestrategy/interface.go
+++
@@ -1,5 +0,0 @@
-package scalestrategy
-
-type Interface interface {
-	GetNodeCount(currentCount int64, desiredCount int64) int64
-}
--- service/controller/resource/instance/internal/scalestrategy/quick.go
+++
@@ -1,8 +0,0 @@
-package scalestrategy
-
-type Quick struct {
-}
-
-func (i Quick) GetNodeCount(currentCount int64, desiredCount int64) int64 {
-	return desiredCount
-}
