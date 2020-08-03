325813d99d0e2c70455fa7d495947f9ee42fc16b
Jose Armesto <github@armesto.net>
2020-08-03 17:24:32 +0200

    Delete bootstrap cloud config blob when deleting the node pool

--- service/controller/resource/cloudconfigblob/delete.go
+++ service/controller/resource/cloudconfigblob/delete.go
@@ -2,8 +2,57 @@ package cloudconfigblob
 
 import (
 	"context"
+	"fmt"
+
+	"github.com/Azure/azure-storage-blob-go/azblob"
+	"github.com/giantswarm/microerror"
+
+	"github.com/giantswarm/azure-operator/v4/service/controller/blobclient"
+	"github.com/giantswarm/azure-operator/v4/service/controller/key"
 )
 
 func (r *Resource) EnsureDeleted(ctx context.Context, obj interface{}) error {
+	azureMachinePool, err := key.ToAzureMachinePool(obj)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	machinePool, err := r.getOwnerMachinePool(ctx, azureMachinePool.ObjectMeta)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	if machinePool == nil {
+		return microerror.Mask(ownerReferenceNotSet)
+	}
+
+	credentialSecret, err := r.getCredentialSecret(ctx, &azureMachinePool)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	blobName := key.BootstrapBlobName(azureMachinePool)
+
+	r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("deleting container object %#q", blobName))
+
+	var containerURL azblob.ContainerURL
+	{
+		containerURL, err = r.getContainerURL(ctx, credentialSecret, key.ClusterID(&azureMachinePool), key.StorageAccountName(&azureMachinePool))
+		if err != nil {
+			return microerror.Mask(err)
+		}
+	}
+
+	blob := containerURL.NewBlockBlobURL(blobName)
+	_, err = blob.Delete(ctx, azblob.DeleteSnapshotsOptionInclude, azblob.BlobAccessConditions{})
+	if blobclient.IsNotFound(err) {
+		r.logger.LogCtx(ctx, "level", "debug", "message", "Bootstrap blob not found when trying to delete it")
+		return nil
+	} else if err != nil {
+		return microerror.Mask(err)
+	}
+
+	r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("deleted container object %#q", blobName))
+
 	return nil
 }
