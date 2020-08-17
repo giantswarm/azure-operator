5fb17b9bf89f4dfa3d7b30f2b063df5a5b73321e
Tuomas Mäkinen <tuomas@giantswarm.io>
2020-08-17 14:03:06 +0300

    Revert "Don't specify release namespace"
    This reverts commit d5fee74c03b0521e7248bc7cca11d8d353988473.

--- service/controller/resource/release/create.go
+++ service/controller/resource/release/create.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/giantswarm/apiextensions/pkg/apis/release/v1alpha1"
 	"github.com/giantswarm/microerror"
+	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/meta"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
@@ -37,7 +38,7 @@ func (r *Resource) EnsureCreated(ctx context.Context, obj interface{}) error {
 		r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("found release version %q from cr", releaseVersion))
 
 		r.logger.LogCtx(ctx, "level", "debug", "message", "reading release object")
-		err = r.k8sClient.CtrlClient().Get(ctx, client.ObjectKey{Name: releaseVersion}, &release)
+		err = r.k8sClient.CtrlClient().Get(ctx, client.ObjectKey{Namespace: corev1.NamespaceAll, Name: releaseVersion}, &release)
 		if err != nil {
 			return microerror.Mask(err)
 		}
