c4191edff01c5fc939bfb616b62920516ad3cb83
Nikola Prokopić <5638639+nprokopic@users.noreply.github.com>
2020-08-18 13:35:13 +0200

    Update service/controller/resource/ipam/azure_machinepool_subnet_collector.go
    Co-authored-by: Tuomas Mäkinen <1947505+tuommaki@users.noreply.github.com>

--- service/controller/resource/ipam/azure_machinepool_subnet_collector.go
+++ service/controller/resource/ipam/azure_machinepool_subnet_collector.go
@@ -155,8 +155,7 @@ func (c *AzureMachinePoolSubnetCollector) collectSubnetsFromAzureVNet(ctx contex
 	resourceGroupName := azureCluster.Name
 
 	// Not assuming VNet name here, keeping it flexible. In order to keep it correct and stable, we
-	// should have a webhook for enforcing a VNet name convention. This check can be removed once
-	// we have implemented a webhook.
+	// should have a webhook for enforcing a VNet name convention.
 	if azureCluster.Spec.NetworkSpec.Vnet.Name == "" {
 		return nil, microerror.Maskf(invalidObjectError, "AzureCluster.Spec.NetworkSpec.Vnet.Name must be set")
 	}
