95a468a8a166a845f23cb08d329c3f2f4a712a64
Tuomas Mäkinen <tuomas@giantswarm.io>
2020-08-20 14:19:28 +0300

    debug log

--- service/controller/resource/clusterid/resource.go
+++ service/controller/resource/clusterid/resource.go
@@ -90,6 +90,7 @@ func (r *Resource) EnsureCreated(ctx context.Context, obj interface{}) error {
 
 // EnsureDeleted is no-op.
 func (r *Resource) EnsureDeleted(ctx context.Context, obj interface{}) error {
+	r.logger.LogCtx(ctx, "level", "debug", "message", "delete event on clusterid handler")
 	return nil
 }
 
