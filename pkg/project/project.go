76cf613bb703d21a30bb4a71592954060d178c3c
axbarsan <mnlk123@yahoo.com>
2020-08-27 10:05:19 +0200

    Revert "Use unique version label"
    This reverts commit 1cbe62e6

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA             = "n/a"
 	name        string = "azure-operator"
 	source      string = "https://github.com/giantswarm/azure-operator"
-	version            = "5.0.0-capilabels"
+	version            = "5.0.0-handle"
 )
 
 func Description() string {
