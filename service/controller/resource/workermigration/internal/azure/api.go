b37405184409e406de7a8fb9a59a7d96c47eed0e
Tuomas Mäkinen <tuomas@giantswarm.io>
2020-09-17 10:17:18 +0300

    Add implementation for Azure API

--- service/controller/resource/workermigration/internal/azure/api.go
+++ service/controller/resource/workermigration/internal/azure/api.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	providerv1alpha1 "github.com/giantswarm/apiextensions/v2/pkg/apis/provider/v1alpha1"
+	"github.com/giantswarm/microerror"
 
 	"github.com/giantswarm/azure-operator/v4/client"
 )
@@ -21,9 +22,29 @@ func GetAPI(f *client.Factory, credentials *providerv1alpha1.CredentialSecret) A
 }
 
 func (a *api) GetVMSS(ctx context.Context, resourceGroupName, vmssName string) (VMSS, error) {
-	return nil, nil
+	client, err := a.clientFactory.GetVirtualMachineScaleSetsClient(a.credentials.Namespace, a.credentials.Name)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	vmss, err := client.Get(ctx, resourceGroupName, vmssName)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	return &vmss, nil
 }
 
 func (a *api) DeleteVMSS(ctx context.Context, resourceGroupName, vmssName string) error {
+	client, err := a.clientFactory.GetVirtualMachineScaleSetsClient(a.credentials.Namespace, a.credentials.Name)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	_, err = client.Delete(ctx, resourceGroupName, vmssName)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
 	return nil
 }
