b5f17843878ab2631b430eae41a9750905887e16
Jose Armesto <github@armesto.net>
2020-09-18 10:20:31 +0200

    Fix version

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA             = "n/a"
 	name        string = "azure-operator"
 	source      string = "https://github.com/giantswarm/azure-operator"
-	version            = "5.0.0-oidc"
+	version            = "5.0.0-dev"
 )
 
 func Description() string {
