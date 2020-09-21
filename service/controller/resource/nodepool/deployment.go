069a28c7e6085c07f6a318cdd65cf69122343193
Whites11 <whites11@gmail.com>
2020-09-21 16:46:14 +0200

    fixed imports

--- service/controller/resource/nodepool/deployment.go
+++ service/controller/resource/nodepool/deployment.go
@@ -4,11 +4,12 @@ import (
 	"context"
 	"encoding/base64"
 	"fmt"
-	"github.com/giantswarm/apiextensions/v2/pkg/apis/provider/v1alpha1"
 	"net/url"
-	"sigs.k8s.io/cluster-api/util"
 	"strconv"
 	"strings"
+
+	"github.com/giantswarm/apiextensions/v2/pkg/apis/provider/v1alpha1"
+	"sigs.k8s.io/cluster-api/util"
 
 	azureresource "github.com/Azure/azure-sdk-for-go/services/resources/mgmt/2019-05-01/resources"
 	"github.com/Azure/azure-sdk-for-go/services/storage/mgmt/2019-04-01/storage"
