a8b367f24cb5cdf791a1175e54de0381ef9e9b22
Jose Armesto <github@armesto.net>
2020-09-23 13:39:24 +0200

    Use interface{} for desired deployment

--- service/controller/resource/nodepool/template/parameters.go
+++ service/controller/resource/nodepool/template/parameters.go
@@ -2,6 +2,7 @@ package template
 
 import (
 	azureresource "github.com/Azure/azure-sdk-for-go/services/resources/mgmt/2019-05-01/resources"
+	"github.com/Azure/go-autorest/autorest/to"
 	"github.com/giantswarm/microerror"
 	"sigs.k8s.io/cluster-api-provider-azure/api/v1alpha3"
 )
@@ -35,10 +36,20 @@ type OSImage struct {
 }
 
 func (p Parameters) ToDeployParams() map[string]interface{} {
+	var dataDisks []map[string]interface{}
+	for _, disk := range p.DataDisks {
+		dataDisks = append(dataDisks, map[string]interface{}{"nameSuffix": disk.NameSuffix, "lun": disk.Lun, "diskSizeGB": disk.DiskSizeGB})
+	}
+
+	var zones []interface{}
+	for _, zone := range p.Zones {
+		zones = append(zones, zone)
+	}
+
 	armDeploymentParameters := map[string]interface{}{}
 	armDeploymentParameters["azureOperatorVersion"] = toARMParam(p.AzureOperatorVersion)
 	armDeploymentParameters["clusterID"] = toARMParam(p.ClusterID)
-	armDeploymentParameters["dataDisks"] = toARMParam(p.DataDisks)
+	armDeploymentParameters["dataDisks"] = toARMParam(dataDisks)
 	armDeploymentParameters["nodepoolName"] = toARMParam(p.NodepoolName)
 	armDeploymentParameters["osImagePublisher"] = toARMParam(p.OSImage.Publisher)
 	armDeploymentParameters["osImageOffer"] = toARMParam(p.OSImage.Offer)
@@ -52,7 +63,7 @@ func (p Parameters) ToDeployParams() map[string]interface{} {
 	armDeploymentParameters["vmCustomData"] = toARMParam(p.VMCustomData)
 	armDeploymentParameters["vmSize"] = toARMParam(p.VMSize)
 	armDeploymentParameters["vnetName"] = toARMParam(p.VnetName)
-	armDeploymentParameters["zones"] = toARMParam(p.Zones)
+	armDeploymentParameters["zones"] = toARMParam(zones)
 
 	return armDeploymentParameters
 }
@@ -84,14 +95,37 @@ func NewFromExtendedDeployment(deployment azureresource.DeploymentExtended) (Par
 }
 
 func newParameters(parameters map[string]interface{}, cast func(param interface{}) interface{}) (Parameters, error) {
-	dataDisks, ok := cast(parameters["dataDisks"]).([]v1alpha3.DataDisk)
+	var dataDisks []v1alpha3.DataDisk
+	disks, ok := cast(parameters["dataDisks"]).([]interface{})
 	if !ok {
-		return Parameters{}, microerror.Maskf(wrongTypeError, "dataDisks should be []v1alpha3.DataDisk, got '%T'", cast(parameters["dataDisks"]))
+		return Parameters{}, microerror.Maskf(wrongTypeError, "dataDisks should be []interface, got '%T'", cast(parameters["dataDisks"]))
 	}
 
-	zones, ok := cast(parameters["zones"]).([]string)
+	for _, disk := range disks {
+		d, ok := disk.(map[string]interface{})
+		if !ok {
+			return Parameters{}, microerror.Maskf(wrongTypeError, "disk should be map[string]interface{}, got '%T'", disk)
+		}
+		dataDisks = append(dataDisks, v1alpha3.DataDisk{
+			NameSuffix: d["nameSuffix"].(string),
+			DiskSizeGB: int32(d["diskSizeGB"].(float64)),
+			Lun:        to.Int32Ptr(int32(d["lun"].(float64))),
+		})
+	}
+
+	var zones []string
+	rawZones, ok := cast(parameters["zones"]).([]interface{})
 	if !ok {
-		return Parameters{}, microerror.Maskf(wrongTypeError, "zones should be []string, got '%T'", cast(parameters["zones"]))
+		return Parameters{}, microerror.Maskf(wrongTypeError, "zones should be []interface, got '%T'", cast(parameters["zones"]))
+	}
+
+	for _, rawZone := range rawZones {
+		zone, ok := rawZone.(string)
+		if !ok {
+			return Parameters{}, microerror.Maskf(wrongTypeError, "zone should be string, got '%T'", rawZone)
+		}
+
+		zones = append(zones, zone)
 	}
 
 	return Parameters{
