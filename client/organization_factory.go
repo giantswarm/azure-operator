e0a0aa275380f0a35a1eaac654e9878bc1880b86
Jose Armesto <github@armesto.net>
2020-09-29 16:53:21 +0200

    Add logging

--- client/organization_factory.go
+++ client/organization_factory.go
@@ -195,6 +195,7 @@ func (f *OrganizationFactory) getCredentialSecret(ctx context.Context, objectMet
 
 // getOrganizationCredentialSecret tries to find a Secret in the organization namespace.
 func (f *OrganizationFactory) getOrganizationCredentialSecret(ctx context.Context, objectMeta v1.ObjectMeta) (*v1alpha1.CredentialSecret, error) {
+	f.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("try in namespace %#q ", objectMeta.Namespace))
 	secretList := &corev1.SecretList{}
 	{
 		err := f.ctrlClient.List(
@@ -237,6 +238,7 @@ func (f *OrganizationFactory) getOrganizationCredentialSecret(ctx context.Contex
 // getLegacyCredentialSecret tries to find a Secret in the default credentials namespace but labeled with the organization name.
 // This is needed while we migrate everything to the org namespace and org credentials are created in the org namespace instead of the default namespace.
 func (f *OrganizationFactory) getLegacyCredentialSecret(ctx context.Context, objectMeta v1.ObjectMeta) (*v1alpha1.CredentialSecret, error) {
+	f.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("try in namespace %#q ", credentialDefaultNamespace))
 	secretList := &corev1.SecretList{}
 	{
 		err := f.ctrlClient.List(
