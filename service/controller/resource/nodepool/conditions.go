2decaaf26b0a9040db1f968637e3bf065252ad87
Nikola Prokopić <nikola@prokopic.rs>
2020-10-07 10:56:17 +0200

    Improved logging when setting AzureMachinePool Status.Condition

--- service/controller/resource/nodepool/conditions.go
+++ service/controller/resource/nodepool/conditions.go
@@ -18,38 +18,56 @@ const (
 
 func (r *Resource) UpdateDeploymentSucceededCondition(ctx context.Context, azureMachinePool *capzexpv1alpha3.AzureMachinePool, provisioningState *string) error {
 	conditionType := azureconditions.DeploymentSucceededCondition
+	var conditionReason string
+	var conditionSeverity capiv1alpha3.ConditionSeverity
+	logger := r.Logger.With("level", "debug", "type", "AzureMachinePool", "message", "setting Status.Condition", "conditionType", conditionType)
 
 	if provisioningState == nil {
+		conditionReason = "DeploymentNotFound"
+		conditionSeverity = capiv1alpha3.ConditionSeverityWarning
 		conditions.MarkFalse(
 			azureMachinePool,
 			conditionType,
-			"DeploymentNotFound",
-			capiv1alpha3.ConditionSeverityWarning,
+			conditionReason,
+			conditionSeverity,
 			"Deployment has not been found.")
+		logger.LogCtx(ctx, "conditionStatus", false, "conditionReason", conditionReason, "conditionSeverity", conditionSeverity)
 	} else {
 		switch *provisioningState {
 		case ProvisioningStateSucceeded:
 			conditions.MarkTrue(azureMachinePool, conditionType)
+			logger.LogCtx(ctx, "conditionStatus", true)
 		case ProvisioningStateFailed:
+			conditionSeverity = capiv1alpha3.ConditionSeverityError
+			conditionReason = "ProvisioningStateFailed"
 			conditions.MarkFalse(
 				azureMachinePool,
 				conditionType,
-				"ProvisioningStateFailed",
-				capiv1alpha3.ConditionSeverityError,
+				conditionReason,
+				conditionSeverity,
 				"Deployment has failed.")
+			logger.LogCtx(ctx, "conditionStatus", false, "conditionReason", conditionReason, "conditionSeverity", conditionSeverity)
 		default:
+			conditionSeverity = capiv1alpha3.ConditionSeverityWarning
+			conditionReason = fmt.Sprintf("ProvisioningState%s", *provisioningState)
 			conditions.MarkFalse(
 				azureMachinePool,
 				conditionType,
-				fmt.Sprintf("ProvisioningState%s", *provisioningState),
-				capiv1alpha3.ConditionSeverityError,
+				conditionReason,
+				conditionSeverity,
 				"Current deployment provisioning status is %s.",
 				*provisioningState)
+			logger.LogCtx(ctx, "conditionStatus", false, "conditionReason", conditionReason, "conditionSeverity", conditionSeverity)
 		}
 	}
 
 	err := r.CtrlClient.Status().Update(ctx, azureMachinePool)
 	if err != nil {
+		r.Logger.LogCtx(ctx,
+			"level", "error",
+			"type", "AzureMachinePool",
+			"conditionType", conditionType,
+			"message", "error while setting Status.Condition")
 		return microerror.Mask(err)
 	}
 
