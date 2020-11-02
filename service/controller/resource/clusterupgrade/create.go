64a0b66a6bf6ff1c9949f3c030445aa572c4d793
Tuomas Mäkinen <tuomas@giantswarm.io>
2020-11-02 12:57:38 +0200

    Improve logs

--- service/controller/resource/clusterupgrade/create.go
+++ service/controller/resource/clusterupgrade/create.go
@@ -35,16 +35,19 @@ func (r *Resource) EnsureCreated(ctx context.Context, obj interface{}) error {
 
 	r.logger.LogCtx(ctx, "level", "debug", "message", "ensuring that machinepools has same release label")
 
+	r.logger.LogCtx(ctx, "level", "debug", "message", "checking if master has been upgraded already")
 	masterUpgraded, err := r.ensureMasterHasUpgraded(ctx, cr)
 	if err != nil {
 		return microerror.Mask(err)
 	}
 
 	if !masterUpgraded {
-		r.logger.LogCtx(ctx, "level", "debug", "message", "master node hasn't upgraded yet")
+		r.logger.LogCtx(ctx, "level", "debug", "message", "master hasn't upgraded yet")
 		r.logger.LogCtx(ctx, "level", "debug", "message", "cancelling resource")
 		return nil
 	}
+
+	r.logger.LogCtx(ctx, "level", "debug", "message", "master has been upgraded already")
 
 	err = r.propagateReleaseToMachinePools(ctx, cr)
 	if err != nil {
