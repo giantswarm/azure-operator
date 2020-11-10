f8ea9127570cdd5f589bc6b6223e31b32eb47dbf
Tuomas Mäkinen <tuomas@giantswarm.io>
2020-11-10 14:19:56 +0200

    adjust project.go

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA             = "n/a"
 	name        string = "azure-operator"
 	source      string = "https://github.com/giantswarm/azure-operator"
-	version            = "5.0.1-machinepoolupgrading"
+	version            = "5.0.1-upgrade"
 )
 
 func Description() string {
