1564c4d66b530680c1b508bd9b6750d2e4531e5f
Tuomas Mäkinen <1947505+tuommaki@users.noreply.github.com>
2020-11-24 12:04:39 +0200

    Add pkger workaround to e2e test packages (#1190) (#1192)
    E2e tests has a build constraint `k8srequired`. This makes `go list` to
    error when enumerating code inside e2e test packages and as a
    consequence, this makes `pkger` to fail because underneath it uses
    `github.com/gobuffalo/here` which runs `go list -json`.
    
    This change adds `nop` variable as a separate source file without build
    constraint into each test package so that Go compiler can find some
    non-restricted code for them and therefore prevent the error.

---
+++ e2e/test/nodepool/pkger.go
@@ -0,0 +1,8 @@
+package nodepool
+
+// nop is unused variable that just enables Go compiler to find something to
+// compile from this package when all other files have build constraing
+// `k8srequired`. This prevents `go list` and therefore `pkger` to fail when
+// enumerating resources in source tree.
+// nolint
+var nop = "enable go list in this package for pkger"
