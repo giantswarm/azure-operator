2147ecaebc446e38807f3b86f3a65efd6fb6dd49
Nikola Prokopić <nikola@prokopic.rs>
2020-12-13 12:35:57 +0100

    Revert project version

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA             = "n/a"
 	name        string = "azure-operator"
 	source      string = "https://github.com/giantswarm/azure-operator"
-	version            = "5.1.0-upgradingfix"
+	version            = "5.1.0-dev"
 )
 
 func Description() string {
