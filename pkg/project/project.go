d500ed021ee4ef775c9a729d89d01275e46b5d14
Nikola Prokopić <nikola@prokopic.rs>
2021-01-11 11:14:06 +0100

    Revert "Change project version"
    This reverts commit 9b4dc818aead49e02fa1f5f3aee800844d952fb9.

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA             = "n/a"
 	name        string = "azure-operator"
 	source      string = "https://github.com/giantswarm/azure-operator"
-	version            = "5.1.1-replicasready"
+	version            = "5.1.1-dev"
 )
 
 func Description() string {
