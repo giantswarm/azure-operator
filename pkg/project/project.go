047e55ff85f994d97aac873f493e0b91f0a661e5
Christian Bianchi <whites11@gmail.com>
2021-01-20 11:00:07 +0100

    Ensure the management cluster's network space is never used for workload clusters

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA             = "n/a"
 	name        string = "azure-operator"
 	source      string = "https://github.com/giantswarm/azure-operator"
-	version            = "5.2.0"
+	version            = "5.2.1-dev"
 )
 
 func Description() string {
