a5e1018e83b88a6cec40de3a9b81780cf647e090
Tuomas Mäkinen <tuomas@giantswarm.io>
2021-01-25 14:30:35 +0200

    Move clusterupgrade handler
    Towards https://github.com/giantswarm/giantswarm/issues/14403

--- service/controller/cluster/controller.go
+++ service/controller/cluster/controller.go
@@ -21,10 +21,10 @@ import (
 	"github.com/giantswarm/azure-operator/v5/pkg/label"
 	"github.com/giantswarm/azure-operator/v5/pkg/project"
 	"github.com/giantswarm/azure-operator/v5/pkg/tenantcluster"
+	"github.com/giantswarm/azure-operator/v5/service/controller/cluster/handler/clusterupgrade"
 	"github.com/giantswarm/azure-operator/v5/service/controller/resource/clusterdependents"
 	"github.com/giantswarm/azure-operator/v5/service/controller/resource/clusterownerreference"
 	"github.com/giantswarm/azure-operator/v5/service/controller/resource/clusterreleaseversion"
-	"github.com/giantswarm/azure-operator/v5/service/controller/resource/clusterupgrade"
 	"github.com/giantswarm/azure-operator/v5/service/controller/setting"
 )
 
