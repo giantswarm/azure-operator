e64c2a6d595019278d2e3b6666a528ce1a9cc5bc
Tuomas Mäkinen <1947505+tuommaki@users.noreply.github.com>
2021-01-25 14:39:13 +0200

    Move clusterreleaseversion handler (#1280)
    Towards https://github.com/giantswarm/giantswarm/issues/14403

--- service/controller/cluster/controller.go
+++ service/controller/cluster/controller.go
@@ -23,7 +23,7 @@ import (
 	"github.com/giantswarm/azure-operator/v5/pkg/tenantcluster"
 	"github.com/giantswarm/azure-operator/v5/service/controller/cluster/handler/clusterdependents"
 	"github.com/giantswarm/azure-operator/v5/service/controller/cluster/handler/clusterownerreference"
-	"github.com/giantswarm/azure-operator/v5/service/controller/resource/clusterreleaseversion"
+	"github.com/giantswarm/azure-operator/v5/service/controller/cluster/handler/clusterreleaseversion"
 	"github.com/giantswarm/azure-operator/v5/service/controller/resource/clusterupgrade"
 	"github.com/giantswarm/azure-operator/v5/service/controller/setting"
 )
