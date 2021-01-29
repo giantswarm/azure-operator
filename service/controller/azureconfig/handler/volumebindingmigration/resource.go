b2a0bbcfe1a209423c567bfa7bb26ec0f55d9bce
Tuomas Mäkinen <tuomas@giantswarm.io>
2021-01-29 14:26:50 +0200

    Re-implement reconciliation of default StorageClasses

--- service/controller/azureconfig/handler/volumebindingmigration/resource.go
+++ service/controller/azureconfig/handler/volumebindingmigration/resource.go
@@ -1,6 +1,7 @@
 package volumebindingmigration
 
 import (
+	"bytes"
 	"context"
 
 	providerv1alpha1 "github.com/giantswarm/apiextensions/v3/pkg/apis/provider/v1alpha1"
@@ -9,10 +10,13 @@ import (
 	"github.com/giantswarm/micrologger"
 	"github.com/giantswarm/tenantcluster/v3/pkg/tenantcluster"
 	storagev1 "k8s.io/api/storage/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/client-go/rest"
-	"sigs.k8s.io/controller-runtime/pkg/client"
+	ctrl "sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/yaml"
 
 	"github.com/giantswarm/azure-operator/v5/service/controller/key"
+	"github.com/giantswarm/azure-operator/v5/service/controller/templates/ignition"
 )
 
 const (
@@ -64,7 +68,7 @@ func (r *Resource) EnsureCreated(ctx context.Context, obj interface{}) error {
 		return microerror.Mask(err)
 	}
 
-	var tenantClusterK8sClient client.Client
+	var tenantClusterK8sClient ctrl.Client
 	{
 		tenantClusterK8sClient, err = r.getTenantClusterClient(ctx, &cr)
 		if err != nil {
@@ -72,32 +76,52 @@ func (r *Resource) EnsureCreated(ctx context.Context, obj interface{}) error {
 		}
 	}
 
-	desiredVolumeBindMode := storagev1.VolumeBindingWaitForFirstConsumer
+	r.logger.Debugf(ctx, "ensuring storageclasses use desired volumeBindingMode")
 
-	r.logger.Debugf(ctx, "ensuring storageclasses use desired volumeBindMode %q", desiredVolumeBindMode)
+	for _, desiredObj := range defaultStorageClasses() {
+		r.logger.Debugf(ctx, "finding present storage class object %q", desiredObj.Name)
 
-	storageClassList := &storagev1.StorageClassList{}
-	err = tenantClusterK8sClient.List(ctx, storageClassList)
-	if err != nil {
-		return microerror.Mask(err)
-	}
+		var presentObj storagev1.StorageClass
+		err := tenantClusterK8sClient.Get(ctx, ctrl.ObjectKey{Name: desiredObj.Name, Namespace: desiredObj.Namespace}, &presentObj)
+		if apierrors.IsNotFound(err) {
+			// All good. We'll create it.
+			r.logger.Debugf(ctx, "did not find present storage class object %q", desiredObj)
+		} else if err != nil {
+			return microerror.Mask(err)
+		} else {
+			r.logger.Debugf(ctx, "finding if present storage class object %q has desired volumeBindingMode %q", desiredObj.Name, desiredObj.VolumeBindingMode)
+		}
 
-	for _, sc := range storageClassList.Items {
-		// Only process Giant Swarm managed StorageClasses.
-		if _, managed := managedStorageClasses[sc.Name]; !managed {
+		// If present object matches the desired one, continue to next one.
+		if presentObj.VolumeBindingMode != nil && *presentObj.VolumeBindingMode == *desiredObj.VolumeBindingMode {
+			r.logger.Debugf(ctx, "present storage class object %q has desired volumeBindingMode: %q", presentObj.Name, desiredObj.VolumeBindingMode)
 			continue
 		}
 
-		if sc.VolumeBindingMode == nil || *sc.VolumeBindingMode != desiredVolumeBindMode {
-			sc.VolumeBindingMode = &desiredVolumeBindMode
-			err = tenantClusterK8sClient.Update(ctx, &sc)
+		// Volume bind mode is immutable field so we must delete the present
+		// object if it exists.
+		if !presentObj.CreationTimestamp.IsZero() && presentObj.ResourceVersion != "" {
+			r.logger.Debugf(ctx, "present storage class object %q does not have desired volumeBindingMode but %q instead", presentObj.Name, presentObj.VolumeBindingMode)
+			r.logger.Debugf(ctx, "deleting present storage class object %q", presentObj.Name)
+			err = tenantClusterK8sClient.Delete(ctx, &presentObj)
 			if err != nil {
 				return microerror.Mask(err)
 			}
+			r.logger.Debugf(ctx, "deleted present storage class object %q", presentObj.Name)
 		}
+
+		r.logger.Debugf(ctx, "creating desired storage class object %q", desiredObj.Name)
+
+		// Finally create the desired object.
+		err = tenantClusterK8sClient.Create(ctx, &desiredObj)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+
+		r.logger.Debugf(ctx, "created desired storage class object %q", desiredObj.Name)
 	}
 
-	r.logger.Debugf(ctx, "ensured storageclasses use desired volumeBindMode %q", desiredVolumeBindMode)
+	r.logger.Debugf(ctx, "ensured storageclasses use desired volumeBindingMode")
 
 	return nil
 }
@@ -112,7 +136,7 @@ func (r *Resource) Name() string {
 	return Name
 }
 
-func (r *Resource) getTenantClusterClient(ctx context.Context, azureConfig *providerv1alpha1.AzureConfig) (client.Client, error) {
+func (r *Resource) getTenantClusterClient(ctx context.Context, azureConfig *providerv1alpha1.AzureConfig) (ctrl.Client, error) {
 	var k8sClient k8sclient.Interface
 	{
 		restConfig, err := r.tenantRestConfigProvider.NewRestConfig(ctx, key.ClusterID(azureConfig), key.ClusterAPIEndpoint(*azureConfig))
@@ -130,4 +154,26 @@ func (r *Resource) getTenantClusterClient(ctx context.Context, azureConfig *prov
 	}
 
 	return k8sClient.CtrlClient(), nil
+}
+
+func defaultStorageClasses() []storagev1.StorageClass {
+	var storageClasses []storagev1.StorageClass
+
+	objs := bytes.Split([]byte(ignition.DefaultStorageClass), []byte("---"))
+
+	for _, bs := range objs {
+		sc := storagev1.StorageClass{}
+		err := yaml.Unmarshal(bs, &sc)
+		if err != nil {
+			panic(err)
+		}
+
+		if sc.Kind != "StorageClass" {
+			continue
+		}
+
+		storageClasses = append(storageClasses, sc)
+	}
+
+	return storageClasses
 }
