43bd89d1b479c021f3166cef82eb49539b02bad3
Christian Bianchi <whites11@gmail.com>
2021-04-15 09:50:37 +0200

    fixed unit test

--- pkg/credential/credential_test.go
+++ pkg/credential/credential_test.go
@@ -279,7 +279,7 @@ func TestFailsToCreateCredentialsUsingOrganizationSecretWhenMissingConfigFromSec
 	}
 }
 
-func TestCredentialsAreConfiguredUsingOrganizationSecretWithSingleTenantServicePrincipalWhenSecretIsLabeled(t *testing.T) {
+func TestFailureWhenSecretIsLabeled(t *testing.T) {
 	fakeK8sClient := unittest.FakeK8sClient()
 	ctx := context.Background()
 	expectedClientID := clientIDFromCredentialSecret
@@ -297,22 +297,9 @@ func TestCredentialsAreConfiguredUsingOrganizationSecretWithSingleTenantServiceP
 
 	azureConfig := createAzureConfigUsingThisOrganizationCredentialSecret("test-cluster", "giantswarm", organizationCredentialSecret)
 	credentialProvider := NewK8SCredentialProvider(fakeK8sClient, "giantswarmTenantID", microloggertest.New())
-	clientCredentialsConfig, _, _, err := credentialProvider.GetOrganizationAzureCredentials(ctx, key.CredentialNamespace(*azureConfig), key.CredentialName(*azureConfig))
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if clientCredentialsConfig.ClientID != expectedClientID {
-		t.Fatalf("clientID has the wrong value: expected %#q, got %#q", expectedClientID, clientCredentialsConfig.ClientID)
-	}
-	if clientCredentialsConfig.ClientSecret != expectedClientSecret {
-		t.Fatalf("clientSecret has the wrong value: expected %#q, got %#q", expectedClientSecret, clientCredentialsConfig.ClientSecret)
-	}
-	if clientCredentialsConfig.TenantID != expectedTenantID {
-		t.Fatalf("tenantID has the wrong value: expected %#q, got %#q", expectedTenantID, clientCredentialsConfig.TenantID)
-	}
-	if len(clientCredentialsConfig.AuxTenants) != 0 {
-		t.Fatalf("there shouldn't be an auxiliary tenant id when secret is labeled as single tenant: expected 0, got %#q auxiliary tenants", clientCredentialsConfig.AuxTenants)
+	_, _, _, err = credentialProvider.GetOrganizationAzureCredentials(ctx, key.CredentialNamespace(*azureConfig), key.CredentialName(*azureConfig))
+	if err == nil {
+		t.Fatal("Credentials creation was expected to fail but it didn't")
 	}
 }
 
