fe4177e286eca56a7824bdaa0d1650accb8ff934
Christian Bianchi <whites11@gmail.com>
2021-05-07 17:19:10 +0200

    improve spot instances upgrades

--- service/controller/azuremachinepool/handler/nodepool/create_wait_for_old_workers_to_be_gone.go
+++ service/controller/azuremachinepool/handler/nodepool/create_wait_for_old_workers_to_be_gone.go
@@ -3,8 +3,6 @@ package nodepool
 import (
 	"context"
 
-	"github.com/Azure/azure-sdk-for-go/services/compute/mgmt/2020-12-01/compute"
-	"github.com/Azure/go-autorest/autorest/to"
 	"github.com/giantswarm/microerror"
 	"sigs.k8s.io/cluster-api/util"
 
@@ -35,36 +33,6 @@ func (r *Resource) waitForOldWorkersToBeGoneTransition(ctx context.Context, obj 
 
 	if len(oldInstances) > 0 {
 		r.Logger.Debugf(ctx, "There are still %d workers from the previous release running", len(oldInstances))
-
-		// We issue another "Delete" to the old instances because sometimes the "Simulate Eviction" API call we
-		// use for spot instances node pools only stops the instances rather than deleting them.
-		{
-			var ids compute.VirtualMachineScaleSetVMInstanceRequiredIDs
-			{
-				var strIds []string
-				for _, i := range oldInstances {
-					strIds = append(strIds, *i.InstanceID)
-				}
-
-				ids = compute.VirtualMachineScaleSetVMInstanceRequiredIDs{
-					InstanceIds: to.StringSlicePtr(strIds),
-				}
-			}
-
-			virtualMachineScaleSetsClient, err := r.ClientFactory.GetVirtualMachineScaleSetsClient(ctx, azureMachinePool.ObjectMeta)
-			if err != nil {
-				return currentState, microerror.Mask(err)
-			}
-
-			res, err := virtualMachineScaleSetsClient.DeleteInstances(ctx, key.ClusterID(&azureMachinePool), key.NodePoolVMSSName(&azureMachinePool), ids, nil)
-			if err != nil {
-				return currentState, microerror.Mask(err)
-			}
-			_, err = virtualMachineScaleSetsClient.DeleteInstancesResponder(res.Response())
-			if err != nil {
-				return currentState, microerror.Mask(err)
-			}
-		}
 
 		return currentState, nil
 	}
