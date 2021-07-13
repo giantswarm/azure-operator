20e66dfe48ebfc765660bfa01a7d281a6d879f3b
Christian Bianchi <whites11@gmail.com>
2021-07-13 14:20:22 +0200

    Allow using an existing public IP for the NAT gateway of worker nodes.

--- pkg/annotation/annotation.go
+++ pkg/annotation/annotation.go
@@ -6,4 +6,6 @@ const (
 
 	// UpgradingToNodePools is set to True during the first cluster upgrade to node pools release.
 	UpgradingToNodePools = "release.giantswarm.io/upgrading-to-node-pools"
+
+	WorkersEgressExternalPublicIP = "giantswarm.io/workers-egress-external-public-ip"
 )
