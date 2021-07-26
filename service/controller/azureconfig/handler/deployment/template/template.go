a6434a7f2342096bd6aec88acc2602c30976806b
Christian Bianchi <whites11@gmail.com>
2021-07-26 17:17:13 +0200

    Use go embed in place of pkger.

--- service/controller/azureconfig/handler/deployment/template/template.go
+++ service/controller/azureconfig/handler/deployment/template/template.go
@@ -8,12 +8,12 @@ import (
 	"github.com/giantswarm/microerror"
 )
 
+//go:embed main.json
+var template string
+
 // GetARMTemplate returns the ARM template reading a json file locally using go embed.
 func GetARMTemplate() (map[string]interface{}, error) {
 	contents := make(map[string]interface{})
-
-	// go:embed main.json
-	var template string
 
 	d := json.NewDecoder(strings.NewReader(template))
 	if err := d.Decode(&contents); err != nil {
