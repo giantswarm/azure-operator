c33798c0072dc9d89bd6e7e4790436e3569ec90d
Christian Bianchi <christian@giantswarm.io>
2021-07-29 13:19:17 +0200

    Use go:embed in place of pkger (#1501)
    * Use go embed in place of pkger.
    
    * Use go embed in place of pkger.
    
    * Use go embed in place of pkger.

--- service/controller/azureconfig/handler/deployment/template/template.go
+++ service/controller/azureconfig/handler/deployment/template/template.go
@@ -1,25 +1,23 @@
 package template
 
 import (
+	_ "embed"
 	"encoding/json"
+	"strings"
 
 	"github.com/giantswarm/microerror"
-	"github.com/markbates/pkger"
 )
 
-// GetARMTemplate returns the ARM template reading a json file locally using pkger.
+//go:embed main.json
+var template string
+
+// GetARMTemplate returns the ARM template reading a json file locally using go embed.
 func GetARMTemplate() (map[string]interface{}, error) {
 	contents := make(map[string]interface{})
 
-	f, err := pkger.Open("/service/controller/azureconfig/handler/deployment/template/main.json")
-	if err != nil {
-		return contents, microerror.Mask(err)
-	}
-	defer f.Close()
-
-	d := json.NewDecoder(f)
+	d := json.NewDecoder(strings.NewReader(template))
 	if err := d.Decode(&contents); err != nil {
 		return contents, microerror.Mask(err)
 	}
-	return contents, microerror.Mask(err)
+	return contents, nil
 }
--- service/controller/azuremachinepool/handler/nodepool/template/template.go
+++ service/controller/azuremachinepool/handler/nodepool/template/template.go
@@ -1,29 +1,27 @@
 package template
 
 import (
+	_ "embed"
 	"encoding/json"
 	"reflect"
+	"strings"
 
 	azureresource "github.com/Azure/azure-sdk-for-go/services/resources/mgmt/2019-05-01/resources"
 	"github.com/giantswarm/microerror"
-	"github.com/markbates/pkger"
 )
 
-// GetARMTemplate returns the ARM template reading a json file locally using pkger.
+//go:embed main.json
+var template string
+
+// GetARMTemplate returns the ARM template reading a json file locally using go embed.
 func GetARMTemplate() (map[string]interface{}, error) {
 	contents := make(map[string]interface{})
 
-	f, err := pkger.Open("/service/controller/azuremachinepool/handler/nodepool/template/main.json")
-	if err != nil {
-		return contents, microerror.Mask(err)
-	}
-	defer f.Close()
-
-	d := json.NewDecoder(f)
+	d := json.NewDecoder(strings.NewReader(template))
 	if err := d.Decode(&contents); err != nil {
 		return contents, microerror.Mask(err)
 	}
-	return contents, microerror.Mask(err)
+	return contents, nil
 }
 
 func NewDeployment(templateParams Parameters) (azureresource.Deployment, error) {
