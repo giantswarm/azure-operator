d2f0dd7c5017b89fa59ae014fe60ad6249ac82a9
GitHub Action <action@github.com>
2021-12-07 10:40:59 +0000

    Bump version to 5.10.3-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA             = "n/a"
 	name        string = "azure-operator"
 	source      string = "https://github.com/giantswarm/azure-operator"
-	version            = "5.10.2"
+	version            = "5.10.3-dev"
 )
 
 func Description() string {
