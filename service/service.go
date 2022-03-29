ec68d2c5a11d2b1810bba38e0be3437332c641b1
Nikola Prokopić <nikola@prokopic.rs>
2022-03-29 15:47:46 +0200

    Add missing schemes to k8s client

--- service/service.go
+++ service/service.go
@@ -7,6 +7,8 @@ import (
 	"sync"
 
 	"github.com/Azure/go-autorest/autorest/azure/auth"
+	oldcapiexpv1alpha3 "github.com/giantswarm/apiextensions/v6/pkg/apis/capiexp/v1alpha3"
+	oldcapzexpv1alpha3 "github.com/giantswarm/apiextensions/v6/pkg/apis/capzexp/v1alpha3"
 	corev1alpha1 "github.com/giantswarm/apiextensions/v6/pkg/apis/core/v1alpha1"
 	providerv1alpha1 "github.com/giantswarm/apiextensions/v6/pkg/apis/provider/v1alpha1"
 	exporterkitcollector "github.com/giantswarm/exporterkit/collector"
@@ -205,6 +207,8 @@ func New(config Config) (*Service, error) {
 				capz.AddToScheme,
 				capiexp.AddToScheme,
 				capzexp.AddToScheme,
+				oldcapiexpv1alpha3.AddToScheme,
+				oldcapzexpv1alpha3.AddToScheme,
 			},
 
 			KubeConfigPath: kubeConfigPath,
