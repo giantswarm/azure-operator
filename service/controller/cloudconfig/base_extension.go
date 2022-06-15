e564849d2180be56f35d14bd813322cb5ceab5b4
Christian Bianchi <whites11@gmail.com>
2022-06-15 09:50:45 +0200

    Switch to external encryption key operator

--- service/controller/cloudconfig/base_extension.go
+++ service/controller/cloudconfig/base_extension.go
@@ -30,7 +30,7 @@ func (e *baseExtension) templateData(certFiles []certs.File) templateData {
 		certsPaths = append(certsPaths, file.AbsolutePath)
 	}
 
-	certsPaths = append(certsPaths, "/etc/kubernetes/encryption/k8s-encryption-config.yaml.enc")
+	certsPaths = append(certsPaths, "/etc/kubernetes/encryption/k8s-encryption-config.yaml")
 
 	primaryScaleSetName := key.WorkerVMSSName(e.customObject)
 	if e.azureMachinePool != nil {
