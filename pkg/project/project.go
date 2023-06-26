07871a16f24e45d68eea83eed402309b8b0432b9
Christian Bianchi <whites11@gmail.com>
2023-06-26 14:49:52 +0200

    - Bump k8scc to 16.2.0 - Remove logic that migrates CAPI CRDs from experimental group to new group.

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA             = "n/a"
 	name        string = "azure-operator"
 	source      string = "https://github.com/giantswarm/azure-operator"
-	version            = "7.2.0"
+	version            = "7.3.0"
 )
 
 func Description() string {
