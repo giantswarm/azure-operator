374a623bda7e21e429f95085914593ae283116c8
taylorbot <dev@giantswarm.io>
2023-07-04 12:48:03 +0000

    Bump version to 8.1.1-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA             = "n/a"
 	name        string = "azure-operator"
 	source      string = "https://github.com/giantswarm/azure-operator"
-	version            = "8.1.0"
+	version            = "8.1.1-dev"
 )
 
 func Description() string {
